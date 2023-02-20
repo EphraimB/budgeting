26a864d3b1ec0fa741b7b1b60e5630b30d257ef0
Ephraim Becker <emb16@outlook.com>
2023-02-20 18:46:12 -0500

    Updated the sql.

--- server/budgeting.sql
+++ server/budgeting.sql
@@ -7,8 +7,8 @@ CREATE TABLE IF NOT EXISTS accounts (
   account_name VARCHAR(255) NOT NULL,
   account_type INT NOT NULL,
   account_balance MONEY NOT NULL,
-  date_created DATE NOT NULL,
-  date_modified DATE NOT NULL
+  date_created TIMESTAMP NOT NULL,
+  date_modified TIMESTAMP NOT NULL
 );
 
 -- Create a deposits table in postgres
@@ -17,8 +17,8 @@ CREATE TABLE IF NOT EXISTS deposits (
   account_id INT NOT NULL REFERENCES accounts(account_id),
   deposit_amount MONEY NOT NULL,
   deposit_description VARCHAR(255) NOT NULL,
-  date_created DATE NOT NULL,
-  date_modified DATE NOT NULL
+  date_created TIMESTAMP NOT NULL,
+  date_modified TIMESTAMP NOT NULL
 );
 
 -- Create a withdrawals table in postgres
@@ -27,8 +27,8 @@ CREATE TABLE IF NOT EXISTS withdrawals (
   account_id INT NOT NULL REFERENCES accounts(account_id),
   withdrawal_amount MONEY NOT NULL,
   withdrawal_description VARCHAR(255) NOT NULL,
-  date_created DATE NOT NULL,
-  date_modified DATE NOT NULL
+  date_created TIMESTAMP NOT NULL,
+  date_modified TIMESTAMP NOT NULL
 );
 
 -- Create a expenses table in postgres
@@ -39,12 +39,10 @@ CREATE TABLE IF NOT EXISTS expenses (
   expense_title VARCHAR(255) NOT NULL,
   expense_description VARCHAR(255) NOT NULL,
   frequency INT NOT NULL,
-  timezone VARCHAR(255) NOT NULL,
-  timezone_offset INT NOT NULL,
-  expense_begin_date DATE NOT NULL,
-  expense_end_date DATE,
-  date_created DATE NOT NULL,
-  date_modified DATE NOT NULL
+  expense_begin_date TIMESTAMP NOT NULL,
+  expense_end_date TIMESTAMP,
+  date_created TIMESTAMP NOT NULL,
+  date_modified TIMESTAMP NOT NULL
 );
 
 -- Create a loans table in postgres
@@ -57,12 +55,10 @@ CREATE TABLE IF NOT EXISTS loans (
   loan_title VARCHAR(255) NOT NULL,
   loan_description VARCHAR(255) NOT NULL,
   frequency INT NOT NULL,
-  timezone VARCHAR(255) NOT NULL,
-  timezone_offset INT NOT NULL,
-  loan_begin_date DATE NOT NULL,
-  loan_end_date DATE,
-  date_created DATE NOT NULL,
-  date_modified DATE NOT NULL
+  loan_begin_date TIMESTAMP NOT NULL,
+  loan_end_date TIMESTAMP,
+  date_created TIMESTAMP NOT NULL,
+  date_modified TIMESTAMP NOT NULL
 );
 
 -- Create a wishlist table in postgres
@@ -73,6 +69,50 @@ CREATE TABLE IF NOT EXISTS wishlist (
   wishlist_title VARCHAR(255) NOT NULL,
   wishlist_description VARCHAR(255) NOT NULL,
   wishlist_priority INT NOT NULL,
-  date_created DATE NOT NULL,
-  date_modified DATE NOT NULL
+  date_created TIMESTAMP NOT NULL,
+  date_modified TIMESTAMP NOT NULL
 );
+
+-- Trigger to update the date_modified column when a row is updated
+CREATE OR REPLACE FUNCTION update_dates()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (TG_OP = 'INSERT') THEN
+        NEW.date_created = current_timestamp;
+        NEW.date_modified = current_timestamp;
+    ELSIF (TG_OP = 'UPDATE') THEN
+        NEW.date_modified = current_timestamp;
+    END IF;
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_accounts_dates
+BEFORE INSERT OR UPDATE ON accounts
+FOR EACH ROW
+EXECUTE PROCEDURE update_dates();
+
+CREATE TRIGGER update_deposits_dates
+BEFORE INSERT OR UPDATE ON deposits
+FOR EACH ROW
+EXECUTE PROCEDURE update_dates();
+
+CREATE TRIGGER update_withdrawals_dates
+BEFORE INSERT OR UPDATE ON withdrawals
+FOR EACH ROW
+EXECUTE PROCEDURE update_dates();
+
+CREATE TRIGGER update_expenses_dates
+BEFORE INSERT OR UPDATE ON expenses
+FOR EACH ROW
+EXECUTE PROCEDURE update_dates();
+
+CREATE TRIGGER update_loans_dates
+BEFORE INSERT OR UPDATE ON loans
+FOR EACH ROW
+EXECUTE PROCEDURE update_dates();
+
+CREATE TRIGGER update_wishlist_dates
+BEFORE INSERT OR UPDATE ON wishlist
+FOR EACH ROW
+EXECUTE PROCEDURE update_dates();
