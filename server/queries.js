410d902c9f282fc20d6f4f311517333c124b84c8
Ephraim Becker <emb16@outlook.com>
2023-02-20 17:58:03 -0500

    Added createAccount function in queries.

--- server/queries.js
+++ server/queries.js
@@ -1,1 +1,16 @@
 const pool = require('./db');
+
+// Create account
+const createAccount = (request, response) => {
+    const { name, type, balance } = request.body;
+
+    pool.query('INSERT INTO account (name, type, balance) VALUES ($1, $2)', [name, type, balance], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(201).send(`Account added with ID: ${results.insertId}`);
+    });
+}
+
+// Export all functions
+module.exports = { createAccount };
--- server/routes/accountsRouter.js
+++ server/routes/accountsRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { createAccount } = require('../queries.js');
 
 router.post('/', createAccount);
 router.put('/:id', updateAccount);
