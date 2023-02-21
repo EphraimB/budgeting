0c5ab49499e27df158786ec550eddd75919369c3
Ephraim Becker <emb16@outlook.com>
2023-02-20 20:49:51 -0500

    Trying to figure out the test.

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -32,4 +32,38 @@ describe('Accounts routes', () => {
         expect(response.body[0].account_type).toBe(0);
         expect(response.body[0].account_balance).toBe("$1,000.00");
     });
+
+    it('should get all accounts', async () => {
+        const response = await request(app).get('/accounts');
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.length).toBeGreaterThan(0);
+    });
+
+    // it('should get an account by id', async () => {
+    //     const response = await request(app).get('/accounts/1');
+
+    //     expect(response.statusCode).toBe(200);
+    //     expect(response.body[0].account_id).toBe(1);
+    // });
+
+    // it('should update an account', async () => {
+    //     const response = await request(app)
+    //         .put('/accounts/1')
+    //         .send({
+    //             name: "Test Account",
+    //             type: 0,
+    //             balance: 1000.00
+    //         });
+
+    //     expect(response.statusCode).toBe(200);
+    //     expect(response.text).toBe("Account modified with ID: 1");
+    // });
+
+    // it('should delete an account', async () => {
+    //     const response = await request(app).delete('/accounts/1');
+
+    //     expect(response.statusCode).toBe(200);
+    //     expect(response.text).toBe("Account deleted with ID: 1");
+    // });
 });
