db3c33b482e6270e26a6dbbb5532146626e1b2ec
Ephraim Becker <emb16@outlook.com>
2023-02-24 11:11:10 -0500

    Put the mock data objects in arrays.

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -5,70 +5,117 @@ const mockClient = new PgMock2();
 const queries = require('../queries');
 
 // Create a mock accounts object
-const mockAccounts = {
-    account_id: 1,
-    account_name: 'Test Account',
-    account_type: 0,
-    account_balance: 1000,
-    date_created: '2020-01-01',
-    date_modified: '2020-01-01'
-};
+const mockAccounts = [
+    {
+        account_id: 1,
+        account_name: 'Test Account',
+        account_type: 0,
+        account_balance: 1000,
+        date_created: '2020-01-01',
+        date_modified: '2020-01-01'
+    },
+    {
+        account_id: 2,
+        account_name: 'Test Account 2',
+        account_type: 1,
+        account_balance: 2000,
+        date_created: '2020-01-01',
+        date_modified: '2020-01-01'
+    }
+];
 
-const mockDeposits = {
-    deposit_id: 1,
-    account_id: 1,
-    deposit_amount: 1000,
-    deposit_description: 'Test Deposit',
-    date_created: '2020-01-01',
-    date_modified: '2020-01-01'
-};
+const mockDeposits = [
+    {
+        deposit_id: 1,
+        account_id: 1,
+        deposit_amount: 1000,
+        deposit_description: 'Test Deposit',
+        date_created: '2020-01-01',
+        date_modified: '2020-01-01'
+    },
+    {
+        deposit_id: 2,
+        account_id: 1,
+        deposit_amount: 2000,
+        deposit_description: 'Test Deposit 2',
+        date_created: '2020-01-01',
+        date_modified: '2020-01-01'
+    }
+];
 
-const mockWithdrawals = {
-    withdrawal_id: 1,
-    account_id: 1,
-    withdrawal_amount: 1000,
-    withdrawal_description: 'Test Withdrawal',
-    date_created: '2020-01-01',
-    date_modified: '2020-01-01'
-};
+const mockWithdrawals = [
+    {
+        withdrawal_id: 1,
+        account_id: 1,
+        withdrawal_amount: 1000,
+        withdrawal_description: 'Test Withdrawal',
+        date_created: '2020-01-01',
+        date_modified: '2020-01-01'
+    },
+    {
+        withdrawal_id: 2,
+        account_id: 1,
+        withdrawal_amount: 200,
+        withdrawal_description: 'Test Withdrawal 2',
+        date_created: '2020-01-01',
+        date_modified: '2020-01-01'
+    }
+];
 
-const mockExpenses = {
-    expense_id: 1,
-    account_id: 1,
-    expense_amount: 50,
-    expense_title: 'Test Expense',
-    expense_description: 'Test Expense to test the expense route',
-    frequency: 0,
-    expense_begin_date: '2020-01-01',
-    expense_end_date: null,
-    date_created: '2020-01-01',
-    date_modified: '2020-01-01'
-};
+const mockExpenses = [
+    {
+        expense_id: 1,
+        account_id: 1,
+        expense_amount: 50,
+        expense_title: 'Test Expense',
+        expense_description: 'Test Expense to test the expense route',
+        frequency: 0,
+        expense_begin_date: '2020-01-01',
+        expense_end_date: null,
+        date_created: '2020-01-01',
+        date_modified: '2020-01-01'
+    },
+    {
+        expense_id: 2,
+        account_id: 1,
+        expense_amount: 100,
+        expense_title: 'Test Expense 2',
+        expense_description: 'Test Expense 2 to test the expense route',
+        frequency: 0,
+        expense_begin_date: '2020-01-01',
+        expense_end_date: null,
+        date_created: '2020-01-01',
+        date_modified: '2020-01-01'
+    }
+];
 
-const mockLoans = {
-    loan_id: 1,
-    account_id: 1,
-    loan_amount: 1000,
-    loan_plan_amount: 100,
-    loan_recipient: 'Test Loan Recipient',
-    loan_title: 'Test Loan',
-    loan_description: 'Test Loan to test the loan route',
-    frequency: 0,
-    loan_begin_date: '2020-01-01',
-    date_created: '2020-01-01',
-    date_modified: '2020-01-01'
-};
+const mockLoans = [
+    {
+        loan_id: 1,
+        account_id: 1,
+        loan_amount: 1000,
+        loan_plan_amount: 100,
+        loan_recipient: 'Test Loan Recipient',
+        loan_title: 'Test Loan',
+        loan_description: 'Test Loan to test the loan route',
+        frequency: 0,
+        loan_begin_date: '2020-01-01',
+        date_created: '2020-01-01',
+        date_modified: '2020-01-01'
+    }
+];
 
-const mockWishlist = {
-    wishlist_id: 1,
-    account_id: 1,
-    wishlist_amount: 1000,
-    wishlist_title: 'Test Wishlist',
-    wishlist_description: 'Test Wishlist to test the wishlist route',
-    wishlist_priority: 0,
-    date_created: '2020-01-01',
-    date_modified: '2020-01-01'
-};
+const mockWishlist = [
+    {
+        wishlist_id: 1,
+        account_id: 1,
+        wishlist_amount: 1000,
+        wishlist_title: 'Test Wishlist',
+        wishlist_description: 'Test Wishlist to test the wishlist route',
+        wishlist_priority: 0,
+        date_created: '2020-01-01',
+        date_modified: '2020-01-01'
+    }];
 
 // Add the mock accounts object to the mock pool
 mockClient.add('SELECT * FROM accounts WHERE account_id = $1', [1], {
