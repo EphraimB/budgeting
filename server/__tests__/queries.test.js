be63e47e612a947037cec8d69820c1a2b3fcde3f
Ephraim Becker <emb16@outlook.com>
2023-02-25 21:42:40 -0500

    Trying to figure out the invalid queries.

--- server/__tests__/queries.test.js
+++ server/__tests__/queries.test.js
@@ -1,12 +1,12 @@
-const app = require("../app");
 const pgmock2 = require('pgmock2').default;
+const { types } = require('pg');
 const { accounts, deposits, withdrawals, expenses, loans, wishlist } = require('./testData/mockData');
 const { accountQueries } = require('../queryData');
 
 const client = new pgmock2();
 
 beforeAll(async () => {
-    client.connect();
+    await client.connect();
 
     const id = accounts[0].account_id;
 
@@ -21,10 +21,20 @@ afterAll(async () => {
 });
 
 describe('GET /accounts/:id', () => {
-    test('GET /accounts/:id returns the correct account', () => {
+    test('GET /accounts/:id returns the correct account', async () => {
         const id = accounts[0].account_id;
 
-        client.query(accountQueries.getAccount, [id])
+        await client.query(accountQueries.getAccount, [id])
+            .then((data) => console.log(data))
+            .catch((err) => console.log(err.message));
+    });
+
+    test('POST /accounts/ returns the correct account', async () => {
+        const name = accounts[0].account_name;
+        const type = accounts[0].account_type;
+        const balance = accounts[0].account_balance;
+
+        await client.query(accountQueries.createAccount, [name, type, balance])
             .then((data) => console.log(data))
             .catch((err) => console.log(err.message));
     });
