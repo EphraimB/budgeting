becf6cc57e227b51d04923a21beb81b5518c8976
Ephraim Becker <emb16@outlook.com>
2023-03-07 10:14:46 -0500

    Debugging months.

--- server/generateExpenses.js
+++ server/generateExpenses.js
@@ -3,12 +3,10 @@ const generateExpenses = (transactions, expense, balance, months) => {
     const currentDate = new Date();
     const amount = parseFloat(expense.expense_amount.substring(1));
 
-    console.log(currentDate);
+    console.log(months);
 
     for (let i = 0; i < months; i++) {
         const expenseDate = new Date(expense.expense_begin_date.getFullYear(), expense.expense_begin_date.getMonth() + i, expense.expense_begin_date.getDate());
-
-        console.log(expenseDate);
 
         // If the expense date is in the future, skip this month
         if (startMonth > currentDate.getMonth()) {
