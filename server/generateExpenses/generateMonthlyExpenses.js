d172d4d13d2281c28c5528084d357848b54c9961
Ephraim Becker <emb16@outlook.com>
2023-03-11 20:25:49 -0500

    Monthly expenses on day of week.

--- server/generateExpenses/generateMonthlyExpenses.js
+++ server/generateExpenses/generateMonthlyExpenses.js
@@ -1,5 +1,13 @@
 const generateMonthlyExpenses = (transactions, expense, toDate) => {
-    const expenseDate = new Date(expense.expense_begin_date);
+    let expenseDate = new Date(expense.expense_begin_date);
+
+    if (expense.frequency_day_of_week) {
+        // If the expense day of week is set, generate expenses every week on specified day of week (0 = Sunday, 6 = Saturday)
+        const startDay = expense.expense_begin_date.getDay();
+        const frequency_day_of_week = expense.frequency_day_of_week;
+
+        expenseDate.setDate(expenseDate.getDate() + (frequency_day_of_week + 28 - startDay) % 7);
+    }
 
     while (expenseDate <= toDate) {
         transactions.push({
