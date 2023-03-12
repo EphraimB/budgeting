8f04436cbb0202fc79b7d831648ff54a0fe88955
Ephraim Becker <emb16@outlook.com>
2023-03-12 11:06:40 -0400

    Fixed more bugs.

--- server/generateLoans/generateDailyLoans.js
+++ server/generateLoans/generateDailyLoans.js
@@ -2,9 +2,11 @@ const generateDailyLoans = (transactions, loan, toDate) => {
     const startDay = loan.loan_begin_date.getDay();
     const planAmount = loan.loan_plan_amount;
 
-    for (let i = 0; ; i += loan.frequency) {
+    for (let i = 0; ; i++) {
         const loanDate = new Date(loan.loan_begin_date);
+        console.log(loanDate);
         loanDate.setDate(startDay + i);
+        console.log(loanDate);
 
         // If the loan date is after toDate, stop generating loans
         if (loanDate > toDate) {
