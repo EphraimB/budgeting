8e0a0a58408c107308563ea8a2bd1bcf8040e4c6
Ephraim Becker <emb16@outlook.com>
2023-03-16 14:03:43 -0400

    Simplified the accounts.

--- server/routes/accountsRouter.js
+++ server/routes/accountsRouter.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getAccounts, createAccount, updateAccount, deleteAccount } = require('../queries.js');
 
-router.get('/', getAccounts);
-router.get('/:id', getAccounts);
+router.get('/:id?', getAccounts);
 router.post('/', createAccount);
 router.put('/:id', updateAccount);
 router.delete('/:id', deleteAccount);
