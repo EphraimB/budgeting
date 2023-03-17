6417e7438a88999d7b9f09e150024c2aeffb0dca
Ephraim Becker <emb16@outlook.com>
2023-03-17 09:55:59 -0400

    Switched to docker env variables.

--- server/db.js
+++ server/db.js
@@ -1,14 +1,11 @@
 const { Pool } = require('pg');
-const dotenv = require('dotenv');
-
-dotenv.config({ override: true });
 
 const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DB,
-    password: process.env.PASSWORD,
-    port: process.env.PORT,
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DB,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
 });
 
 module.exports = pool;
