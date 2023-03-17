c23eb0b8f25479d080d3b3c627305acb36142a41
Ephraim Becker <emb16@outlook.com>
2023-03-17 14:56:46 -0400

    FIxed a typo.

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
-    database: process.env.PGDB,
+    database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT,
 });
