0a5832c393089c73ebdb886c0f7d80f0282144bb
Ephraim Becker <emb16@outlook.com>
2023-03-19 15:28:33 -0400

    Fixed all but one middleware.

--- server/queries.js
+++ server/queries.js
@@ -67,10 +67,9 @@ const deleteAccount = (request, response) => {
 
 // Get deposits by account
 const getDepositsByAccount = (request, response, next) => {
-    const accountId = parseInt(request.query.account_id);
-    const fromDate = request.query.from_date;
+    const { account_id, from_date } = parseInt(request.query);
 
-    pool.query(depositQueries.getDepositsDateFiltered, [accountId, fromDate], (error, results) => {
+    pool.query(depositQueries.getDepositsDateFiltered, [account_id, from_date], (error, results) => {
         if (error) {
             throw error;
         }
@@ -142,31 +141,9 @@ const deleteDeposit = (request, response) => {
 
 // Get withdrawals by account
 const getWithdrawalsByAccount = (request, response, next) => {
-    const accountId = parseInt(request.body.account_id);
-    const fromDate = request.body.from_date;
+    const { account_id, from_date } = parseInt(request.query);
 
-    if (!accountId) {
-        response.status(400).send("Account ID must be provided");
-        return;
-    }
-
-    if (isNaN(accountId)) {
-        response.status(400).send("Account ID must be a number");
-        return;
-    }
-
-    if (!fromDate) {
-        response.status(400).send("From date must be provided");
-        return;
-    }
-
-    // Validate date format in yyyy-mm-dd format
-    if (!fromDate.match(/^\d{4}-\d{2}-\d{2}$/)) {
-        response.status(400).send("From date must be in yyyy-mm-dd format");
-        return;
-    }
-
-    pool.query(withdrawalQueries.getWithdrawalsByAccount, [accountId, fromDate], (error, results) => {
+    pool.query(withdrawalQueries.getWithdrawalsByAccount, [account_id, from_date], (error, results) => {
         if (error) {
             throw error;
         }
@@ -238,34 +215,7 @@ const deleteWithdrawal = (request, response) => {
 
 // Get expenses by account
 const getExpensesByAccount = (request, response, next) => {
-    const account_id = parseInt(request.body.account_id);
-    const to_date = request.body.to_date;
-
-    if (!account_id) {
-        response.status(400).send("Account ID must be provided");
-        return;
-    }
-
-    if (isNaN(account_id)) {
-        response.status(400).send("Account ID must be a number");
-        return;
-    }
-
-    if (account_id <= 0) {
-        response.status(400).send("Account ID must be greater than 0");
-        return;
-    }
-
-    if (!to_date) {
-        response.status(400).send("To date must be provided");
-        return;
-    }
-
-    // Validate date format in yyyy-mm-dd format
-    if (!to_date.match(/^\d{4}-\d{2}-\d{2}$/)) {
-        response.status(400).send("To date must be in yyyy-mm-dd format");
-        return;
-    }
+    const { account_id, to_date } = parseInt(request.query);
 
     pool.query(expenseQueries.getExpensesByAccount, [account_id, to_date], (error, results) => {
         if (error) {
@@ -339,31 +289,9 @@ const deleteExpense = (request, response) => {
 
 // Get loans by account
 const getLoansByAccount = (request, response, next) => {
-    const accountId = parseInt(request.body.account_id);
-    const to_date = request.body.to_date;
+    const { account_id, to_date } = parseInt(request.query);
 
-    if (!accountId) {
-        response.status(400).send("Account ID must be provided");
-        return;
-    }
-
-    if (accountId < 1) {
-        response.status(400).send("Account ID must be greater than 0");
-        return;
-    }
-
-    if (!to_date) {
-        response.status(400).send("To date must be provided");
-        return;
-    }
-
-    // If to date isn't in the format YYYY-MM-DD, it will be rejected
-    if (!to_date.match(/^\d{4}-\d{2}-\d{2}$/)) {
-        response.status(400).send("To date must be in the format YYYY-MM-DD");
-        return;
-    }
-
-    pool.query(loanQueries.getLoansByAccount, [accountId, to_date], (error, results) => {
+    pool.query(loanQueries.getLoansByAccount, [account_id, to_date], (error, results) => {
         if (error) {
             throw error;
         }
@@ -435,10 +363,9 @@ const deleteLoan = (request, response) => {
 
 // Get wishlists by account
 const getWishlistsByAccount = (request, response, next) => {
-    const accountId = parseInt(request.params.accountId);
-    const months = parseInt(request.params.months);
+    const account_id = parseInt(request.params);
 
-    pool.query(wishlistQueries.getWishlistsByAccount, [accountId], (error, results) => {
+    pool.query(wishlistQueries.getWishlistsByAccount, [account_id], (error, results) => {
         if (error) {
             throw error;
         }
@@ -510,17 +437,9 @@ const deleteWishlist = (request, response) => {
 
 // Get current balance of account based on deposits and withdrawals
 const getCurrentBalance = (request, response, next) => {
-    const accountId = parseInt(request.body.account_id);
+    const { account_id } = parseInt(request.body);
 
-    if (isNaN(accountId)) {
-        return response.status(400).send(`Account ID must be a number`);
-    }
-
-    if (accountId < 0) {
-        return response.status(400).send(`Account ID must be greater than 0`);
-    }
-
-    pool.query(currentBalanceQueries.getCurrentBalance, [accountId], (error, results) => {
+    pool.query(currentBalanceQueries.getCurrentBalance, [account_id], (error, results) => {
         if (error) {
             throw error;
         }
