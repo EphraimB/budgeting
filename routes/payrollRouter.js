38d35070ad2fcd0bd6c7b49e60a61d9cac4b7e5a
Ephraim Becker <emb16@outlook.com>
2023-03-28 14:45:40 -0400

    Started work on the payroll router.

---
+++ routes/payrollRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { query, param, body } = require('express-validator');
+const router = express.Router();
+const { getPayroll } = require('../queries.js');
+const validateRequest = require('../validateRequest.js');
+
+router.get('/:account_id',
+    [
+        param('account_id').isInt({ min: 1 }).withMessage('Account ID must be a number'),
+        query('id').optional().isInt({ min: 1 }).withMessage('ID must be a number'),
+        validateRequest,
+    ],
+    getPayroll);
