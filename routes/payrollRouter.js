41a157fd5d8452361b8b482964c64ba3f08f58a5
Ephraim Becker <emb16@outlook.com>
2023-04-16 14:17:17 -0400

    Reverted.

--- routes/payrollRouter.js
+++ routes/payrollRouter.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const { query, param, body } = require('express-validator');
 const router = express.Router();
-const { getPayrollsMiddleware } = require('../queries.js');
+const { getPayrolls } = require('../queries.js');
 const validateRequest = require('../validateRequest.js');
 
-router.get('/',
-    // [
-    //     param('account_id').isInt({ min: 1 }).withMessage('Account ID must be a number'),
-    //     query('id').optional().isInt({ min: 1 }).withMessage('ID must be a number'),
-    //     validateRequest,
-    // ],
-    getPayrollsMiddleware);
+router.get('/:account_id',
+    [
+        param('account_id').isInt({ min: 1 }).withMessage('Account ID must be a number'),
+        query('id').optional().isInt({ min: 1 }).withMessage('ID must be a number'),
+        validateRequest,
+    ],
+    getPayrolls);
 
 module.exports = router;
