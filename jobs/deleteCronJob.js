b8f7471e20bfed54325333a900bf967f2dbb3376
Ephraim Becker <emb16@outlook.com>
2023-05-02 18:32:42 -0400

    Deleted uneeded comment.

--- jobs/deleteCronJob.js
+++ jobs/deleteCronJob.js
@@ -29,33 +29,6 @@ const deleteCronJob = (cronId) => {
             pool.end();
         });
     });
-
-    //     return new Promise((resolve, reject) => {
-    //         pool.query(cronJobQueries.getCronJob, [cronId], (error, results) => {
-    //             if (error) {
-    //                 return reject(error);
-    //             }
-    //             const uniqueId = results.rows[0].unique_id;
-
-    //             // Stop the running cron job if it exists
-    //             const myJob = schedule.scheduledJobs[uniqueId];
-
-    //             if(myJob) {
-    //                 myJob.cancel();
-    //                 console.log('Cron job stopped ' + cronId);
-
-    //                 // Delete the cron job from the database
-    //                 pool.query(cronJobQueries.deleteCronJob, [cronId], (error, results) => {
-    //                     if (error) {
-    //                         return reject(error);
-    //                     }
-    //                     console.log('Cron job deleted ' + cronId);
-    //                 });
-    //             } else {
-    //                 console.log('Cron job not found ' + cronId);
-    //             }
-    //         });
-    //     });
 }
 
 module.exports = deleteCronJob
