5cf5120e4fbf9cfddfa14bfb7e6f6ac0407477bb
Ephraim Becker <emb16@outlook.com>
2023-05-12 09:22:12 -0400

    Passes the first api test. #46

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,5 +6,6 @@ describe("Test the root path", () => {
     test("It should response the GET method", async () => {
         const response = await request(app).get("/api/");
         expect(response.statusCode).toBe(200);
+        expect(response.text).toEqual("Hello World!");
     });
 });
