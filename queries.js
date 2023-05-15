9a3f80f15935eb88a9b1880eef1925f8ab83e352
Ephraim Becker <emb@paay.co>
2023-05-15 12:02:58 -0400

    Parsed the getLoans response. #43

--- queries.js
+++ queries.js
@@ -463,22 +463,65 @@ const getLoansByAccount = (request, response, next) => {
 
 // Get all loans
 const getLoans = (request, response) => {
-    const { account_id } = request.params;
     const { id } = request.query;
 
     if (!id) {
-        pool.query(loanQueries.getLoans, [account_id], (error, results) => {
+        pool.query(loanQueries.getLoans, (error, results) => {
             if (error) {
                 return response.status(400).send({ errors: { "msg": "Error getting loans", "param": null, "location": "query" } });
             }
-            response.status(200).json(results.rows);
+
+            // Parse the data to correct format and return an object
+            const loans = results.rows.map((loan) => ({
+                loan_id: parseInt(loan.loan_id),
+                account_id: parseInt(loan.account_id),
+                loan_amount: parseFloat(loan.loan_amount),
+                loan_plan_amount: parseFloat(loan.loan_plan_amount),
+                loan_recipient: loan.loan_recipient,
+                loan_title: loan.loan_title,
+                loan_description: loan.loan_description,
+                frequency_type: loan.frequency_type,
+                frequency_type_variable: loan.frequency_type_variable,
+                frequency_day_of_month: loan.frequency_day_of_month,
+                frequency_day_of_week: loan.frequency_day_of_week,
+                frequency_week_of_month: loan.frequency_week_of_month,
+                frequency_month_of_year: loan.frequency_month_of_year,
+                loan_begin_date: loan.loan_begin_date,
+                loan_end_date: loan.loan_end_date,
+                date_created: loan.date_created,
+                date_modified: loan.date_modified,
+            }));
+
+            response.status(200).json(loans);
         });
     } else {
-        pool.query(loanQueries.getLoan, [account_id, id], (error, results) => {
+        pool.query(loanQueries.getLoan, [id], (error, results) => {
             if (error) {
                 return response.status(400).send({ errors: { "msg": "Error getting loan", "param": null, "location": "query" } });
             }
-            response.status(200).json(results.rows);
+
+            // Parse the data to correct format and return an object
+            const loans = results.rows.map((loan) => ({
+                loan_id: parseInt(loan.loan_id),
+                account_id: parseInt(loan.account_id),
+                loan_amount: parseFloat(loan.loan_amount),
+                loan_plan_amount: parseFloat(loan.loan_plan_amount),
+                loan_recipient: loan.loan_recipient,
+                loan_title: loan.loan_title,
+                loan_description: loan.loan_description,
+                frequency_type: loan.frequency_type,
+                frequency_type_variable: loan.frequency_type_variable,
+                frequency_day_of_month: loan.frequency_day_of_month,
+                frequency_day_of_week: loan.frequency_day_of_week,
+                frequency_week_of_month: loan.frequency_week_of_month,
+                frequency_month_of_year: loan.frequency_month_of_year,
+                loan_begin_date: loan.loan_begin_date,
+                loan_end_date: loan.loan_end_date,
+                date_created: loan.date_created,
+                date_modified: loan.date_modified,
+            }));
+
+            response.status(200).json(loans);
         });
     }
 }
