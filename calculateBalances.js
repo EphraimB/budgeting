91a388429f2b8d959957dd5dc288a3c62f75e39e
Ephraim Becker <emb16@outlook.com>
2023-05-16 15:06:33 -0400

    Refactored calculateBalances. #58

--- calculateBalances.js
+++ calculateBalances.js
@@ -1,22 +1,23 @@
 const calculateBalances = (transactions, currentBalance) => {
-    transactions.sort((a, b) => new Date(a.date) - new Date(b.date));
-
-    const pastTransactions = transactions.filter(transaction => transaction.date < new Date());
-    const futureTransactions = transactions.filter(transaction => transaction.date >= new Date());
-    const reversedPastTransactions = pastTransactions.slice().reverse();
+    const sortedTransactions = transactions.sort((a, b) => new Date(a.date) - new Date(b.date));
     let balance = currentBalance;
-
-    reversedPastTransactions.forEach(transaction => {
-        transaction.balance = balance;
-        balance -= transaction.amount;
-    });
-
-    balance = currentBalance;
-
-    futureTransactions.forEach(transaction => {
-        balance += transaction.amount;
-        transaction.balance = parseFloat(balance.toFixed(2));
-    });
-}
-
-export default calculateBalances;
+  
+    const calculateBalanceForPastTransaction = (transaction) => {
+      transaction.balance = balance;
+      balance -= transaction.amount;
+    };
+  
+    const calculateBalanceForFutureTransaction = (transaction) => {
+      balance += transaction.amount;
+      transaction.balance = parseFloat(balance.toFixed(2));
+    };
+  
+    const pastTransactions = sortedTransactions.filter(transaction => transaction.date < new Date());
+    const futureTransactions = sortedTransactions.filter(transaction => transaction.date >= new Date());
+  
+    pastTransactions.reverse().forEach(calculateBalanceForPastTransaction);
+    futureTransactions.forEach(calculateBalanceForFutureTransaction);
+  };
+  
+  export default calculateBalances;
+  
