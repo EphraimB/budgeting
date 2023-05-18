9a524956c3cf85302d820d3c8931cb75b1217944
Ephraim Becker <emb16@outlook.com>
2023-05-18 10:57:50 -0400

    Payroll dates is now mvc. #58

--- routes/payrollDatesRouter.js
+++ routes/payrollDatesRouter.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { query, param, body } from 'express-validator';
 const router = express.Router();
-import { getPayrollDates, createPayrollDate, updatePayrollDate, deletePayrollDate } from '../queries.js';
+import { getPayrollDates, createPayrollDate, updatePayrollDate, deletePayrollDate } from '../controllers/payrollDatesController.js';
 import validateRequest from '../validateRequest.js';
 
 router.get('/',
