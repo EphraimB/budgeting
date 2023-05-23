c259d6f83c0f6be8987b7df0491abc4b7d007ccd
Ephraim Becker <emb16@outlook.com>
2023-05-23 10:44:52 -0400

    Passes the post accounts router test. #46

--- __tests__/accountsRouter.test.js
+++ __tests__/accountsRouter.test.js
@@ -2,23 +2,43 @@ import { jest } from '@jest/globals';
 import request from 'supertest';
 import express from 'express';
 
-jest.unstable_mockModule('../controllers/accountsController', () => ({
-    getAccounts: (req, res, next) => res.json({ message: 'success' }),
-    createAccount: jest.fn(),
-    updateAccount: jest.fn(),
-    deleteAccount: jest.fn(),
-}));
+// Factory function for creating an app with the mock router
+const createApp = async () => {
+    const app = express();
+    app.use(express.json());
 
-let accountsRouter;
-
-const app = express();
-app.use(express.json());
-
-beforeEach(async () => {
-    // Then, import the module that uses the mock
+    // Import the module that uses the mock
     const routerModule = await import('../routes/accountsRouter');
     accountsRouter = routerModule.default;
     app.use('/', accountsRouter);
+
+    return app;
+};
+
+beforeAll(() => {
+    const newAccount = {
+        name: 'test',
+        balance: 100,
+        type: 1
+    };
+
+    jest.unstable_mockModule('../controllers/accountsController', () => ({
+        getAccounts: (req, res, next) => res.json({ message: 'success' }),
+        createAccount: (req, res, next) => res.json(newAccount),
+        updateAccount: jest.fn(),
+        deleteAccount: jest.fn(),
+    }));
+});
+
+afterAll(() => {
+    jest.restoreAllMocks();
+});
+
+let app;
+
+beforeEach(async () => {
+    // Create a new app for each test
+    app = await createApp();
 });
 
 describe('GET /', () => {
@@ -42,5 +62,24 @@ describe('GET / with id query', () => {
 
         expect(response.status).toBe(200);
         expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('POST /', () => {
+    it('responds with json', async () => {
+        const newAccount = {
+            name: 'test',
+            balance: 100,
+            type: 1
+        };
+
+        const response = await request(app)
+            .post('/')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .send(newAccount);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(newAccount);
     });
 });
