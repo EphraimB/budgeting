34ed52185597a5680a97bcede1ad17086713ec20
Ephraim Becker <emb16@outlook.com>
2023-05-22 20:19:47 -0400

    Passes the test. #46

--- __tests__/accountsRouter.test.js
+++ __tests__/accountsRouter.test.js
@@ -1,16 +1,34 @@
+import { jest } from '@jest/globals';
 import request from 'supertest';
 import express from 'express';
-import accountsRouter from '../routes/accountsRouter';
+
+jest.unstable_mockModule('../controllers/accountsController', () => ({
+    getAccounts: (req, res, next) => res.json({ message: 'success' }),
+    createAccount: jest.fn(),
+    updateAccount: jest.fn(),
+    deleteAccount: jest.fn(),
+}));
+
+let accountsRouter;
 
 const app = express();
 app.use(express.json());
-app.use('/', accountsRouter);
 
 describe('GET /', function () {
-    it('responds with json', function (done) {
-        request(app)
+    beforeAll(async () => {
+        // Then, import the module that uses the mock
+        const routerModule = await import('../routes/accountsRouter');
+        accountsRouter = routerModule.default;
+        app.use('/', accountsRouter);
+    });
+
+    it('responds with json', async () => {
+        const response = await request(app)
             .get('/')
-            .expect('Content-Type', /json/)
-            .expect(200, done);
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
     });
 });
