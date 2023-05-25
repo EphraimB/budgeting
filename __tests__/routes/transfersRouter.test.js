7823dcdcc2c204125e36e43122ee98dc5588409d
Ephraim Becker <emb16@outlook.com>
2023-05-25 10:47:19 -0400

    Passes transfers router test. #46

---
+++ __tests__/routes/transfersRouter.test.js
@@ -0,0 +1,113 @@
+import { jest } from '@jest/globals';
+import request from 'supertest';
+import express from 'express';
+
+// Factory function for creating an app with the mock router
+const createApp = async () => {
+    const app = express();
+    app.use(express.json());
+
+    // Import the module that uses the mock
+    const routerModule = await import('../../routes/transfersRouter');
+    const transfersRouter = routerModule.default;
+    app.use('/', transfersRouter);
+
+    return app;
+};
+
+const createFutureTransfer = () => {
+    const dateInFuture = new Date();
+    dateInFuture.setDate(dateInFuture.getDate() + 7);
+
+    return {
+        source_account_id: 1,
+        destination_account_id: 2,
+        amount: 100,
+        title: 'test',
+        description: 'test',
+        frequency_type: 2,
+        begin_date: dateInFuture
+    };
+};
+
+beforeAll(() => {
+    jest.unstable_mockModule('../../controllers/transfersController', () => ({
+        getTransfers: (req, res, next) => res.json({ message: 'success' }),
+        createTransfer: (req, res, next) => res.json({ message: 'success' }),
+        updateTransfer: (req, res, next) => res.json({ message: 'success' }),
+        deleteTransfer: (req, res, next) => res.json({ message: 'success' }),
+    }));
+});
+
+afterAll(() => {
+    jest.restoreAllMocks();
+});
+
+let app;
+
+beforeEach(async () => {
+    // Create a new app for each test
+    app = await createApp();
+});
+
+describe('GET /', () => {
+    it('responds with json', async () => {
+        const response = await request(app)
+            .get('/?account_id=1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('GET / with id query', () => {
+    it('responds with json', async () => {
+        const response = await request(app)
+            .get('/?account_id=1&id=1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('POST /', () => {
+    it('responds with json', async () => {
+        const response = await request(app)
+            .post('/')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .send(createFutureTransfer());
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('PUT /:id', () => {
+    it('responds with json', async () => {
+        const response = await request(app)
+            .put('/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .send(createFutureTransfer());
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('DELETE /:id', () => {
+    it('responds with json', async () => {
+        const response = await request(app)
+            .delete('/1?account_id=1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
