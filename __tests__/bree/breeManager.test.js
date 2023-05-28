a60d739dac1cd30316a8691d1af4aaa92493b91d
Ephraim Becker <emb16@outlook.com>
2023-05-28 13:52:44 -0400

    Formatting.

--- __tests__/bree/breeManager.test.js
+++ __tests__/bree/breeManager.test.js
@@ -3,41 +3,41 @@ import { jest } from '@jest/globals';
 const mockGetJobs = jest.fn().mockResolvedValue([{ name: 'job1' }, { name: 'job2' }]);
 
 jest.unstable_mockModule('cabin', () => ({
-  default: jest.fn(),
+    default: jest.fn(),
 }));
 
 jest.unstable_mockModule('../../breeManager.js', () => {
-  const mockConfig = {
-    jobs: [{ name: 'mockJob' }], // Set the initial jobs in the mock config
-  };
-  const mockGetBree = jest.fn().mockReturnValue({
-    start: jest.fn(),
-    config: mockConfig, // Use the mock config
-  });
+    const mockConfig = {
+        jobs: [{ name: 'mockJob' }], // Set the initial jobs in the mock config
+    };
+    const mockGetBree = jest.fn().mockReturnValue({
+        start: jest.fn(),
+        config: mockConfig, // Use the mock config
+    });
 
-  return {
-    initializeBree: jest.fn(),
-    getBree: mockGetBree,
-  };
+    return {
+        initializeBree: jest.fn(),
+        getBree: mockGetBree,
+    };
 });
 
 jest.unstable_mockModule('../../getJobs', () => ({
-  default: mockGetJobs,
+    default: mockGetJobs,
 }));
 
 const { initializeBree, getBree } = await import('../../breeManager');
 
 describe('breeManager', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
 
-  it('should initialize Bree correctly', async () => {
-    await initializeBree();
+    it('should initialize Bree correctly', async () => {
+        await initializeBree();
 
-    expect(getBree().config.jobs).toEqual([
-      { name: 'mockJob' }, // Expect the initial jobs from the mock config
-    ]);
-    expect(getBree().start).toHaveBeenCalled();
-  });
+        expect(getBree().config.jobs).toEqual([
+            { name: 'mockJob' }, // Expect the initial jobs from the mock config
+        ]);
+        expect(getBree().start).toHaveBeenCalled();
+    });
 });
