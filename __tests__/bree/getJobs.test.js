ca1e90c102cc17d38695c08e4aafef8d697a2ed4
Ephraim Becker <emb16@outlook.com>
2023-05-28 18:30:26 -0400

    Trying to mock getJobs.

--- __tests__/bree/getJobs.test.js
+++ __tests__/bree/getJobs.test.js
@@ -1,54 +1,45 @@
-import { jest } from '@jest/globals';
+import fs from 'fs';
 import path from 'path';
+import { jest } from '@jest/globals';
 import { fileURLToPath } from 'url';
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const jobsFilePath = path.resolve(__dirname, 'jobs.json');
 
-const jobsFilePath = path.join(__dirname, 'jobs.json');
-
-// Create a sample jobs.json file in the test directory
-beforeAll(() => {
-    const jobs = [{ name: 'existing-job', cron: '0 0 * * *', path: '/app/jobs/script.js' }];
-    jest.unstable_mockModule('fs', () => ({
-        default: jest.requireActual('fs'),
-        existsSync: jest.fn(() => true),
-        readFileSync: jest.fn(() => JSON.stringify(jobs, null, 2)),
-        writeFileSync: jest.fn(),
-    }));
+// Mock the getEmployeesData function
+jest.doMock('../../getJobs.js', () => {
+    const actualModule = require('../../getJobs.js');
+    return {
+        getJobs: actualModule,
+        getEmployeesData: jest.fn().mockResolvedValue([
+            { employee_id: 1 },
+            { employee_id: 2 },
+        ]),
+    };
 });
 
-// Delete the jobs.json file from the test directory
-afterAll(() => {
-    jest.resetModules(); // Reset modules to clear the fs mock
-});
-
-// Mock the ./getPayrolls module and provide a mock implementation for the getPayrolls function
-jest.unstable_mockModule('../../getPayrolls.js', () => ({
-    default: jest.fn(),
+// Mock fs module
+jest.unstable_mockModule('fs', () => ({
+    default: jest.requireActual('fs'),
+    existsSync: jest.fn((path) => path === jobsFilePath),
+    readFileSync: jest.fn((path) => path === jobsFilePath ? '[]' : fs.readFileSync(path)),
+    writeFileSync: jest.fn(),
 }));
 
 describe('getJobs', () => {
-    it('should return jobs without calling getPayrolls', async () => {
-        const mockQuery = jest.fn((_, callback) => {
-            const rows = [{ employee_id: 1 }, { employee_id: 2 }];
-            callback(null, { rows });
-        });
+    it('should return an empty jobs object when no jobs.json file exists', async () => {
+        const { getJobs } = await import('../../getJobs.js');
 
-        jest.unstable_mockModule('../../models/db.js', () => ({
-            default: {
-                query: mockQuery,
-            },
-        }));
-
-        const jobs = import('../../getJobs.js');
+        const { jobs } = await getJobs(jobsFilePath);
 
         expect(jobs).toEqual([
-            { name: 'existing-job', cron: '0 0 * * *', path: '/app/jobs/script.js' },
             { name: 'payroll-checker-employee-1', cron: '0 0 1 * *', path: '/app/jobs/cronScriptGetPayrolls.js', worker: { workerData: { employee_id: 1 } } },
-            { name: 'payroll-checker-employee-2', cron: '0 0 1 * *', path: '/app/jobs/cronScriptGetPayrolls.js', worker: { workerData: { employee_id: 2 } } },
+            { name: 'payroll-checker-employee-2', cron: '0 0 1 * *', path: '/app/jobs/cronScriptGetPayrolls.js', worker: { workerData: { employee_id: 2 } } }
         ]);
 
-        const { writeFileSync } = require('fs');
-        expect(writeFileSync).toHaveBeenCalledWith(jobsFilePath, JSON.stringify(expect.any(Array), null, 2), 'utf8');
+        expect(fs.existsSync).toHaveBeenCalledWith(jobsFilePath);
+        expect(fs.readFileSync).toHaveBeenCalledWith(jobsFilePath, 'utf8');
+        expect(fs.writeFileSync).toHaveBeenCalledWith(jobsFilePath, '[]', 'utf8');
     });
 });
--- getJobs.js
+++ getJobs.js
@@ -1,10 +1,10 @@
 import pool from "./models/db.js";
 import { payrollQueries } from "./models/queryData.js";
 import fs from "fs";
-const jobsFilePath = 'jobs.json';
 import getPayrolls from "./getPayrolls.js";
 
-const getJobs = () => {
+// Function to fetch employee data from the database
+export const getEmployeesData = () => {
     return new Promise((resolve, reject) => {
         pool.query(payrollQueries.getEmployees, (error, results) => {
             if (error) {
@@ -13,41 +13,49 @@ const getJobs = () => {
                 return;
             }
 
-            let jobs = [];
-
-            // Function to merge the payrollCheckerjobs array with the existing jobs array
-            if (fs.existsSync(jobsFilePath)) {
-                // Read the job definitions from the JSON file
-                jobs = JSON.parse(fs.readFileSync(jobsFilePath, 'utf8'));
-            }
-
-            // Execute the cronScriptGetPayrolls.js script if there are no jobs that start with payroll- in the jobs array
-            if (!jobs.some(job => job.name.startsWith("payroll-"))) {
-                results.rows.forEach((employee) => {
-                    (async () => {
-                        await getPayrolls(employee.employee_id);
-                    })();
-                });
-            }
-
-            const payrollCheckerjobs = results.rows.map((employee) => ({
-                name: `payroll-checker-employee-${employee.employee_id}`,
-                cron: "0 0 1 * *",
-                path: "/app/jobs/cronScriptGetPayrolls.js",
-                worker: {
-                    workerData: {
-                        employee_id: employee.employee_id,
-                    },
-                },
-            }));
-
-            fs.writeFileSync(jobsFilePath, JSON.stringify(jobs, null, 2), 'utf8');
-
-            jobs = jobs.concat(payrollCheckerjobs);
-
-            resolve(jobs);
+            resolve(results.rows);
         });
     });
 };
 
-export default getJobs;
+// Main function to get jobs
+export const getJobs = async (jobsFilePath = 'jobs.json') => {
+    try {
+        const employeeData = await getEmployeesData();
+
+        let jobs = [];
+
+        // Function to merge the payrollCheckerjobs array with the existing jobs array
+        if (fs.existsSync(jobsFilePath)) {
+            // Read the job definitions from the JSON file
+            jobs = JSON.parse(fs.readFileSync(jobsFilePath, 'utf8'));
+        }
+
+        // Execute the cronScriptGetPayrolls.js script if there are no jobs that start with payroll- in the jobs array
+        if (!jobs.some((job) => job.name.startsWith("payroll-"))) {
+            for (const employee of employeeData) {
+                await getPayrolls(employee.employee_id);
+            }
+        }
+
+        const payrollCheckerjobs = employeeData.map((employee) => ({
+            name: `payroll-checker-employee-${employee.employee_id}`,
+            cron: "0 0 1 * *",
+            path: "/app/jobs/cronScriptGetPayrolls.js",
+            worker: {
+                workerData: {
+                    employee_id: employee.employee_id,
+                },
+            },
+        }));
+
+        fs.writeFileSync(jobsFilePath, JSON.stringify(jobs, null, 2), 'utf8');
+
+        jobs = jobs.concat(payrollCheckerjobs);
+
+        return jobs;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
