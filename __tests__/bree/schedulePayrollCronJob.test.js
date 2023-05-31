d7ce4426320f7769cb5439efdbde5ab5ab1a0dc7
Ephraim Becker <emb@paay.co>
2023-05-31 16:40:43 -0400

    Trying to get memfs to work.

--- __tests__/bree/schedulePayrollCronJob.test.js
+++ __tests__/bree/schedulePayrollCronJob.test.js
@@ -2,33 +2,38 @@ import { jest } from '@jest/globals';
 import { Volume } from 'memfs';
 import schedulePayrollCronJob from '../../jobs/schedulePayrollCronJob.js';
 
-const vol = Volume.fromJSON({
-    './jobs.json': '[]',
-    'cron-jobs': {},
-}, '/app');
+// Define variables in the outer scope
+const vol = Volume.fromJSON({});
+const fs = vol;
+let mockBree, mockGetBree, payrollData, accountId;
 
 describe('schedulePayrollCronJob', () => {
-    it('creates and starts a job', async () => {
-        const mockBree = {
+    beforeAll(async () => {
+        mockBree = {
             add: jest.fn(),
             start: jest.fn(),
         };
 
-        const mockGetBree = jest.fn().mockReturnValue(mockBree);
+        mockGetBree = jest.fn().mockReturnValue(mockBree);
 
-        const payrollData = {
+        payrollData = {
             end_date: '2023-12-12T12:12:12Z',
             net_pay: '1200.00',
         };
 
-        const accountId = 1;
+        accountId = 1;
 
-        await schedulePayrollCronJob(mockGetBree, payrollData, accountId);
+        await schedulePayrollCronJob(mockGetBree, fs, payrollData, accountId);
+    });
 
+    it('creates and starts a job', async () => {
         expect(mockBree.add).toBeCalledTimes(1);
+
         // Expect the job name to include "payroll-" prefix but it's hard to predict the full uuid, we can check if it starts with the prefix
         expect(mockBree.start.mock.calls[0][0]).toContain('payroll-');
+    });
 
+    it('writes to the jobs.json file', async () => {
         // Verify that the jobs.json file was written to correctly
         const jobs = JSON.parse(vol.readFileSync('/app/jobs.json', 'utf-8'));
         expect(jobs).toHaveLength(1);
