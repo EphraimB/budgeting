0a0525bbc0f7c551fb2a1d6f1fe10db375a12fc7
Ephraim Becker <emb16@outlook.com>
2023-06-06 15:09:07 -0400

    Refactored payroll dates. #58

--- controllers/payrollDatesController.js
+++ controllers/payrollDatesController.js
@@ -1,6 +1,6 @@
-import pool from '../config/db.js';
 import { payrollQueries } from '../models/queryData.js';
 import { getPayrolls } from '../getPayrolls.js';
+import { handleError, executeQuery } from '../utils/helperFunctions.js';
 
 const payrollDatesParse = payrollDate => ({
     payroll_date_id: parseInt(payrollDate.payroll_date_id),
@@ -9,85 +9,85 @@ const payrollDatesParse = payrollDate => ({
 });
 
 // Get payroll dates
-export const getPayrollDates = (request, response) => {
-    const { employee_id, id } = request.query;
-    const query = id ? payrollQueries.getPayrollDate : payrollQueries.getPayrollDates;
-    const params = id ? [employee_id, id] : [employee_id];
+export const getPayrollDates = async (request, response) => {
+    try {
+        const { employee_id, id } = request.query;
+        const query = id ? payrollQueries.getPayrollDate : payrollQueries.getPayrollDates;
+        const params = id ? [employee_id, id] : [employee_id];
 
-    pool.query(query, params, (error, results) => {
-        if (error) {
-            return response.status(400).send({ errors: { msg: 'Error getting payroll dates', param: null, location: 'query' } });
-        }
+        const results = await executeQuery(query, params);
 
-        // Parse the data to the correct format and return an object
-        const payrollDates = results.rows.map(payrollDate => payrollDatesParse(payrollDate));
+        // Parse the data to correct format and return an object
+        const payrollDates = results.map(payrollDate => payrollDatesParse(payrollDate));
 
         const returnObj = {
             employee_id: parseInt(employee_id),
             payroll_dates: payrollDates,
         };
         response.status(200).send(returnObj);
-    });
+    } catch (error) {
+        handleError(response, 'Error getting payroll dates');
+    }
 };
 
 // Create payroll date
-export const createPayrollDate = (request, response) => {
-    const { employee_id, start_day, end_day } = request.body;
+export const createPayrollDate = async (request, response) => {
+    try {
+        const { employee_id, start_day, end_day } = request.body;
 
-    pool.query(payrollQueries.createPayrollDate, [employee_id, start_day, end_day], (error, results) => {
-        if (error) {
-            return response.status(400).send({ errors: { "msg": "Error creating payroll date", "param": null, "location": "query" } });
-        }
+        const results = await executeQuery(payrollQueries.createPayrollDate, [employee_id, start_day, end_day]);
 
-        getPayrolls(employee_id);
+        await getPayrolls(employee_id);
 
         // Parse the data to correct format and return an object
-        const payrollDates = results.rows.map(payrollDate => payrollDatesParse(payrollDate));
+        const payrollDates = results.map(payrollDate => payrollDatesParse(payrollDate));
 
         const returnObj = {
             employee_id: parseInt(employee_id),
             payroll_date: payrollDates,
-        }
+        };
         response.status(201).send(returnObj);
-    });
+    } catch (error) {
+        handleError(response, 'Error creating payroll date');
+    }
 };
 
 // Update payroll date
-export const updatePayrollDate = (request, response) => {
-    const { id } = request.params;
-    const { employee_id, start_day, end_day } = request.body;
+export const updatePayrollDate = async (request, response) => {
+    try {
+        const { id } = request.params;
+        const { employee_id, start_day, end_day } = request.body;
 
-    pool.query(payrollQueries.updatePayrollDate, [start_day, end_day, id], (error, results) => {
-        if (error) {
-            return response.status(400).send({ errors: { "msg": "Error updating payroll date", "param": null, "location": "query" } });
-        }
+        const results = await executeQuery(payrollQueries.updatePayrollDate, [start_day, end_day, id]);
 
-        getPayrolls(employee_id);
+        await getPayrolls(employee_id);
 
         // Parse the data to correct format and return an object
-        const payrollDates = results.rows.map(payrollDate => payrollDatesParse(payrollDate));
+        const payrollDates = results.map(payrollDate => payrollDatesParse(payrollDate));
 
         const returnObj = {
             employee_id: parseInt(employee_id),
             payroll_date: payrollDates,
-        }
+        };
 
         response.status(200).send(returnObj);
-    });
+    } catch (error) {
+        handleError(response, 'Error updating payroll date');
+    }
 };
 
 // Delete payroll date
-export const deletePayrollDate = (request, response) => {
-    const { employee_id } = request.query;
-    const { id } = request.params;
+export const deletePayrollDate = async (request, response) => {
+    try {
+        const { employee_id } = request.query;
+        const { id } = request.params;
 
-    pool.query(payrollQueries.deletePayrollDate, [id], (error, results) => {
-        if (error) {
-            return response.status(400).send({ errors: { "msg": "Error deleting payroll date", "param": null, "location": "query" } });
-        }
+        await executeQuery(payrollQueries.deletePayrollDate, [id]);
 
-        getPayrolls(employee_id);
+        await getPayrolls(employee_id);
 
         response.status(200).send("Successfully deleted payroll date");
-    });
+    } catch (error) {
+        handleError(response, 'Error deleting payroll date');
+    }
 };
