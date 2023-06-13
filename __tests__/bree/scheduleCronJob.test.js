6ac54b021112071f6405a408213dd46a60b299e4
Ephraim Becker <emb16@outlook.com>
2023-06-12 20:53:47 -0400

    Added another test.

--- __tests__/bree/scheduleCronJob.test.js
+++ __tests__/bree/scheduleCronJob.test.js
@@ -1,14 +1,6 @@
 import { jest } from '@jest/globals';
 import { Volume } from 'memfs';
 
-const jobDetails = {
-    account_id: 1,
-    amount: 500.00,
-    description: 'Testing',
-    begin_date: '2021-07-19T13:30',
-    frequency_type: 2
-};
-
 const vol = Volume.fromJSON({
     './jobs.json': '[]',
     'cron-jobs/jobs.js': '',
@@ -29,11 +21,37 @@ const scheduleCronJobModule = await import('../../jobs/scheduleCronJob.js');
 const scheduleCronJob = scheduleCronJobModule.default;
 
 describe('scheduleCronJob', () => {
-    it('schedules a new job successfully', async () => {
+    it('gets back the cron date', async () => {
+        const jobDetails = {
+            account_id: 1,
+            amount: 500.00,
+            description: 'Testing',
+            begin_date: '2021-07-19T13:30',
+            frequency_type: 2
+        };
+
         const { cronDate, uniqueId } = await scheduleCronJob(jobDetails, '/app/cron-jobs/jobs.js', '/app/jobs.json');
 
         // Specify expected values here
         const expectedCronDate = '30 13 19 */1 *'; // Provide the expected cronDate
+
+        expect(cronDate).toBe(expectedCronDate);
+    });
+
+    it('Gets back a cron date with a unique frequency', async () => {
+        const jobDetailsUnique = {
+            account_id: 1,
+            amount: 500.00,
+            description: 'Testing',
+            begin_date: '2021-07-19T13:30',
+            frequency_type: 1,
+            frequency_type_variable: 2
+        };
+
+        const { cronDate, uniqueId } = await scheduleCronJob(jobDetailsUnique, '/app/cron-jobs/jobs.js', '/app/jobs.json');
+
+        // Specify expected values here
+        const expectedCronDate = '30 13 */72 * *'; // Provide the expected cronDate
 
         expect(cronDate).toBe(expectedCronDate);
     });
