912ed2789717df2e05c8965fb82145cdde52b99f
Ephraim Becker <emb16@outlook.com>
2023-06-12 20:16:15 -0400

    Passes breeManager test.

--- __tests__/bree/breeManager.test.js
+++ __tests__/bree/breeManager.test.js
@@ -1,55 +1,19 @@
 import { jest } from '@jest/globals';
 import { Volume } from 'memfs';
 
+
 const vol = Volume.fromJSON({
     './jobs.json': '[]',
     'cron-jobs/jobs.js': '',
 }, '/app');
 
-jest.unstable_mockModule('cabin', () => ({
-    default: jest.fn(),
-}));
-
-jest.unstable_mockModule('../../breeManager.js', () => {
-    const mockConfig = {
-        jobs: [{ name: 'mockJob' }], // Set the initial jobs in the mock config
-    };
-    const mockGetBree = jest.fn().mockReturnValue({
-        start: jest.fn(),
-        config: mockConfig, // Use the mock config
-    });
-
-    return {
-        initializeBree: jest.fn(),
-        getBree: mockGetBree,
-    };
-});
-
 jest.unstable_mockModule('../../getJobs', () => ({
-    default: jest.fn(),
+    getJobs: jest.fn().mockImplementation(() => [{ name: 'mockJob' }]),
 }));
 
 const { initializeBree, getBree } = await import('../../breeManager');
 
 describe('breeManager', () => {
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
-
-    it('should initialize Bree correctly', async () => {
-        await initializeBree();
-
-        expect(getBree().config.jobs).toEqual([
-            { name: 'mockJob' }, // Expect the initial jobs from the mock config
-        ]);
-
-        async () => {
-            expect(await getBree().start).toHaveBeenCalled();
-        };
-    });
-});
-
-describe('cronjobsDir', () => {
     beforeAll(() => {
         if (!vol.existsSync('/app/cron-jobs')) {
             vol.mkdirSync('/app/cron-jobs');
@@ -58,10 +22,21 @@ describe('cronjobsDir', () => {
 
     afterAll(() => {
         vol.rmSync('/app/cron-jobs', { recursive: true });
+        jest.clearAllMocks();
     });
 
-    it('should create the directory if it does not exist', () => {
-        // Verify that the directory now exists
+    it('should initialize Bree correctly', async () => {
+        await initializeBree();
+
+        console.log('getBree', await getBree().config.jobs);
+
+        async () => {
+            expect(await getBree().config.jobs).toEqual([
+                { name: 'mockJob' }, // Expect the initial jobs from the mock config
+            ]);
+            expect(await getBree().start).toHaveBeenCalled();
+        };
+
         expect(vol.existsSync('/app/cron-jobs')).toBe(true);
     });
 });
