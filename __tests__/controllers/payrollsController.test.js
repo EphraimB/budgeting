4062f6da7c096f5f32c4ec4df7d159cb0d73584c
Ephraim Becker <emb16@outlook.com>
2023-06-16 12:06:59 -0400

    Passed the getPayrolls controller. #46

---
+++ __tests__/controllers/payrollsController.test.js
@@ -0,0 +1,70 @@
+import { jest } from '@jest/globals';
+import { payrolls } from '../../models/mockData.js';
+
+// Mock request and response
+let mockRequest;
+let mockResponse;
+
+beforeEach(() => {
+    mockRequest = {};
+    mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+        send: jest.fn(),
+    };
+});
+
+afterEach(() => {
+    jest.resetModules();
+});
+
+// Helper function to generate mock module
+const mockModule = (executeQueryValue, errorMessage) => {
+    jest.unstable_mockModule('../../utils/helperFunctions.js', () => ({
+        executeQuery: errorMessage
+            ? jest.fn().mockRejectedValue(new Error(errorMessage))
+            : jest.fn().mockResolvedValue(executeQueryValue),
+        handleError: jest.fn((res, message) => {
+            res.status(400).json({ message });
+        }),
+    }));
+};
+
+describe('GET /api/payrolls', () => {
+    it('should respond with an array of payrolls', async () => {
+        // Arrange
+        mockModule(payrolls);
+
+        mockRequest.query = { employee_id: 1 };
+
+        const { getPayrolls } = await import('../../controllers/payrollsController.js');
+
+        // Call the function with the mock request and response
+        await getPayrolls(mockRequest, mockResponse);
+
+        const expectedPayrolls = {
+            employee_id: 1,
+            payrolls
+        }
+
+        // Assert
+        expect(mockResponse.status).toHaveBeenCalledWith(200);
+        expect(mockResponse.json).toHaveBeenCalledWith(expectedPayrolls);
+    });
+
+    it('should respond with an error message', async () => {
+        // Arrange
+        mockModule(null, 'Error getting payrolls');
+
+        mockRequest.query = { employee_id: 1 };
+
+        const { getPayrolls } = await import('../../controllers/payrollsController.js');
+
+        // Call the function with the mock request and response
+        await getPayrolls(mockRequest, mockResponse);
+
+        // Assert
+        expect(mockResponse.status).toHaveBeenCalledWith(400);
+        expect(mockResponse.json).toHaveBeenCalledWith({ message: 'Error getting payrolls' });
+    });
+});
