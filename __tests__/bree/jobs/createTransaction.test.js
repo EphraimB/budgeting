a9b222aee3a8fd857a50d1876d77fdac19b735d7
Ephraim Becker <emb16@outlook.com>
2023-06-20 17:27:23 -0400

    createTransaction passes test. #46

--- __tests__/bree/jobs/createTransaction.test.js
+++ __tests__/bree/jobs/createTransaction.test.js
@@ -1,50 +1,37 @@
 import { jest } from '@jest/globals';
 
-jest.unstable_mockModule('../../../config/db.js', () => {
-    return {
-        default: {
-            query: jest.fn().mockReturnValue(Promise.resolve({ rows: [{}] })),
-        },
-    }
-});
+jest.unstable_mockModule('../../../config/db.js', () => ({
+    default: {
+        query: jest.fn()
+            .mockImplementation((query, values, callback) => {
+                if (values.includes(-1000)) {
+                    // Simulate error case for negative amount transaction
+                    callback(new Error('Transaction failed'));
+                } else {
+                    // Simulate success case
+                    callback(null, { rows: [{}] });
+                }
+            }),
+    },
+}));
 
-jest.unstable_mockModule('../../../models/queryData.js', () => {
-    return {
-        transactionQueries: {
-            createTransaction: 'INSERT INTO transactions (account_id, amount, description) VALUES ($1, $2, $3) RETURNING *',
-        },
-    };
-});
+jest.unstable_mockModule('../../../models/queryData.js', () => ({
+    transactionQueries: {
+        createTransaction: 'INSERT INTO transactions (account_id, amount, description) VALUES ($1, $2, $3) RETURNING *',
+    },
+}));
 
 const { createTransaction } = await import('../../../bree/jobs/createTransaction.js');
 
 describe('createTransaction', () => {
-    it('should create two transactions successfully', async () => {
-        const result = await createTransaction(1, 1000, 'Test Transaction');
 
+    it('should create a transaction successfully', async () => {
+        const result = await createTransaction(1, 1000, 'Test Transaction');
         expect(result).toEqual([{}]); // adjust this expectation as needed
     });
 
-    it('should throw an error if the first transaction fails', async () => {
-        const transactionData = {
-            account_id: 1,
-            amount: 1000,
-            description: 'Test Transaction',
-            destination_account_id: 2,
-        };
-
-        const error = new Error('First transaction failed');
-        pool.query.mockImplementation(() => Promise.reject(error));
-
-        await expect(
-            createTransaction(
-                transactionData.account_id,
-                transactionData.amount,
-                transactionData.description,
-                transactionData.destination_account_id
-            )
-        ).rejects.toThrow(error);
+    it('should throw an error if the transaction fails', async () => {
+        await expect(createTransaction(2, -1000, 'Test Transaction'))
+            .rejects.toThrow('Transaction failed');
     });
-
-    // ... more test cases ...
 });
