c84371f2a79b126a2b0e25bb23c1b1c65699faf7
Ephraim Becker <emb16@outlook.com>
2023-06-23 13:05:18 -0400

    frequency_day_of_week results in infinite loop.

--- __tests__/generation/generateExpenses.test.js
+++ __tests__/generation/generateExpenses.test.js
@@ -105,7 +105,7 @@ describe('Test generateMonthlyExpenses', () => {
         const toDate = new Date();
         toDate.setMonth(toDate.getMonth() + 7);
         toDate.setDate(toDate.getDate() + 1);
-        
+
         const fromDate = new Date();
         fromDate.setMonth(fromDate.getMonth() + 5);
         fromDate.setDate(fromDate.getDate() + 1);
@@ -125,5 +125,39 @@ describe('Test generateMonthlyExpenses', () => {
         expect(transactions[0].description).toBe(expense.expense_description);
         expect(transactions[0].amount).toBe(-expense.expense_amount);
         expect(expectedEndDate.toISOString().slice(0, 10)).toBe(new Date(toBeEndDate).toISOString().slice(0, 10));
+    });
+
+    it('Should generate monthly expenses correctly when the frequency day of week is set', () => {
+        // Preparing the test data
+        const transactions = [];
+        const skippedTransactions = [];
+        const expense = {
+            expense_begin_date: new Date().setDate(new Date().getDate() + 1),
+            expense_title: "Test expense",
+            expense_description: "Test description",
+            expense_amount: 100,
+            frequency_day_of_week: 1
+        };
+        const toDate = new Date();
+        toDate.setMonth(toDate.getMonth() + 5);
+        toDate.setDate(toDate.getDate() + 1);
+
+        const fromDate = new Date();
+
+        // Running the function
+        generateMonthlyExpenses(transactions, skippedTransactions, expense, toDate, fromDate);
+
+        const expectedEndDate = new Date(transactions[transactions.length - 1].date);
+        const toBeEndDate = new Date();
+        toBeEndDate.setDate(new Date().getDate() + 1);
+        toBeEndDate.setMonth(new Date().getMonth() + 5);
+
+        // Checking the results
+        expect(transactions.length).toBe(6);
+        expect(skippedTransactions.length).toBe(0);
+        expect(transactions[0].title).toBe(expense.expense_title);
+        expect(transactions[0].description).toBe(expense.expense_description);
+        expect(transactions[0].amount).toBe(-expense.expense_amount);
+        expect(expectedEndDate.toISOString().slice(0, 10)).toBe(toBeEndDate.toISOString().slice(0, 10));
     });
 });
