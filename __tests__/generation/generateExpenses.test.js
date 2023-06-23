3015acf2675d8798c4416f6b144ae42ad2a973cf
Ephraim Becker <emb16@outlook.com>
2023-06-23 12:22:16 -0400

    Passes second test. #46

--- __tests__/generation/generateExpenses.test.js
+++ __tests__/generation/generateExpenses.test.js
@@ -28,5 +28,33 @@ describe('Test generateDailyExpenses', () => {
         expect(expectedEndDate.toISOString().slice(0,10)).toBe(new Date(new Date().setDate(new Date().getDate() + 5)).toISOString().slice(0,10));
     });
 
-    // You can add more test cases to handle different scenarios
+    it('Should generate daily expenses correctly when the expense begin date is in the past', () => {
+        // Preparing the test data
+        const transactions = [];
+        const skippedTransactions = [];
+        const expense = {
+            expense_begin_date: new Date().setDate(new Date().getDate() + 1),
+            expense_title: "Test expense",
+            expense_description: "Test description",
+            expense_amount: 100
+        };
+        const toDate = new Date().setDate(new Date().getDate() + 7);
+        const fromDate = new Date().setDate(new Date().getDate() + 5);
+
+        // Running the function
+        generateDailyExpenses(transactions, skippedTransactions, expense, toDate, fromDate);
+
+        console.log(transactions);
+        console.log(skippedTransactions);
+
+        const expectedEndDate = new Date(transactions[transactions.length - 1].date);
+
+        // Checking the results
+        expect(transactions.length).toBe(3);
+        expect(skippedTransactions.length).toBe(4);
+        expect(transactions[0].title).toBe(expense.expense_title);
+        expect(transactions[0].description).toBe(expense.expense_description);
+        expect(transactions[0].amount).toBe(-expense.expense_amount);
+        expect(expectedEndDate.toISOString().slice(0,10)).toBe(new Date(new Date().setDate(new Date().getDate() + 7)).toISOString().slice(0,10));
+    });
 });
--- generation/generateExpenses.js
+++ generation/generateExpenses.js
@@ -26,7 +26,7 @@ const generateExpenses = (transactions, skippedTransactions, expense, toDate, fr
         };
 
         if (expenseDate <= new Date()) {
-            
+
         } else if (fromDate > expenseDate) {
             skippedTransactions.push(newTransaction);
         } else {
