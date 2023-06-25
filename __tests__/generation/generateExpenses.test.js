4c4d041b69c1df66aeac44f36bd1d75d6d4c5e1d
Ephraim Becker <emb16@outlook.com>
2023-06-25 11:18:10 -0400

    Passes all tests. #46

--- __tests__/generation/generateExpenses.test.js
+++ __tests__/generation/generateExpenses.test.js
@@ -1,96 +1,96 @@
 import { generateDailyExpenses, generateMonthlyExpenses } from '../../generation/generateExpenses';
 
-// describe('Test generateDailyExpenses', () => {
-//     it('Should generate daily expenses correctly', () => {
-//         // Preparing the test data
-//         const transactions = [];
-//         const skippedTransactions = [];
-//         const expense = {
-//             expense_begin_date: new Date().setDate(new Date().getDate() + 1),
-//             expense_title: "Test expense",
-//             expense_description: "Test description",
-//             expense_amount: 100
-//         };
-//         const toDate = new Date().setDate(new Date().getDate() + 5);
-//         const fromDate = new Date();
+describe('Test generateDailyExpenses', () => {
+    it('Should generate daily expenses correctly', () => {
+        // Preparing the test data
+        const transactions = [];
+        const skippedTransactions = [];
+        const expense = {
+            expense_begin_date: new Date().setDate(new Date().getDate() + 1),
+            expense_title: "Test expense",
+            expense_description: "Test description",
+            expense_amount: 100
+        };
+        const toDate = new Date().setDate(new Date().getDate() + 5);
+        const fromDate = new Date();
 
-//         // Running the function
-//         generateDailyExpenses(transactions, skippedTransactions, expense, toDate, fromDate);
+        // Running the function
+        generateDailyExpenses(transactions, skippedTransactions, expense, toDate, fromDate);
 
-//         const expectedEndDate = new Date(transactions[transactions.length - 1].date);
+        const expectedEndDate = new Date(transactions[transactions.length - 1].date);
 
-//         // Checking the results
-//         expect(transactions.length).toBe(5);
-//         expect(skippedTransactions.length).toBe(0);
-//         expect(transactions[0].title).toBe(expense.expense_title);
-//         expect(transactions[0].description).toBe(expense.expense_description);
-//         expect(transactions[0].amount).toBe(-expense.expense_amount);
-//         expect(expectedEndDate.toISOString().slice(0, 10)).toBe(new Date(new Date().setDate(new Date().getDate() + 5)).toISOString().slice(0, 10));
-//     });
+        // Checking the results
+        expect(transactions.length).toBe(5);
+        expect(skippedTransactions.length).toBe(0);
+        expect(transactions[0].title).toBe(expense.expense_title);
+        expect(transactions[0].description).toBe(expense.expense_description);
+        expect(transactions[0].amount).toBe(-expense.expense_amount);
+        expect(expectedEndDate.toISOString().slice(0, 10)).toBe(new Date(new Date().setDate(new Date().getDate() + 5)).toISOString().slice(0, 10));
+    });
 
-//     it('Should generate daily expenses correctly when the expense begin date is less than the from date', () => {
-//         // Preparing the test data
-//         const transactions = [];
-//         const skippedTransactions = [];
-//         const expense = {
-//             expense_begin_date: new Date().setDate(new Date().getDate() + 1),
-//             expense_title: "Test expense",
-//             expense_description: "Test description",
-//             expense_amount: 100
-//         };
-//         const toDate = new Date().setDate(new Date().getDate() + 7);
-//         const fromDate = new Date().setDate(new Date().getDate() + 5);
+    it('Should generate daily expenses correctly when the expense begin date is less than the from date', () => {
+        // Preparing the test data
+        const transactions = [];
+        const skippedTransactions = [];
+        const expense = {
+            expense_begin_date: new Date().setDate(new Date().getDate() + 1),
+            expense_title: "Test expense",
+            expense_description: "Test description",
+            expense_amount: 100
+        };
+        const toDate = new Date().setDate(new Date().getDate() + 7);
+        const fromDate = new Date().setDate(new Date().getDate() + 5);
 
-//         // Running the function
-//         generateDailyExpenses(transactions, skippedTransactions, expense, toDate, fromDate);
+        // Running the function
+        generateDailyExpenses(transactions, skippedTransactions, expense, toDate, fromDate);
 
-//         const expectedEndDate = new Date(transactions[transactions.length - 1].date);
-//         const toBeEndDate = new Date();
-//         toBeEndDate.setDate(toBeEndDate.getDate() + 7);
+        const expectedEndDate = new Date(transactions[transactions.length - 1].date);
+        const toBeEndDate = new Date();
+        toBeEndDate.setDate(toBeEndDate.getDate() + 7);
 
-//         // Checking the results
-//         expect(transactions.length).toBe(3);
-//         expect(skippedTransactions.length).toBe(4);
-//         expect(transactions[0].title).toBe(expense.expense_title);
-//         expect(transactions[0].description).toBe(expense.expense_description);
-//         expect(transactions[0].amount).toBe(-expense.expense_amount);
-//         expect(expectedEndDate.toISOString().slice(0, 10)).toBe(new Date(toBeEndDate).toISOString().slice(0, 10));
-//     });
-// });
+        // Checking the results
+        expect(transactions.length).toBe(3);
+        expect(skippedTransactions.length).toBe(4);
+        expect(transactions[0].title).toBe(expense.expense_title);
+        expect(transactions[0].description).toBe(expense.expense_description);
+        expect(transactions[0].amount).toBe(-expense.expense_amount);
+        expect(expectedEndDate.toISOString().slice(0, 10)).toBe(new Date(toBeEndDate).toISOString().slice(0, 10));
+    });
+});
 
 describe('Test generateMonthlyExpenses', () => {
-    // it('Should generate monthly expenses correctly', () => {
-    //     // Preparing the test data
-    //     const transactions = [];
-    //     const skippedTransactions = [];
-    //     const expense = {
-    //         expense_begin_date: new Date().setDate(new Date().getDate() + 1),
-    //         expense_title: "Test expense",
-    //         expense_description: "Test description",
-    //         expense_amount: 100
-    //     };
-    //     const toDate = new Date();
-    //     toDate.setMonth(toDate.getMonth() + 5);
-    //     toDate.setDate(toDate.getDate() + 1);
+    it('Should generate monthly expenses correctly', () => {
+        // Preparing the test data
+        const transactions = [];
+        const skippedTransactions = [];
+        const expense = {
+            expense_begin_date: new Date().setDate(new Date().getDate() + 1),
+            expense_title: "Test expense",
+            expense_description: "Test description",
+            expense_amount: 100
+        };
+        const toDate = new Date();
+        toDate.setMonth(toDate.getMonth() + 5);
+        toDate.setDate(toDate.getDate() + 1);
 
-    //     const fromDate = new Date();
+        const fromDate = new Date();
 
-    //     // Running the function
-    //     generateMonthlyExpenses(transactions, skippedTransactions, expense, toDate, fromDate);
+        // Running the function
+        generateMonthlyExpenses(transactions, skippedTransactions, expense, toDate, fromDate);
 
-    //     const expectedEndDate = new Date(transactions[transactions.length - 1].date);
-    //     const toBeEndDate = new Date();
-    //     toBeEndDate.setDate(new Date().getDate() + 1);
-    //     toBeEndDate.setMonth(new Date().getMonth() + 5);
+        const expectedEndDate = new Date(transactions[transactions.length - 1].date);
+        const toBeEndDate = new Date();
+        toBeEndDate.setDate(new Date().getDate() + 1);
+        toBeEndDate.setMonth(new Date().getMonth() + 5);
 
-    //     // Checking the results
-    //     expect(transactions.length).toBe(6);
-    //     expect(skippedTransactions.length).toBe(0);
-    //     expect(transactions[0].title).toBe(expense.expense_title);
-    //     expect(transactions[0].description).toBe(expense.expense_description);
-    //     expect(transactions[0].amount).toBe(-expense.expense_amount);
-    //     expect(expectedEndDate.toISOString().slice(0, 10)).toBe(toBeEndDate.toISOString().slice(0, 10));
-    // });
+        // Checking the results
+        expect(transactions.length).toBe(6);
+        expect(skippedTransactions.length).toBe(0);
+        expect(transactions[0].title).toBe(expense.expense_title);
+        expect(transactions[0].description).toBe(expense.expense_description);
+        expect(transactions[0].amount).toBe(-expense.expense_amount);
+        expect(expectedEndDate.toISOString().slice(0, 10)).toBe(toBeEndDate.toISOString().slice(0, 10));
+    });
 
     it('Should generate monthly expenses correctly when the expense begin date is less than the from date', () => {
         // Preparing the test data
@@ -129,51 +129,51 @@ describe('Test generateMonthlyExpenses', () => {
         expect(expectedEndDate.toISOString().slice(0, 10)).toBe(new Date(toBeEndDate).toISOString().slice(0, 10));
     });
 
-    // it('Should generate monthly expenses correctly when the frequency day of week is set', () => {
-    //     // Preparing the test data
-    //     const transactions = [];
-    //     const skippedTransactions = [];
-    //     const expense = {
-    //         expense_begin_date: new Date().setDate(new Date().getDate() + 1),
-    //         expense_title: "Test expense",
-    //         expense_description: "Test description",
-    //         expense_amount: 150,
-    //         frequency_day_of_week: 2,
-    //     };
-    //     const toDate = new Date();
-    //     toDate.setMonth(toDate.getMonth() + 5);
-    //     toDate.setDate(toDate.getDate() + 1);
+    it('Should generate monthly expenses correctly when the frequency day of week is set', () => {
+        // Preparing the test data
+        const transactions = [];
+        const skippedTransactions = [];
+        const expense = {
+            expense_begin_date: new Date().setDate(new Date().getDate() + 1),
+            expense_title: "Test expense",
+            expense_description: "Test description",
+            expense_amount: 150,
+            frequency_day_of_week: 2,
+        };
+        const toDate = new Date();
+        toDate.setMonth(toDate.getMonth() + 5);
+        toDate.setDate(toDate.getDate() + 1);
 
-    //     const fromDate = new Date();
+        const fromDate = new Date();
 
-    //     // Running the function
-    //     generateMonthlyExpenses(transactions, skippedTransactions, expense, toDate, fromDate);
+        // Running the function
+        generateMonthlyExpenses(transactions, skippedTransactions, expense, toDate, fromDate);
 
-    //     const expectedEndDate = new Date(transactions[transactions.length - 1].date);
-    //     const toBeEndDate = new Date();
+        const expectedEndDate = new Date(transactions[transactions.length - 1].date);
+        const toBeEndDate = new Date();
 
-    //     // advance by 5 months
-    //     toBeEndDate.setMonth(toBeEndDate.getMonth() + 4);
+        // advance by 5 months
+        toBeEndDate.setMonth(toBeEndDate.getMonth() + 4);
 
-    //     // days of the week from 0 (Sunday) to 6 (Saturday)
-    //     const TUESDAY = 2;
+        // days of the week from 0 (Sunday) to 6 (Saturday)
+        const TUESDAY = 2;
 
-    //     // calculate the number of days to add to get to the next Tuesday
-    //     let daysUntilNextTuesday = (7 + TUESDAY - toBeEndDate.getDay()) % 7;
+        // calculate the number of days to add to get to the next Tuesday
+        let daysUntilNextTuesday = (7 + TUESDAY - toBeEndDate.getDay()) % 7;
 
-    //     // if today is already Tuesday, move to next week's Tuesday
-    //     if (daysUntilNextTuesday === 0) {
-    //         daysUntilNextTuesday += 7;
-    //     }
+        // if today is already Tuesday, move to next week's Tuesday
+        if (daysUntilNextTuesday === 0) {
+            daysUntilNextTuesday += 7;
+        }
 
-    //     toBeEndDate.setDate(toBeEndDate.getDate() + daysUntilNextTuesday);
+        toBeEndDate.setDate(toBeEndDate.getDate() + daysUntilNextTuesday);
 
-    //     // Checking the results
-    //     expect(transactions.length).toBe(5);
-    //     expect(skippedTransactions.length).toBe(0);
-    //     expect(transactions[0].title).toBe(expense.expense_title);
-    //     expect(transactions[0].description).toBe(expense.expense_description);
-    //     expect(transactions[0].amount).toBe(-expense.expense_amount);
-    //     expect(expectedEndDate.toISOString().slice(0, 10)).toBe(toBeEndDate.toISOString().slice(0, 10));
-    // });
+        // Checking the results
+        expect(transactions.length).toBe(5);
+        expect(skippedTransactions.length).toBe(0);
+        expect(transactions[0].title).toBe(expense.expense_title);
+        expect(transactions[0].description).toBe(expense.expense_description);
+        expect(transactions[0].amount).toBe(-expense.expense_amount);
+        expect(expectedEndDate.toISOString().slice(0, 10)).toBe(toBeEndDate.toISOString().slice(0, 10));
+    });
 });
