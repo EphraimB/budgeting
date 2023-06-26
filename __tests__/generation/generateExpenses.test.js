7581172570e71c95906187885867b0a9982ea0ad
Ephraim Becker <emb16@outlook.com>
2023-06-26 10:57:23 -0400

    Passes day of week generate year test. #46

--- __tests__/generation/generateExpenses.test.js
+++ __tests__/generation/generateExpenses.test.js
@@ -462,4 +462,41 @@ describe('generateYearlyExpenses', () => {
         expect(transactions[0].amount).toBe(-expense.expense_amount);
         expect(expectedEndDate.toISOString().slice(0, 10)).toBe(new Date('2025-01-02').toISOString().slice(0, 10));
     });
+
+    it('Should generate monthly expenses correctly when the frequency day of week is set', () => {
+        // Preparing the test data
+        const transactions = [];
+        const skippedTransactions = [];
+        const expense = {
+            expense_begin_date: new Date('2020-01-02'),
+            expense_title: "Test expense",
+            expense_description: "Test description",
+            expense_amount: 150,
+            frequency_day_of_week: 2,
+        };
+        const toDate = new Date('2023-01-10');
+        const fromDate = new Date('2020-01-01');
+
+        // Running the function
+        generateYearlyExpenses(transactions, skippedTransactions, expense, toDate, fromDate);
+
+        const expectedEndDate = new Date(transactions[transactions.length - 1].date);
+        const toBeEndDate = new Date('2023-01-02');
+
+        // days of the week from 0 (Sunday) to 6 (Saturday)
+        const TUESDAY = 2;
+
+        // calculate the number of days to add to get to the next Tuesday
+        let daysUntilNextTuesday = (7 + TUESDAY - toBeEndDate.getDay()) % 7;
+
+        toBeEndDate.setDate(toBeEndDate.getDate() + daysUntilNextTuesday);
+
+        // Checking the results
+        expect(transactions.length).toBe(4);
+        expect(skippedTransactions.length).toBe(0);
+        expect(transactions[0].title).toBe(expense.expense_title);
+        expect(transactions[0].description).toBe(expense.expense_description);
+        expect(transactions[0].amount).toBe(-expense.expense_amount);
+        expect(expectedEndDate.toISOString().slice(0, 10)).toBe(toBeEndDate.toISOString().slice(0, 10));
+    });
 });
