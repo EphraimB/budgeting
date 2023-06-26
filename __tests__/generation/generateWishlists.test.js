986d134d93af87a3329d60de7cca7ef043914070
Ephraim Becker <emb16@outlook.com>
2023-06-26 12:42:15 -0400

    Passes both tests. #46

--- __tests__/generation/generateWishlists.test.js
+++ __tests__/generation/generateWishlists.test.js
@@ -10,7 +10,35 @@ afterAll(() => {
 });
 
 describe('generateWishlists', () => {
-    test('should generate wishlist transaction', () => {
+    it('Should generate wishlist transaction', () => {
+        const transactions = [
+            { date: new Date('2023-07-01'), amount: 200, balance: 500 },
+            { date: new Date('2023-08-01'), amount: 200, balance: 700 },
+            { date: new Date('2023-09-01'), amount: 200, balance: 900 },
+        ];
+        const skippedTransactions = [];
+        const wishlist = {
+            wishlist_amount: '150',
+            wishlist_title: 'New TV',
+            wishlist_description: 'For watching movies',
+        };
+        const fromDate = new Date('2023-06-01');
+
+        generateWishlists(transactions, skippedTransactions, wishlist, fromDate);
+
+        expect(transactions).toHaveLength(4);
+        expect(skippedTransactions).toHaveLength(0);
+
+        const wishlistTransaction = transactions.find(
+            (t) => t.title === wishlist.wishlist_title
+        );
+
+        expect(wishlistTransaction).toBeDefined();
+        expect(wishlistTransaction.amount).toBe(-150);
+        expect(wishlistTransaction.date).toEqual(new Date('2023-07-01'));
+    });
+
+    it('Should generate wishlist transaction with a date available set', () => {
         const transactions = [
             { date: new Date('2023-07-01'), amount: 200, balance: 500 },
             { date: new Date('2023-08-01'), amount: 200, balance: 700 },
