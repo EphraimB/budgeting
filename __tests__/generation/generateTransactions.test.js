e21eea7c47e12895e641a57440c8deec1078166b
Ephraim Becker <emb16@outlook.com>
2023-06-26 17:18:57 -0400

    Commented out code for now.

--- __tests__/generation/generateTransactions.test.js
+++ __tests__/generation/generateTransactions.test.js
@@ -3,13 +3,6 @@ import generateTransactions from '../../generation/generateTransactions';
 import { transactions, expenses, payrolls, loans, transfers, wishlists } from '../../models/mockData';
 import MockDate from 'mockdate';
 
-jest.unstable_mockModule('../../generation/generateExpenses', () => ({
-    generateDailyExpenses: jest.fn(),
-    generateWeeklyExpenses: jest.fn(),
-    generateMonthlyExpenses: jest.fn(),
-    generateYearlyExpenses: jest.fn(),
-}));
-
 beforeAll(() => {
     MockDate.set('2023-07-01');
 });
@@ -49,4 +42,40 @@ describe('generateTransactions', () => {
 
         expect(mockRequest.currentBalance).toBe(500);
     });
+
+    // it('should make sure that transactions are sorted by date', () => {
+    //     // setup your data
+    //     const mockRequest = {
+    //         query: {
+    //             from_date: '2023-07-01',
+    //             to_date: '2023-08-01',
+    //             account_id: 1,
+    //         },
+    //         currentBalance: 500,
+    //         transaction: transactions.filter(transaction => transaction.account_id === 1),
+    //         expenses: expenses.filter(expense => expense.account_id === 1),
+    //         payrolls: payrolls.filter(payroll => payroll.account_id === 1),
+    //         loans: loans.filter(loan => loan.account_id === 1),
+    //         transfers: transfers.filter(transfer => transfer.account_id === 1),
+    //         wishlists: wishlists.filter(wishlist => wishlist.account_id === 1),
+    //     };
+
+    //     const mockResponse = {};
+    //     const next = jest.fn();
+
+    //     // Call your function with the mock data
+    //     generateTransactions(mockRequest, mockResponse, next);
+
+    //     // assert that next was called
+    //     expect(next).toBeCalled();
+
+    //     expect(mockRequest.transaction).toHaveLength(4);
+
+    //     console.log(mockRequest.transaction);
+
+    //     expect(mockRequest.transaction[0].date).toBe('2023-07-01');
+    //     expect(mockRequest.transaction[1].date).toBe('2023-07-02');
+    //     expect(mockRequest.transaction[2].date).toBe('2023-07-03');
+    //     expect(mockRequest.transaction[3].date).toBe('2023-07-04');
+    // });
 });
