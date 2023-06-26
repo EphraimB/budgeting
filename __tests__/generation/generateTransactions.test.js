a3d11ccb3c092e11a51996d757381a46e32d8b86
Ephraim Becker <emb16@outlook.com>
2023-06-26 17:54:02 -0400

    Passed the last test. #46

--- __tests__/generation/generateTransactions.test.js
+++ __tests__/generation/generateTransactions.test.js
@@ -12,9 +12,13 @@ afterAll(() => {
 });
 
 describe('generateTransactions', () => {
-    it('should process transactions correctly', () => {
+    let mockRequest;
+    let mockResponse;
+    let next;
+
+    beforeEach(() => {
         // setup your data
-        const mockRequest = {
+        mockRequest = {
             query: {
                 from_date: '2023-07-01',
                 to_date: '2023-08-01',
@@ -29,9 +33,11 @@ describe('generateTransactions', () => {
             wishlists: wishlists.filter(wishlist => wishlist.account_id === 1),
         };
 
-        const mockResponse = {};
-        const next = jest.fn();
+        mockResponse = {};
+        next = jest.fn();
+    });
 
+    it('should process transactions correctly', () => {
         // Call your function with the mock data
         generateTransactions(mockRequest, mockResponse, next);
 
@@ -41,41 +47,22 @@ describe('generateTransactions', () => {
         expect(mockRequest.transaction).toHaveLength(4);
 
         expect(mockRequest.currentBalance).toBe(500);
+
+        // assert end state of request object
+        // add checks for any additional properties or state you expect mockRequest to have after generateTransactions
+        expect(mockRequest.expenses).toEqual(expenses.filter(expense => expense.account_id === 1));
+        expect(mockRequest.payrolls).toEqual(payrolls.filter(payroll => payroll.account_id === 1));
     });
 
-    // it('should make sure that transactions are sorted by date', () => {
-    //     // setup your data
-    //     const mockRequest = {
-    //         query: {
-    //             from_date: '2023-07-01',
-    //             to_date: '2023-08-01',
-    //             account_id: 1,
-    //         },
-    //         currentBalance: 500,
-    //         transaction: transactions.filter(transaction => transaction.account_id === 1),
-    //         expenses: expenses.filter(expense => expense.account_id === 1),
-    //         payrolls: payrolls.filter(payroll => payroll.account_id === 1),
-    //         loans: loans.filter(loan => loan.account_id === 1),
-    //         transfers: transfers.filter(transfer => transfer.account_id === 1),
-    //         wishlists: wishlists.filter(wishlist => wishlist.account_id === 1),
-    //     };
+    it('should make sure that transactions are sorted by date', () => {
+        // Call your function with the mock data
+        generateTransactions(mockRequest, mockResponse, next);
 
-    //     const mockResponse = {};
-    //     const next = jest.fn();
+        // assert that next was called
+        expect(next).toBeCalled();
 
-    //     // Call your function with the mock data
-    //     generateTransactions(mockRequest, mockResponse, next);
-
-    //     // assert that next was called
-    //     expect(next).toBeCalled();
-
-    //     expect(mockRequest.transaction).toHaveLength(4);
-
-    //     console.log(mockRequest.transaction);
-
-    //     expect(mockRequest.transaction[0].date).toBe('2023-07-01');
-    //     expect(mockRequest.transaction[1].date).toBe('2023-07-02');
-    //     expect(mockRequest.transaction[2].date).toBe('2023-07-03');
-    //     expect(mockRequest.transaction[3].date).toBe('2023-07-04');
-    // });
+        // assert that transactions are ordered by date
+        const sortedTransactions = [...mockRequest.transaction].sort((a, b) => new Date(a.date) - new Date(b.date));
+        expect(mockRequest.transaction).toEqual(sortedTransactions);
+    });
 });
