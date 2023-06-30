15572cd851fa4b451f22a65089ec749c985f0bde
Ephraim Becker <emb16@outlook.com>
2023-06-30 12:51:52 -0400

    Linted wishlistsController test. #72

--- __tests__/controllers/wishlistsController.test.js
+++ __tests__/controllers/wishlistsController.test.js
@@ -16,7 +16,7 @@ beforeEach(() => {
     mockResponse = {
         status: jest.fn().mockReturnThis(),
         json: jest.fn(),
-        send: jest.fn(),
+        send: jest.fn()
     };
 });
 
@@ -37,7 +37,7 @@ const mockModule = (executeQueryValue, errorMessage) => {
             : jest.fn().mockResolvedValue(executeQueryValue),
         handleError: jest.fn((res, message) => {
             res.status(400).json({ message });
-        }),
+        })
     }));
 };
 
