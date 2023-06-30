62719a2b5ecb018076a276b2a2a19137a5deafed
Ephraim Becker <emb16@outlook.com>
2023-06-30 11:48:25 -0400

    Linted createTransaction. #72

--- bree/jobs/createTransaction.js
+++ bree/jobs/createTransaction.js
@@ -3,7 +3,7 @@ import { transactionQueries } from '../../models/queryData.js';
 
 export const createTransaction = async (account_id, amount, description, destination_account_id) => {
     return new Promise((resolve, reject) => {
-        pool.query(transactionQueries.createTransaction, [destination_account_id ? destination_account_id : account_id, destination_account_id ? -amount : amount, description], (error, results) => {
+        pool.query(transactionQueries.createTransaction, [destination_account_id || account_id, destination_account_id ? -amount : amount, description], (error, results) => {
             if (error) {
                 reject(error);
             }
