98a25c9b3a09c880b7832c06197bef972d549bc0
Ephraim Becker <emb16@outlook.com>
2023-06-30 09:06:04 -0400

    Linted wishlistsRouter. #72

--- routes/wishlistRouter.js
+++ routes/wishlistRouter.js
@@ -1,38 +1,39 @@
 import express from 'express';
 import { query, param, body } from 'express-validator';
-const router = express.Router();
 import { getWishlists, createWishlist, updateWishlist, deleteWishlist } from '../controllers/wishlistsController.js';
 import validateRequest from '../utils/validateRequest.js';
+
+const router = express.Router();
 
 router.get('/',
     [
         query('id').optional().isInt({ min: 1 }).withMessage('ID must be a number'),
-        validateRequest,
+        validateRequest
     ],
     getWishlists);
 router.post('/',
     [
-        body("amount").isNumeric().withMessage("Amount must be a number"),
-        body("title").isString().withMessage("Title must be a string"),
-        body("description").isString().withMessage("Description must be a string"),
-        body("priority").isInt().withMessage("Priority must be a number"),
-        validateRequest,
+        body('amount').isNumeric().withMessage('Amount must be a number'),
+        body('title').isString().withMessage('Title must be a string'),
+        body('description').isString().withMessage('Description must be a string'),
+        body('priority').isInt().withMessage('Priority must be a number'),
+        validateRequest
     ],
     createWishlist);
 router.put('/:id',
     [
-        param("id").isInt({ min: 1 }).withMessage("ID must be a number"),
-        body("amount").isNumeric().withMessage("Amount must be a number"),
-        body("title").isString().withMessage("Title must be a string"),
-        body("description").isString().withMessage("Description must be a string"),
-        body("priority").isInt().withMessage("Priority must be a number"),
-        validateRequest,
+        param('id').isInt({ min: 1 }).withMessage('ID must be a number'),
+        body('amount').isNumeric().withMessage('Amount must be a number'),
+        body('title').isString().withMessage('Title must be a string'),
+        body('description').isString().withMessage('Description must be a string'),
+        body('priority').isInt().withMessage('Priority must be a number'),
+        validateRequest
     ],
     updateWishlist);
 router.delete('/:id',
     [
-        param("id").isInt({ min: 1 }).withMessage("ID must be a number"),
-        validateRequest,
+        param('id').isInt({ min: 1 }).withMessage('ID must be a number'),
+        validateRequest
     ],
     deleteWishlist);
 
