0cb9ba75629920f92dcfc345039eca3e7b0c6789
Ephraim Becker <emb16@outlook.com>
2023-06-30 10:56:20 -0400

    Linted generatePayrolls. #72

--- generation/generatePayrolls.js
+++ generation/generatePayrolls.js
@@ -1,20 +1,20 @@
 const generatePayrolls = (transactions, skippedTransactions, payrolls, fromDate) => {
-    let payroll_end_date = new Date(payrolls.end_date);
+    const payroll_end_date = new Date(payrolls.end_date);
 
     const newTransaction = {
-        title: "Payroll",
-        description: "payroll",
+        title: 'Payroll',
+        description: 'payroll',
         date: new Date(payroll_end_date),
-        amount: parseFloat(payrolls.net_pay),
+        amount: parseFloat(payrolls.net_pay)
     };
 
-    if (payroll_end_date <= new Date()) {
-
-    } else if (fromDate > payroll_end_date) {
-        skippedTransactions.push(newTransaction);
-    } else {
-        transactions.push(newTransaction);
+    if (payroll_end_date > new Date()) {
+        if (fromDate > payroll_end_date) {
+            skippedTransactions.push(newTransaction);
+        } else {
+            transactions.push(newTransaction);
+        }
     }
-}
+};
 
 export default generatePayrolls;
