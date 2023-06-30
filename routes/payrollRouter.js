ba97efa02e943a4e3d829ed6f1abac3b107b2fde
Ephraim Becker <emb16@outlook.com>
2023-06-30 09:24:55 -0400

    Linted payrollRouter. #72

--- routes/payrollRouter.js
+++ routes/payrollRouter.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { query } from 'express-validator';
-const router = express.Router();
 import { getPayrolls } from '../controllers/payrollsController.js';
 import validateRequest from '../utils/validateRequest.js';
+
+const router = express.Router();
 
 router.get('/',
     [
         query('employee_id').isInt({ min: 1 }).withMessage('Employee ID must be a number'),
-        validateRequest,
+        validateRequest
     ],
     getPayrolls);
 
