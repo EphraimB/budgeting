db1564786b74ea89e207adb81ce58ae74eeea5ab
Ephraim Becker <emb16@outlook.com>
2023-07-03 19:29:25 -0400

    COnverted db to TypeScript. #47

--- config/db.ts
+++ config/db.ts
@@ -1,11 +1,11 @@
 import pg from 'pg';
 
-const pool = new pg.Pool({
+const pool: pg.Pool = new pg.Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
+    port: parseInt(process.env.PGPORT)
 });
 
 export default pool;
