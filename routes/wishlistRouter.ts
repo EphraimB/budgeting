8c1704167284dc1a410673041a57314b68339a00
Ephraim Becker <emb16@outlook.com>
2023-07-04 10:54:41 -0400

    Converted wishlistRouter to ts. #47

--- routes/wishlistRouter.ts
+++ routes/wishlistRouter.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express, { Router } from 'express';
 import { query, param, body } from 'express-validator';
 import { getWishlists, createWishlist, updateWishlist, deleteWishlist } from '../controllers/wishlistsController.js';
 import validateRequest from '../utils/validateRequest.js';
 
-const router = express.Router();
+const router: Router = express.Router();
 
 router.get('/',
     [
