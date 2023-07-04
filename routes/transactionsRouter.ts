8e2ea723e2ab76ed75e67ed90610e907c9dae90e
Ephraim Becker <emb16@outlook.com>
2023-07-04 11:49:24 -0400

    Fixed types in transactionRouter. #47

--- routes/transactionsRouter.ts
+++ routes/transactionsRouter.ts
@@ -1,9 +1,15 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import express from 'express';
 import generateTransactions from '../generation/generateTransactions.js';
 import { getCurrentBalance, getTransactionsByAccount, getExpensesByAccount, getLoansByAccount, getWishlistsByAccount, getPayrollsMiddleware, getTransfersByAccount } from '../middleware/middleware.js';
 import { query } from 'express-validator';
 import validateRequest from '../utils/validateRequest.js';
+
+declare module 'express-serve-static-core' {
+    interface Request {
+        transactions: any[];
+    }
+}
 
 const router: Router = express.Router();
 
@@ -13,8 +19,8 @@ router.get('/', [
     query('from_date').exists().withMessage('From date is required').isDate().withMessage('From date must be a date in YYYY-MM-DD format'),
     query('to_date').exists().withMessage('To date is required').isDate().withMessage('To date must be a date in YYYY-MM-DD format'),
     validateRequest
-], getCurrentBalance, getTransactionsByAccount, getExpensesByAccount, getLoansByAccount, getPayrollsMiddleware, getTransfersByAccount, getWishlistsByAccount, generateTransactions, (request, response) => {
-    response.json({ account_id: parseInt(request.query.account_id), currentBalance: request.currentBalance, transactions: request.transactions });
+], getCurrentBalance, getTransactionsByAccount, getExpensesByAccount, getLoansByAccount, getPayrollsMiddleware, getTransfersByAccount, getWishlistsByAccount, generateTransactions, (request: Request, response: Response) => {
+    response.json({ account_id: request.query.account_id, currentBalance: request.currentBalance, transactions: request.transactions });
 });
 
 export default router;
