c9806f75e59ff3905540d9b7e90237d27694475c
Ephraim Becker <emb16@outlook.com>
2023-07-04 10:48:02 -0400

    Converted getEmployee to ts. #47

--- controllers/employeesController.ts
+++ controllers/employeesController.ts
@@ -1,8 +1,34 @@
+import { Request, Response } from 'express';
 import { payrollQueries } from '../models/queryData.js';
 import { getPayrolls } from '../bree/getPayrolls.js';
 import { handleError, executeQuery } from '../utils/helperFunctions.js';
 
-const employeeParse = employee => ({
+interface EmployeeInput {
+    employee_id: string;
+    name: string;
+    hourly_rate: string;
+    regular_hours: string;
+    vacation_days: string;
+    sick_days: string;
+    work_schedule: string;
+}
+
+interface EmployeeOutput {
+    employee_id: number;
+    name: string;
+    hourly_rate: number;
+    regular_hours: number;
+    vacation_days: number;
+    sick_days: number;
+    work_schedule: string;
+}
+
+/**
+ * 
+ * @param employee - Employee object
+ * @returns - Employee object with correct data types
+ */
+const employeeParse = (employee: EmployeeInput): EmployeeOutput => ({
     employee_id: parseInt(employee.employee_id),
     name: employee.name,
     hourly_rate: parseFloat(employee.hourly_rate),
@@ -12,22 +38,28 @@ const employeeParse = employee => ({
     work_schedule: employee.work_schedule
 });
 
-// Get employee
-export const getEmployee = async (request, response) => {
+/**
+ * 
+ * @param request - Request object
+ * @param response - Response object
+ * Sends a GET request to the database to retrieve all employees
+ */
+export const getEmployee = async (request: Request, response: Response): Promise<void> => {
     const { employee_id } = request.query;
 
     try {
-        const query = employee_id ? payrollQueries.getEmployee : payrollQueries.getEmployees;
-        const params = employee_id ? [employee_id] : [];
+        const query: string = employee_id ? payrollQueries.getEmployee : payrollQueries.getEmployees;
+        const params: any[] = employee_id ? [employee_id] : [];
 
-        const results = await executeQuery(query, params);
+        const results = await executeQuery<EmployeeInput>(query, params);
 
         if (employee_id && results.length === 0) {
-            return response.status(404).send('Employee not found');
+            response.status(404).send('Employee not found');
+            return;
         }
 
         // Parse the data to the correct format and return an object
-        const employees = results.map(employee => employeeParse(employee));
+        const employees: EmployeeOutput[] = results.map(employee => employeeParse(employee));
 
         response.status(200).json(employees);
     } catch (error) {
