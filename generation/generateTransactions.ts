169a59aeba97e722ee09e9e452e4d371ddd7d20c
Ephraim Becker <emb16@outlook.com>
2023-07-04 12:27:09 -0400

    Converted generateTransactions to ts. #47

--- generation/generateTransactions.ts
+++ generation/generateTransactions.ts
@@ -6,6 +6,97 @@ import { generateDailyTransfers, generateWeeklyTransfers, generateMonthlyTransfe
 import generateWishlists from './generateWishlists.js';
 import calculateBalances from './calculateBalances.js';
 
+interface Transaction {
+    transaction_id: number;
+    account_id: number;
+    transaction_amount: number;
+    transaction_type: string;
+    transaction_title: string;
+    transaction_description: string;
+    transaction_date: string;
+    date_created: string;
+    date_modified: string;
+}
+
+interface Expense {
+    expense_id: number;
+    account_id: number;
+    expense_amount: number;
+    expense_title: string;
+    expense_description: string;
+    expense_date: string;
+    frequency_type: number;
+    date_created: string;
+    date_modified: string;
+}
+
+interface Loan {
+    loan_id: number;
+    account_id: number;
+    cron_job_id?: number;
+    loan_amount: number;
+    loan_plan_amount: number;
+    loan_recipient: string;
+    loan_title: string;
+    loan_description: string;
+    frequency_type: number;
+    frequency_type_variable: number;
+    frequency_day_of_month: number;
+    frequency_day_of_week: number;
+    frequency_week_of_month: number;
+    frequency_month: number;
+    loan_begin_date: string;
+    loan_end_date: string;
+    date_created: string;
+    date_modified: string;
+}
+
+interface Payroll {
+    payroll_id: number;
+    account_id: number;
+    payroll_amount: number;
+    payroll_title: string;
+    payroll_description: string;
+    payroll_date: string;
+    date_created: string;
+    date_modified: string;
+}
+
+interface Wishlist {
+    wishlist_id: number;
+    account_id: number;
+    wishlist_amount: number;
+    wishlist_title: string;
+    wishlist_description: string;
+    wishlist_date: string;
+    date_created: string;
+    date_modified: string;
+}
+
+interface Transfer {
+    transfer_id: number;
+    account_id: number;
+    transfer_amount: number;
+    transfer_title: string;
+    transfer_description: string;
+    transfer_date: string;
+    frequency_type: number;
+    date_created: string;
+    date_modified: string;
+}
+
+declare module 'express-serve-static-core' {
+    interface Request {
+        transaction: Transaction[];
+        expenses: Expense[];
+        loans: Loan[];
+        payrolls: Payroll[];
+        wishlists: Wishlist[];
+        transfers: Transfer[];
+        currentBalance: number;
+    }
+}
+
 /**
  * 
  * @param request - The request object
@@ -14,12 +105,12 @@ import calculateBalances from './calculateBalances.js';
  * Generates transactions for the given account and date range
  */
 const generateTransactions = (request: Request, response: Response, next: NextFunction): void => {
-    const fromDate = new Date(request.query.from_date);
-    const toDate = new Date(request.query.to_date);
+    const fromDate = new Date(request.query.from_date as string);
+    const toDate = new Date(request.query.to_date as string);
     const currentBalance = request.currentBalance;
-    const account_id = parseInt(request.query.account_id);
-    const transactions = [];
-    const skippedTransactions = [];
+    const account_id = parseInt(request.query.account_id as string);
+    const transactions: any[] = [];
+    const skippedTransactions: any[] = [];
 
     transactions.push(
         ...request.transaction.map(transaction => ({
@@ -28,7 +119,7 @@ const generateTransactions = (request: Request, response: Response, next: NextFu
             description: transaction.transaction_description,
             date: transaction.date_created,
             date_modified: transaction.date_modified,
-            amount: parseFloat(transaction.transaction_amount)
+            amount: transaction.transaction_amount
         }))
     );
 
@@ -72,14 +163,14 @@ const generateTransactions = (request: Request, response: Response, next: NextFu
         }
     });
 
-    transactions.sort((a, b) => new Date(a.date) - new Date(b.date));
+    transactions.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
 
     calculateBalances(transactions.concat(skippedTransactions), currentBalance);
 
     request.wishlists.forEach(wishlist => {
         generateWishlists(transactions, skippedTransactions, wishlist, fromDate);
 
-        transactions.sort((a, b) => new Date(a.date) - new Date(b.date));
+        transactions.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
 
         calculateBalances(transactions.concat(skippedTransactions), currentBalance);
     });
