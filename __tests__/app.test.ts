6c62c2e0f344d58c13110149d757cbc9bcaf9afd
Ephraim Becker <emb16@outlook.com>
2023-07-06 18:53:49 -0400

    Narrowed down to an import error.

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,45 +1,25 @@
 import { jest } from '@jest/globals';
 import request from 'supertest';
-import express, { Express, Router } from 'express';
+import { Express } from 'express';
 
-/**
- * 
- * @returns {Promise<Express>} A promise that resolves to an Express app
- */
-const createApp = async (): Promise<Express> => {
-    const app: Express = express();
 
-    // Import the module that uses the mock
-    const routerModule = await import('../app.js');
-    const router: Router = routerModule.default;
-    app.use('/', router as express.Router);
+// Mock the breeManager module
+jest.mock('../bree/breeManager.js', () => ({
+    initializeBree: jest.fn(),
+    getBree: jest.fn()
+}));
 
-    return app;
-};
-
-beforeAll(async () => {
-    // Mock the breeManager module
-    jest.mock('../bree/breeManager.js', () => ({
-        initializeBree: jest.fn(),
-        getBree: jest.fn()
-    }));
-
-    // Mock the getJobs module
-    jest.mock('../bree/getJobs.js', () => ({
-        default: jest.fn()
-    }));
-
-    app = await createApp();
-});
-
-afterAll(() => {
-    jest.restoreAllMocks();
-});
-
-let app: Express;
+// Mock the getJobs module
+jest.mock('../bree/getJobs.js', () => ({
+    default: jest.fn()
+}));
 
 describe('Test application', () => {
     it('should trigger not found for site 404', async () => {
+        // Import the module that uses the mock
+        const appModule = await import('../app.js');
+        const app: Express = appModule.default;
+
         const response = await request(app).get('/no-such-path');
         expect(response.statusCode).toBe(404);
     });
