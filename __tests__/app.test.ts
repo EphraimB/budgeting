078640cc9bc46e46e09af75a89e6c21e84e18cfe
Ephraim Becker <emb16@outlook.com>
2023-07-06 18:47:00 -0400

    Removed redundant code.

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -8,7 +8,6 @@ import express, { Express, Router } from 'express';
  */
 const createApp = async (): Promise<Express> => {
     const app: Express = express();
-    app.use(express.json());
 
     // Import the module that uses the mock
     const routerModule = await import('../app.js');
@@ -18,27 +17,30 @@ const createApp = async (): Promise<Express> => {
     return app;
 };
 
-// Mock the breeManager module
-jest.mock('../bree/breeManager.js', () => ({
-    initializeBree: jest.fn(),
-    getBree: jest.fn()
-}));
+beforeAll(async () => {
+    // Mock the breeManager module
+    jest.mock('../bree/breeManager.js', () => ({
+        initializeBree: jest.fn(),
+        getBree: jest.fn()
+    }));
 
-// Mock the getJobs module
-jest.mock('../bree/getJobs.js', () => ({
-    default: jest.fn()
-}));
+    // Mock the getJobs module
+    jest.mock('../bree/getJobs.js', () => ({
+        default: jest.fn()
+    }));
 
-let app: Express;
-
-beforeEach(async () => {
-    // Create a new app for each test
     app = await createApp();
 });
 
+afterAll(() => {
+    jest.restoreAllMocks();
+});
+
+let app: Express;
+
 describe('Test application', () => {
     it('should trigger not found for site 404', async () => {
-        const response: request.Response = await request(app).get('/no-such-path');
+        const response = await request(app).get('/no-such-path');
         expect(response.statusCode).toBe(404);
     });
 });
