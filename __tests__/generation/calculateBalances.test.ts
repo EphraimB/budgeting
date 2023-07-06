ba5bc95031386c686e3e54545d77813aa477d062
Ephraim Becker <emb16@outlook.com>
2023-07-06 11:38:55 -0400

    Passes half the tests. #88

--- __tests__/generation/calculateBalances.test.ts
+++ __tests__/generation/calculateBalances.test.ts
@@ -1,25 +1,64 @@
 import { GeneratedTransaction } from '../../types/types.js';
 import calculateBalances from '../../generation/calculateBalances.js';
 
-describe('calculateBalances', () => {
-    it('should calculate the balance for each transaction', () => {
-        // Create dates properly
-        const now = new Date();
-        const yesterday = new Date();
-        yesterday.setDate(now.getDate() - 1);
-        const tomorrow = new Date();
-        tomorrow.setDate(now.getDate() + 1);
-        const dayAfterTomorrow = new Date();
-        dayAfterTomorrow.setDate(now.getDate() + 2);
+// Create dates properly
+const now = new Date();
+const lastWeek = new Date();
+lastWeek.setDate(now.getDate() - 7);
+const yesterday = new Date();
+yesterday.setDate(now.getDate() - 1);
+const tomorrow = new Date();
+tomorrow.setDate(now.getDate() + 1);
+const dayAfterTomorrow = new Date();
+dayAfterTomorrow.setDate(now.getDate() + 2);
 
+describe('calculateBalances', () => {
+    it('should calculate the balance for each transaction with no past transactions', () => {
+        const transactions: GeneratedTransaction[] = [
+            {
+                title: 'Test Transaction 2',
+                description: 'Testing the transaction 2',
+                date: tomorrow,
+                amount: 500
+            },
+            {
+                title: 'Test Transaction 3',
+                description: 'Testing the transaction 3',
+                date: dayAfterTomorrow,
+                amount: -500
+            },
+            {
+                title: 'Test Transaction 4',
+                description: 'Testing the transaction 4',
+                date: dayAfterTomorrow,
+                amount: 1000
+            }
+        ];
+
+        calculateBalances(transactions, 5000);
+
+        expect(transactions[0].balance).toBe(5500);
+        expect(transactions[1].balance).toBe(5000);
+        expect(transactions[2].balance).toBe(6000);
+    });
+
+    it('should calculate the balance for each transaction with past transactions', () => {
         const transactions: GeneratedTransaction[] = [
             {
                 transaction_id: 1,
                 title: 'Test Transaction',
                 description: 'Testing the transaction',
                 date: yesterday,
-                date_modified: yesterday,
+                date_modified: lastWeek,
                 amount: 1000
+            },
+            {
+                transaction_id: 2,
+                title: 'Test Transaction 2',
+                description: 'Testing the transaction 2',
+                date: tomorrow,
+                date_modified: yesterday,
+                amount: -500
             },
             {
                 title: 'Test Transaction 2',
