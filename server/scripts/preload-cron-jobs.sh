c466dc091aa92d93a60713df4a8708476d9657e9
Ephraim Becker <emb16@outlook.com>
2023-07-09 18:48:00 -0400

    Preloads a cron job.

--- server/scripts/preload-cron-jobs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Add the cron job to the system crontab
-(crontab -l 2>/dev/null; echo "0 0 1 * * /app/dist/crontab/scripts/cronScriptCheckEmployees.sh") | crontab -
-
-# Start the main process (your application)
-exec "$@"
