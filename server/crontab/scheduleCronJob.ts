58f9b83aa3654d134e99926670914a16f04ce3e4
Ephraim Becker <emb16@outlook.com>
2023-07-09 12:00:19 -0400

    Moved Express into server directory.

--- server/crontab/scheduleCronJob.ts
+++ server/crontab/scheduleCronJob.ts
@@ -1,5 +1,5 @@
 import { exec } from 'child_process';
-import { CronJobData, JobOptions, determineCronValues, createCronJob } from './yourOtherFunctions';
+import determineCronValues from './determineCronValues';
 import { v4 as uuidv4 } from 'uuid';
 
 /**
@@ -7,15 +7,14 @@ import { v4 as uuidv4 } from 'uuid';
  * @param jobDetails - Job details
  * @returns - Cron job data
  */
-const scheduleCronJob = async (jobDetails: CronJobData) => {
-    const uniqueId = uuidv4();
-    const newJob = createCronJob(jobDetails, uniqueId);
+const scheduleCronJob = async (jobDetails: any) => {
+    const uniqueId: string = uuidv4();
 
     // You should replace this with the path to your Node.js script that the cron job will run
-    const yourScriptPath = "/path/to/your/script.js";
+    const yourScriptPath: string = "/path/to/your/script.js";
 
     // add a new cron job to the system crontab
-    exec(`(crontab -l ; echo "${newJob.cron} node ${yourScriptPath}") | crontab -`, (error: Error | null, stdout: string, stderr: string) => {
+    exec(`(crontab -l ; echo "${determineCronValues} node ${yourScriptPath}") | crontab -`, (error: Error | null, stdout: string, stderr: string) => {
         if (error) {
             console.error(`Error setting up cron job: ${error}`);
             return;
@@ -24,8 +23,8 @@ const scheduleCronJob = async (jobDetails: CronJobData) => {
     });
 
     return {
-        cronDate: newJob.cron,
-        uniqueId: newJob.name
+        cronDate: determineCronValues,
+        uniqueId: uniqueId
     };
 };
 
