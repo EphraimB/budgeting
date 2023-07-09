d8b4069661e88b4c74fd5bc35c90839255f6121b
Ephraim Becker <emb16@outlook.com>
2023-07-09 12:21:34 -0400

    Trying to migrate to actual crontab.

--- server/crontab/determineCronValues.ts
+++ server/crontab/determineCronValues.ts
@@ -1,15 +1,20 @@
 /**
  * 
- * @param frequency_type - Frequency type (0 = daily, 1 = weekly, 2 = monthly)
- * @param frequency_type_variable - Frequency type variable (e.g. 1 = every day, 2 = every other day, etc.)
- * @param frequency_day_of_month - Frequency day of month (e.g. 1 = every 1st of the month, 2 = every 2nd of the month, etc.)
- * @param frequency_day_of_week - Frequency day of week (e.g. 1 = every Monday, 2 = every Tuesday, etc.)
- * @param frequency_week_of_month - Frequency week of month (e.g. 0 = every 1st week of the month, 1 = every 2nd week of the month, etc.)
- * @param frequency_month_of_year - Frequency month of year (e.g. 0 = every January, 1 = every February, etc.)
+ * @param jobDetails - Job details
  * @param transactionDate - Transaction date
  * @returns 
  */
-const determineCronValues = (frequency_type: number, frequency_type_variable: number, frequency_day_of_month: number, frequency_day_of_week: number, frequency_week_of_month: number, frequency_month_of_year: number, transactionDate: Date): any => {
+const determineCronValues = (jobDetails: any): any => {
+    const {
+        frequency_type,
+        frequency_type_variable,
+        frequency_day_of_month,
+        frequency_day_of_week,
+        frequency_week_of_month,
+        frequency_month_of_year,
+        transactionDate,
+    } = jobDetails;
+
     let cronDay = '*';
     let cronMonth = '*';
     let cronDayOfWeek = '*';
--- server/crontab/scheduleCronJob.ts
+++ server/crontab/scheduleCronJob.ts
@@ -3,28 +3,36 @@ import determineCronValues from './determineCronValues';
 import { v4 as uuidv4 } from 'uuid';
 
 /**
- * 
  * @param jobDetails - Job details
  * @returns - Cron job data
  */
 const scheduleCronJob = async (jobDetails: any) => {
     const uniqueId: string = uuidv4();
 
-    // You should replace this with the path to your Node.js script that the cron job will run
-    const yourScriptPath: string = "/path/to/your/script.js";
+    // You should replace this with the path to your Bash script that the cron job will run
+    const yourScriptPath: string = "/path/to/your/script.sh";
 
-    // add a new cron job to the system crontab
-    exec(`(crontab -l ; echo "${determineCronValues} node ${yourScriptPath}") | crontab -`, (error: Error | null, stdout: string, stderr: string) => {
-        if (error) {
-            console.error(`Error setting up cron job: ${error}`);
-            return;
+    // Determine the cron values based on the job details
+    const { cronDay, cronMonth, cronDayOfWeek } = determineCronValues(jobDetails);
+
+    // Build the cron command that will execute the Bash script
+    const cronCommand = `${yourScriptPath} --arg1 value1 --arg2 value2`;
+
+    // Add a new cron job to the system crontab
+    exec(
+        `(crontab -l ; echo "${cronDay} ${cronMonth} ${cronDayOfWeek} ${cronCommand}") | crontab -`,
+        (error: Error | null, stdout: string, stderr: string) => {
+            if (error) {
+                console.error(`Error setting up cron job: ${error}`);
+                return;
+            }
+            console.log(`Cron job set up successfully!`);
         }
-        console.log(`Cron job set up successfully!`);
-    });
+    );
 
     return {
-        cronDate: determineCronValues,
-        uniqueId: uniqueId
+        cronDate: { cronDay, cronMonth, cronDayOfWeek },
+        uniqueId: uniqueId,
     };
 };
 
