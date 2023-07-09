3617e158b86acd6db87856091e6117d3339e853e
Ephraim Becker <emb16@outlook.com>
2023-07-09 15:24:58 -0400

    Added uniqueId.

--- server/crontab/scheduleCronJob.ts
+++ server/crontab/scheduleCronJob.ts
@@ -25,7 +25,7 @@ const scheduleCronJob = async (jobDetails: any) => {
     const cronDate = determineCronValues({ date, frequency_type, frequency_type_variable, frequency_day_of_month, frequency_day_of_week, frequency_week_of_month, frequency_month_of_year });
 
     // Build the cron command that will execute the Bash script
-    const cronCommand = `${scriptPath} --account_id ${account_id} --amount ${amount} --description ${description}`;
+    const cronCommand = `${scriptPath} --account_id ${account_id} --amount ${amount} --description ${description} --uniqueId ${uniqueId}`;
 
     // Add a new cron job to the system crontab
     exec(
