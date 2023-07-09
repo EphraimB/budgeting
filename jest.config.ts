68224a0274b22dc8eef15d9572c15bb69321149b
Ephraim Becker <emb16@outlook.com>
2023-07-09 09:40:07 -0400

    Added ts-node.

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+import { defaults } from 'jest-config';
+
+const jestConfig: JestConfigWithTsJest = {
+    ...defaults,
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.(t|j)s$': ['ts-jest', {
+            tsconfig: './tsconfig.json',
+            useESM: true,
+            diagnostics: {
+                ignoreCodes: [1343]
+            },
+        }]
+    },
+    moduleNameMapper: {
+        '^(\\.\\.?\\/.+)\\.js$': '$1'
+    }
+};
+
+export default jestConfig;
