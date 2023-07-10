55a2c57ccfad5604b38eaee22ce70b3d2258866e
Ephraim Becker <emb@paay.co>
2023-07-10 13:27:29 -0400

    Added title as cron param.

---
+++ server/scripts/createTransaction.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Fetch the employee IDs from the database using psql and environment variables
+createTransaction=$(PGPASSWORD="$PGPASSWORD" psql -h "$PGHOST" -p "$PGPORT" -d "$PGDB" -U "$PGUSER" -c "INSERT INTO transaction_history (account_id, transaction_amount, transaction_title, transaction_description) VALUES ($2, $3, $4, $5)" -t)
+
+# Log if the transaction was successful
+if [ $? -eq 0 ]; then
+    echo "Transaction successful created"
+else
+    echo "Transaction creation failed"
+fi
+
+# If 
