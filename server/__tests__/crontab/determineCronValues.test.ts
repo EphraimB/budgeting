fb9ae02b7ab5cbc81422985dbbf5a8742e3010aa
Ephraim Becker <emb16@outlook.com>
2023-07-10 19:47:56 -0400

    Need to pass tests.

--- server/__tests__/crontab/determineCronValues.test.ts
+++ server/__tests__/crontab/determineCronValues.test.ts
@@ -3,17 +3,19 @@ import determineCronValues from '../../crontab/determineCronValues';
 describe('determineCronValues', () => {
     it('should return correct cron date for frequency type 0', () => {
         const jobDetails = {
+            date: '2021-01-01T00:00:00.000Z',
             frequency_type: 0,
             frequency_type_variable: 2,
         };
 
         const result = determineCronValues(jobDetails);
 
-        expect(result).toEqual('* * */2 * *');
+        expect(result).toEqual('0 0 */2 * *');
     });
 
     it('should return correct cron date for frequency type 1', () => {
         const jobDetails = {
+            date: '2021-01-01T00:00:00.000Z',
             frequency_type: 1,
             frequency_type_variable: 1,
             frequency_day_of_week: 5,
@@ -22,7 +24,7 @@ describe('determineCronValues', () => {
 
         const result = determineCronValues(jobDetails);
 
-        expect(result).toEqual('* * 10 * 5');
+        expect(result).toEqual('0 0 10 * 5');
     });
 
     // Add more tests for other frequency types and edge cases...
