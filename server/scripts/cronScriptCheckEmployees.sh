d136ea4e0c1b5187e981970dd96b0a0855d6344d
Ephraim Becker <emb@paay.co>
2023-07-10 11:11:43 -0400

    Now adds uniqueId in crontab.

--- server/scripts/cronScriptCheckEmployees.sh
+++ server/scripts/cronScriptCheckEmployees.sh
@@ -87,14 +87,17 @@ for employeeId in $employeeIds; do
     startDay=$(echo "$startDate" | cut -d '-' -f 3)
     endDay=$(echo "$endDate" | cut -d '-' -f 3)
 
-    # Generate a unique ID for the cron job based on the employee ID and payroll period
-    cronJobId="payroll_${employeeId}_${startDay}_${endDay}_${currentMonth}"
+    # Generate a unique ID for the cron job using uuidgen
+    uniqueId=$(uuidgen)
+
+    # Generate a cron job ID based on the unique ID
+    cronJobId="payroll_${uniqueId}"
 
     # Generate the cron schedule for the current payroll period
     cronSchedule="0 0 $endDay $currentMonth *"
 
     # Create the cron command with the payroll details
-    cronCommand="/app/dist/crontab/scripts/createTransaction.sh --employee_id $employeeId --net_pay $netPay"
+    cronCommand="/app/dist/crontab/scripts/createTransaction.sh --employee_id $employeeId --net_pay $netPay --uniqueId $cronJobId"
 
     # Append new cron entry to temp file
     echo "$cronSchedule $cronCommand" >> "$cronFile"
