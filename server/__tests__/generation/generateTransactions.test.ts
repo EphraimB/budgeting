dae2003249d105e569920533709013e0fe6884e8
Ephraim Becker <emb16@outlook.com>
2023-07-16 20:24:55 -0400

    Passed another test.

--- server/__tests__/generation/generateTransactions.test.ts
+++ server/__tests__/generation/generateTransactions.test.ts
@@ -34,9 +34,9 @@ describe('generateTransactions', () => {
             transaction: [{ account_id: 1, transactions: transactions.filter(transaction => transaction.account_id === 1) }],
             expenses: [{ account_id: 1, expenses: expenses.filter(expense => expense.account_id === 1) }],
             payrolls: [{ employee_id: 1, payrolls }],
-            loans: [loans.filter(loan => loan.account_id === 1)],
-            transfers: [transfers.filter(transfer => transfer.account_id === 1)],
-            wishlists: [wishlists.filter(wishlist => wishlist.account_id === 1)]
+            loans: [{ account_id: 1, loan: loans.filter(loan => loan.account_id === 1) }],
+            transfers: [{ account_id: 1, transfer: transfers.filter(transfer => transfer.account_id === 1) }],
+            wishlists: [{ account_id: 1, wishlist: wishlists.filter(wishlist => wishlist.account_id === 1) }]
         };
 
         mockResponse = {};
@@ -58,6 +58,9 @@ describe('generateTransactions', () => {
         // add checks for any additional properties or state you expect mockRequest to have after generateTransactions
         expect(mockRequest.expenses[0].expenses).toEqual(expenses.filter(expense => expense.account_id === 1));
         expect(mockRequest.payrolls[0].payrolls).toEqual(payrolls);
+        expect(mockRequest.loans[0].loan).toEqual(loans.filter(loan => loan.account_id === 1));
+        expect(mockRequest.transfers[0].transfer).toEqual(transfers.filter(transfer => transfer.account_id === 1));
+        expect(mockRequest.wishlists[0].wishlist).toEqual(wishlists.filter(wishlist => wishlist.account_id === 1));
     });
 
     it('should make sure that transactions are sorted by date', () => {
