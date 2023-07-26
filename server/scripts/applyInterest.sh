9e820d529b2a3350053b17dee3ce904b4335acee
Ephraim Becker <emb@paay.co>
2023-07-26 12:37:22 -0400

    Fixed a bug in the script.

--- server/scripts/applyInterest.sh
+++ server/scripts/applyInterest.sh
@@ -8,7 +8,7 @@ transaction_title=$5
 transaction_description=$6
 
 # Update the loan_amount in the loans table
-updateLoanAmount=$(PGPASSWORD="$PGPASSWORD" psql -h "$PGHOST" -p "$PGPORT" -d "$PGDB" -U "$PGUSER" -c "UPDATE loans SET loan_amount * '$interest_rate' WHERE loan_id = '$id'" -t)
+updateLoanAmount=$(PGPASSWORD="$PGPASSWORD" psql -h "$PGHOST" -p "$PGPORT" -d "$PGDB" -U "$PGUSER" -c "UPDATE loans SET loan_amount = loan_amount + (loan_amount * $interest_rate) WHERE loan_id = $id" -t)
 
 # If successful
 if [ $? -eq 0 ]; then
