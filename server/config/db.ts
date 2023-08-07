b8792569cc85b00043c122c65dffacac6c656d74
Ephraim Becker <emb16@outlook.com>
2023-08-07 18:09:49 -0400

    Lint improvements.

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,11 +1,11 @@
-import pg, { Pool } from 'pg';
+import pg, { type Pool } from 'pg';
 
 const pool: Pool = new pg.Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
-    port: parseFloat(process.env.PGPORT ?? '5432') ?? 5432,
+    port: parseFloat(process.env.PGPORT ?? '5432'),
 });
 
 export default pool;
