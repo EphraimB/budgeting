471cc582908852cf5a24f4ad2750a4e64331559d
Ephraim Becker <emb16@outlook.com>
2023-08-10 18:03:14 -0400

    Tested winston.

---
+++ server/__tests__/config/winston.test.ts
@@ -0,0 +1,46 @@
+import { jest } from '@jest/globals';
+import { logger } from '../../config/winston';
+
+// Mock Winston's transports so logs aren't actually written during tests
+jest.mock('winston', () => {
+    const mFormat = {
+        combine: jest.fn(),
+        timestamp: jest.fn(),
+        printf: jest.fn(),
+        colorize: jest.fn(),
+        errors: jest.fn(),
+        json: jest.fn(),
+        splat: jest.fn(),
+    };
+    const mTransports = {
+        Console: jest.fn().mockReturnThis(),
+        File: jest.fn().mockReturnThis(),
+    };
+    const mLogger = {
+        error: jest.fn(),
+        warn: jest.fn(),
+        info: jest.fn(),
+        debug: jest.fn(),
+    };
+    return {
+        createLogger: jest.fn().mockReturnValue(mLogger),
+        format: mFormat,
+        transports: mTransports,
+    };
+});
+
+describe('Logger', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should log error', () => {
+        logger.error('test error');
+        expect(logger.error).toHaveBeenCalledWith('test error');
+    });
+
+    it('should log info', () => {
+        logger.info('test info');
+        expect(logger.info).toHaveBeenCalledWith('test info');
+    });
+});
