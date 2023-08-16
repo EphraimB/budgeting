62946a04c48ed31645596ae279ba5f3dc2fdaec1
Ephraim Becker <emb16@outlook.com>
2023-08-16 17:34:51 -0400

    Added commuteHistoryRouter test.

---
+++ server/__tests__/routes/commuteHistoryRouter.test.ts
@@ -0,0 +1,148 @@
+import { jest } from '@jest/globals';
+import request from 'supertest';
+import express, {
+    type Express,
+    type Request,
+    type Response,
+    type NextFunction,
+    type Router,
+} from 'express';
+
+/**
+ *
+ * @returns {Promise<Express>} A promise that resolves to an Express app
+ */
+const createApp = async (): Promise<Express> => {
+    const app: Express = express();
+    app.use(express.json());
+
+    // Import the module that uses the mock
+    const routerModule = await import('../../routes/commuteHistoryRouter');
+    const commuteHistoryRouter: Router = routerModule.default;
+    app.use('/', commuteHistoryRouter);
+
+    return app;
+};
+
+beforeAll(() => {
+    jest.mock('../../controllers/commuteHistoryController', () => ({
+        getCommuteHistory: (req: Request, res: Response, next: NextFunction) =>
+            res.json({ message: 'success' }),
+        createCommuteHistory: (
+            req: Request,
+            res: Response,
+            next: NextFunction,
+        ) => res.json({ message: 'success' }),
+        updateCommuteHistory: (
+            req: Request,
+            res: Response,
+            next: NextFunction,
+        ) => res.json({ message: 'success' }),
+        deleteCommuteHistory: (
+            req: Request,
+            res: Response,
+            next: NextFunction,
+        ) => res.json({ message: 'success' }),
+    }));
+});
+
+afterAll(() => {
+    jest.restoreAllMocks();
+});
+
+let app: Express;
+
+beforeEach(async () => {
+    // Create a new app for each test
+    app = await createApp();
+});
+
+describe('GET /', () => {
+    it('responds with json', async () => {
+        const response: request.Response = await request(app)
+            .get('/')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('GET / with id query', () => {
+    it('responds with json', async () => {
+        const response: request.Response = await request(app)
+            .get('/?id=1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('GET / with account_id query', () => {
+    it('responds with json', async () => {
+        const response: request.Response = await request(app)
+            .get('/?account_id=1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('POST /', () => {
+    it('responds with json', async () => {
+        const newCommuteHistory = {
+            account_id: 1,
+            fare_amount: 2.75,
+            commute_system: 'OMNY',
+            fare_type: 'Single Ride',
+            timestamp: '2021-07-01 00:00:00',
+        };
+
+        const response: request.Response = await request(app)
+            .post('/')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .send(newCommuteHistory);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('PUT /:id', () => {
+    it('responds with json', async () => {
+        const newCommuteHistory = {
+            account_id: 1,
+            fare_amount: 2.75,
+            commute_system: 'OMNY',
+            fare_type: 'Single Ride',
+            timestamp: '2021-07-01 00:00:00',
+        };
+
+        const response: request.Response = await request(app)
+            .put('/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .send(newCommuteHistory);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('DELETE /:id', () => {
+    it('responds with json', async () => {
+        const response: request.Response = await request(app)
+            .delete('/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
