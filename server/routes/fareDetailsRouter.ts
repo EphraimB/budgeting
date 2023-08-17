2a9aab4deac2b4a4ad916f1804a4b75fc9399fce
Ephraim Becker <emb16@outlook.com>
2023-08-17 10:09:34 -0400

    Added fare datails router.

---
+++ server/routes/fareDetailsRouter.ts
@@ -0,0 +1,103 @@
+import express, { type Router } from 'express';
+import { query, param, body } from 'express-validator';
+import {
+    getFareDetails,
+    createFareDetail,
+    updateFareDetail,
+    deleteFareDetail,
+} from '../controllers/fareDetailsController.js';
+import validateRequest from '../utils/validateRequest.js';
+
+const router: Router = express.Router();
+
+router.get(
+    '/',
+    [
+        query('id')
+            .optional()
+            .isInt({ min: 1 })
+            .withMessage('ID must be a number'),
+        validateRequest,
+        query('account_id')
+            .optional()
+            .isInt({ min: 1 })
+            .withMessage('Account ID must be a number'),
+    ],
+    getFareDetails,
+);
+
+router.post(
+    '/',
+    [
+        body('account_id')
+            .isInt({ min: 1 })
+            .withMessage('Account ID must be a number'),
+        body('commute_system_id')
+            .isInt({ min: 1 })
+            .withMessage('Commute System ID must be a number'),
+        body('name').isString().withMessage('Name must be a string'),
+        body('fare_amount')
+            .isFloat()
+            .withMessage('Fare amount must be a number'),
+        body('begin_in_effect_day_of_week')
+            .isInt({ min: 0, max: 6 })
+            .withMessage(
+                'Begin in effect day of week must be a number between 0 and 6',
+            ),
+        body('begin_in_effect_time')
+            .isTime({ hourFormat: 'hour24' })
+            .withMessage('Invalid time'),
+        body('end_in_effect_day_of_week')
+            .isInt({ min: 0, max: 6 })
+            .withMessage(
+                'End in effect day of week must be a number between 0 and 6',
+            ),
+        body('end_in_effect_time').isTime({ hourFormat: 'hour24' }),
+        validateRequest,
+    ],
+    createFareDetail,
+);
+
+router.put(
+    '/:id',
+    [
+        param('id').isInt({ min: 1 }).withMessage('ID must be a number'),
+        body('account_id')
+            .isInt({ min: 1 })
+            .withMessage('Account ID must be a number'),
+        body('commute_system_id')
+            .isInt({ min: 1 })
+            .withMessage('Commute System ID must be a number'),
+        body('name').isString().withMessage('Name must be a string'),
+        body('fare_amount')
+            .isFloat()
+            .withMessage('Fare amount must be a number'),
+        body('begin_in_effect_day_of_week')
+            .isInt({ min: 0, max: 6 })
+            .withMessage(
+                'Begin in effect day of week must be a number between 0 and 6',
+            ),
+        body('begin_in_effect_time')
+            .isTime({ hourFormat: 'hour24' })
+            .withMessage('Invalid time'),
+        body('end_in_effect_day_of_week')
+            .isInt({ min: 0, max: 6 })
+            .withMessage(
+                'End in effect day of week must be a number between 0 and 6',
+            ),
+        body('end_in_effect_time').isTime({ hourFormat: 'hour24' }),
+        validateRequest,
+    ],
+    updateFareDetail,
+);
+
+router.delete(
+    '/:id',
+    [
+        param('id').isInt({ min: 1 }).withMessage('ID must be a number'),
+        validateRequest,
+    ],
+    deleteFareDetail,
+);
+
+export default router;
