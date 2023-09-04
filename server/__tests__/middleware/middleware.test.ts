60582725b445a5ed1555c9d0f546c838d14d6eee
Ephraim Becker <emb16@outlook.com>
2023-09-04 16:19:45 -0400

    Passed another middleware test.

--- server/__tests__/middleware/middleware.test.ts
+++ server/__tests__/middleware/middleware.test.ts
@@ -802,6 +802,23 @@ describe('getCommuteExpensesByAccount', () => {
             message: 'Error getting commute expenses',
         });
     });
+
+    it('should return a 404 when account_id is not found', async () => {
+        mockModule([], []);
+
+        const { getCommuteExpensesByAccount } = await import(
+            '../../middleware/middleware.js'
+        );
+
+        mockRequest.query = { account_id: '5', from_date: '2023-06-01' };
+
+        await getCommuteExpensesByAccount(mockRequest, mockResponse, mockNext);
+
+        expect(mockResponse.status).toHaveBeenCalledWith(404);
+        expect(mockResponse.send).toHaveBeenCalledWith(
+            'Account with ID 5 not found',
+        );
+    });
 });
 
 describe('getCurrentBalance', () => {
