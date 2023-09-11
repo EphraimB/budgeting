7d8a07995845434b2708d437eab3a6360d3ad27a
Ephraim Becker <emb16@outlook.com>
2023-09-11 15:57:04 -0400

    Fixed the router test.

--- server/__tests__/routes/commuteScheduleRouter.test.ts
+++ server/__tests__/routes/commuteScheduleRouter.test.ts
@@ -25,6 +25,66 @@ const createApp = async (): Promise<Express> => {
 };
 
 beforeAll(() => {
+    jest.mock('../../middleware/middleware', () => ({
+        setQueries: jest.fn(
+            (req: Request, res: Response, next: NextFunction) => {
+                next();
+            },
+        ),
+        getCurrentBalance: jest.fn(
+            (req: Request, res: Response, next: NextFunction) => {
+                next();
+            },
+        ),
+        getTransactionsByAccount: jest.fn(
+            (req: Request, res: Response, next: NextFunction) => {
+                next();
+            },
+        ),
+        getIncomeByAccount: jest.fn(
+            (req: Request, res: Response, next: NextFunction) => {
+                next();
+            },
+        ),
+        getExpensesByAccount: jest.fn(
+            (req: Request, res: Response, next: NextFunction) => {
+                next();
+            },
+        ),
+        getLoansByAccount: jest.fn(
+            (req: Request, res: Response, next: NextFunction) => {
+                next();
+            },
+        ),
+        getPayrollsMiddleware: jest.fn(
+            (req: Request, res: Response, next: NextFunction) => {
+                next();
+            },
+        ),
+        getTransfersByAccount: jest.fn(
+            (req: Request, res: Response, next: NextFunction) => {
+                next();
+            },
+        ),
+        getWishlistsByAccount: jest.fn(
+            (req: Request, res: Response, next: NextFunction) => {
+                next();
+            },
+        ),
+        updateWishlistCron: jest.fn(
+            (req: Request, res: Response, next: NextFunction) => {
+                next();
+            },
+        ),
+    }));
+
+    jest.mock('../../generation/generateTransactions', () => {
+        return jest.fn((req: Request, res: Response, next: NextFunction) => {
+            req.transactions = [];
+            next();
+        });
+    });
+
     jest.mock('../../controllers/commuteScheduleController', () => ({
         getCommuteSchedule: (req: Request, res: Response, next: NextFunction) =>
             res.json({ message: 'success' }),
