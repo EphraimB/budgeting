250505ccc11ad67e63149d0239b5a1af505b5167
Ephraim Becker <emb16@outlook.com>
2023-09-20 21:05:44 -0400

    Trying to fix some issues.

--- client/components/AccountList.tsx
+++ client/components/AccountList.tsx
@@ -1,18 +1,25 @@
-"use client";
-
 import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 
-function AccountList({ accounts }: /*onAccountClick*/ { accounts: any }) {
+function AccountList({
+  accounts,
+  onAccountClick,
+}: {
+  accounts: object[];
+  onAccountClick: any;
+}) {
   return (
     <Stack direction="row" justifyContent="center" spacing={2}>
       {accounts.map((account: any) => (
         <Paper
-          key={account.account_id} /*onClick={() => onAccountClick(account)}*/
+          key={account.account_id}
+          onClick={() => onAccountClick(account)}
           sx={{
             p: 2,
+            cursor: "pointer",
+            // elevation: account.account_id === selectedAccountId ? 0 : 4,
           }}
         >
           <Typography variant="subtitle1" color="text.primary">
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,31 @@
+"use client";
+
 import Image from "next/image";
 import AccountList from "../../components/AccountList";
 
 async function getAccounts() {
   const res = await fetch("http://server:5001/api/accounts");
+
+  if (!res.ok) {
+    // This will activate the closest `errpor.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+async function getTransactions({
+  accountId,
+  startDate,
+  endDate,
+}: {
+  accountId: number;
+  startDate: string;
+  endDate: string;
+}) {
+  const res = await fetch(
+    `http://server:5001/api/transactions?account_id=${accountId}&start_date=${startDate}&end_date=${endDate}}`
+  );
 
   if (!res.ok) {
     // This will activate the closest `errpor.js` Error Boundary
@@ -21,7 +44,10 @@ export default async function Home() {
 
   return (
     <main>
-      <AccountList accounts={accounts} />
+      <AccountList
+        accounts={accounts}
+        onAccountClick={onAccountClick}
+      />
     </main>
   );
 }
