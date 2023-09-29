fb916b844aa68a1631192924ed537456a6504b79
Ephraim Becker <emb16@outlook.com>
2023-09-29 10:37:38 -0400

    Fixed a bug.

--- cron/utils/helperFunctions.ts
+++ cron/utils/helperFunctions.ts
@@ -1,5 +1,4 @@
 import { type Response } from "express";
-import pool from "../config/db.js";
 
 /**
  *
@@ -9,22 +8,4 @@ import pool from "../config/db.js";
  */
 export const handleError = (response: Response, message: string): void => {
   response.status(500).send(message);
-};
-
-/**
- *
- * @param query - SQL query
- * @param params - Query parameters
- * @returns - Query result
- */
-export const executeQuery = async <T = any>(
-  query: string,
-  params: any[] = []
-): Promise<T[]> => {
-  try {
-    const { rows } = await pool.query(query, params);
-    return rows;
-  } catch (error) {
-    throw new Error(error);
-  }
 };
