2fee7641bc6fab518c8c6eec6286e30aa921e016
Ephraim Becker <emb16@outlook.com>
2023-10-04 12:27:56 -0400

    Passes all tests.

--- cron/__tests__/controllers/updatePayrollsCronController.test.ts
+++ cron/__tests__/controllers/updatePayrollsCronController.test.ts
@@ -62,4 +62,35 @@ describe("POST /api/update-payrolls", () => {
     expect(mockResponse.status).toHaveBeenCalledWith(200);
     expect(mockResponse.json).toHaveBeenCalledWith(responseObj);
   });
+
+  it("should respond with an error if the script fails to execute", async () => {
+    jest.mock("child_process", () => ({
+      exec: jest.fn((command, callback) => {
+        (
+          callback as (
+            error: Error | null,
+            stdout: string,
+            stderr: string
+          ) => void
+        )(new Error("script failed"), "", "");
+      }),
+    }));
+
+    const { updatePayrollsCron } = await import(
+      "../../controllers/updatePayrollsCronController.js"
+    );
+
+    mockRequest.body = { employee_id: 1 };
+
+    await updatePayrollsCron(mockRequest as Request, mockResponse);
+
+    const responseObj = {
+      status: "error",
+      message: "Failed to create cron job",
+    };
+
+    // Assert
+    expect(mockResponse.status).toHaveBeenCalledWith(500);
+    expect(mockResponse.json).toHaveBeenCalledWith(responseObj);
+  });
 });
