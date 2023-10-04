9de4671e1556ad42c0862e6656f5dcefb846e9c6
Ephraim Becker <emb16@outlook.com>
2023-10-04 19:33:13 -0400

    Simplified.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,38 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import AccountDisplay from "../../components/accountDisplay";
 import { useAlert } from "../../context/AlertContext";
 import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import dayjs, { Dayjs } from "dayjs";
+import AccountList from "../../components/AccountList";
+import DateRange from "../../components/DateRange";
+import TransactionDisplay from "../../components/TransactionDisplay";
+import Actions from "../../components/Actions";
 
 export default function Home() {
   const { showAlert } = useAlert();
   const [accounts, setAccounts] = useState(null);
-  const [loading, setLoading] = useState(true); // Added loading state
+  const [loading, setLoading] = useState(true);
+
+  // State to keep track of the currently selected account ID
+  const [selectedAccountId, setSelectedAccountId] = useState<number | null>(
+    null
+  );
+  const [isComponentVisible, setComponentVisible] = useState(false);
+
+  const [fromDate, setFromDate] = useState<Dayjs | null>(dayjs());
+  const [toDate, setToDate] = useState<Dayjs | null>(dayjs().add(1, "month"));
+
+  function onAccountClick(account: any) {
+    if (selectedAccountId === account.account_id) {
+      // If the same account is clicked again, toggle the component's visibility
+      setComponentVisible(!isComponentVisible);
+    } else {
+      setSelectedAccountId(account.account_id);
+      setComponentVisible(false);
+    }
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,7 +60,37 @@ export default function Home() {
 
   return (
     <main>
-      <AccountDisplay accounts={accounts} />
+      <Box
+        sx={{
+          mt: 2,
+        }}
+      >
+        <AccountList
+          accounts={accounts}
+          onAccountClick={onAccountClick}
+          selectedAccountId={selectedAccountId}
+        />
+        {isComponentVisible && selectedAccountId && (
+          <Actions accountId={selectedAccountId} />
+        )}
+        {selectedAccountId && (
+          <>
+            <DateRange
+              fromDate={fromDate}
+              setFromDate={setFromDate}
+              toDate={toDate}
+              setToDate={setToDate}
+            />
+            {fromDate && toDate && (
+              <TransactionDisplay
+                accountId={selectedAccountId}
+                fromDate={fromDate}
+                toDate={toDate}
+              />
+            )}
+          </>
+        )}
+      </Box>
     </main>
   );
 }
