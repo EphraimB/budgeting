82307f30214fbe49204c495d1c05b10352271d30
Ephraim Becker <emb16@outlook.com>
2023-10-13 10:11:26 -0400

    Split table head.

--- client/components/ExpensesWidget.tsx
+++ client/components/ExpensesWidget.tsx
@@ -44,140 +44,6 @@ interface Expense {
   date_modified: string;
 }
 
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-type Order = "asc" | "desc";
-
-function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key
-): (
-  a: { [key in Key]: number | string },
-  b: { [key in Key]: number | string }
-) => number {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort<T>(
-  array: readonly T[],
-  comparator: (a: T, b: T) => number
-) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-interface HeadCell {
-  id: keyof Expense;
-  label: string;
-  numeric: boolean;
-}
-
-const headCells: readonly HeadCell[] = [
-  {
-    id: "expense_title",
-    numeric: false,
-    label: "Title",
-  },
-  {
-    id: "expense_description",
-    numeric: true,
-    label: "Description",
-  },
-  {
-    id: "expense_amount",
-    numeric: true,
-    label: "Amount ($)",
-  },
-  {
-    id: "expense_begin_date",
-    numeric: false,
-    label: "Date",
-  },
-];
-
-interface EnhancedTableProps {
-  numSelected: number;
-  onRequestSort: (
-    event: React.MouseEvent<unknown>,
-    property: keyof Expense
-  ) => void;
-  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  order: Order;
-  orderBy: string;
-  rowCount: number;
-}
-
-function EnhancedTableHead(props: EnhancedTableProps) {
-  const {
-    onSelectAllClick,
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-    onRequestSort,
-  } = props;
-  const createSortHandler =
-    (property: keyof Expense) => (event: React.MouseEvent<unknown>) => {
-      onRequestSort(event, property);
-    };
-
-  return (
-    <TableHead>
-      <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              "aria-label": "select all desserts",
-            }}
-          />
-        </TableCell>
-        {headCells.map((headCell: HeadCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? "right" : "left"}
-            padding={"normal"}
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : "asc"}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === "desc" ? "sorted descending" : "sorted ascending"}
-                </Box>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-
 interface EnhancedTableToolbarProps {
   numSelected: number;
 }
@@ -330,14 +196,17 @@ function ExpensesWidget({ selectedAccountId }: { selectedAccountId: number }) {
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - expenses.length) : 0;
 
-  const visibleRows = useMemo(
-    () =>
-      stableSort(expenses, getComparator(order, orderBy)).slice(
-        page * rowsPerPage,
-        page * rowsPerPage + rowsPerPage
-      ),
-    [order, orderBy, page, rowsPerPage]
-  );
+  const visibleRows =
+    loading || !expenses
+      ? null
+      : useMemo(
+          () =>
+            stableSort(expenses, getComparator(order, orderBy)).slice(
+              page * rowsPerPage,
+              page * rowsPerPage + rowsPerPage
+            ),
+          [order, orderBy, page, rowsPerPage]
+        );
 
   return (
     <Card sx={{ p: 2, margin: "auto", maxWidth: 500, flexGrow: 1 }}>
@@ -359,6 +228,8 @@ function ExpensesWidget({ selectedAccountId }: { selectedAccountId: number }) {
               rowCount={expenses.length}
             />
             <TableBody>
+              {loading || !expenses || !visibleRows ? (
+              ) : (
               {visibleRows.map((row, index) => {
                 const isItemSelected = isSelected(expenses.expense_title);
                 const labelId = `enhanced-table-checkbox-${index}`;
