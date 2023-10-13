6fb488d4f314aeb5a0fbd16e4a998284eadfb62e
Ephraim Becker <emb16@outlook.com>
2023-10-13 11:55:19 -0400

    Helper functions.

--- client/components/ExpensesWidget.tsx
+++ client/components/ExpensesWidget.tsx
@@ -8,6 +8,12 @@ import { useAlert } from "../context/FeedbackContext";
 import Skeleton from "@mui/material/Skeleton";
 import EnhancedTableHead from "./EnhancedTableHead";
 import EnhancedTableToolbar from "./EnhancedTableToolbar";
+import {
+  descendingComparator,
+  getComparator,
+  stableSort,
+  type Order,
+} from "../utils/helperFunctions";
 
 interface Expense {
   expense_id: number;
@@ -57,45 +63,6 @@ const headCells: readonly HeadCell[] = [
     label: "Date",
   },
 ];
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-type Order = "asc" | "desc";
-
-function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key
-): (
-  a: { [key in Key]: number | string },
-  b: { [key in Key]: number | string }
-) => number {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort<T>(
-  array: readonly T[],
-  comparator: (a: T, b: T) => number
-) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
 
 function ExpensesWidget({ selectedAccountId }: { selectedAccountId: number }) {
   const [expenses, setExpenses] = useState(null) as any[];
