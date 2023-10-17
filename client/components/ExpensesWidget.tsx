ea5d8cb9c9ab238a86ce05ced5f0dbf9474dd678
Ephraim Becker <emb16@outlook.com>
2023-10-17 09:59:50 -0400

    Added an expenses widget.

--- client/components/ExpensesWidget.tsx
+++ client/components/ExpensesWidget.tsx
@@ -2,6 +2,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
 import Link from "next/link";
+import Typography from "@mui/material/Typography";
 
 function ExpensesWidget({ account_id }: { account_id: number }) {
   return (
@@ -10,9 +11,26 @@ function ExpensesWidget({ account_id }: { account_id: number }) {
       as={`/expenses/${account_id}`}
       style={{ color: "inherit", textDecoration: "inherit" }}
     >
-      <Card sx={{ p: 2, margin: "auto", maxWidth: 500, flexGrow: 1 }}>
+      <Card
+        sx={{
+          p: 2,
+          margin: "auto",
+          maxWidth: 250,
+          flexGrow: 1,
+          background:
+            "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('/img/expenses.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          color: "white",
+        }}
+      >
         <CardHeader title="Expenses" />
-        <CardContent sx={{ flexGrow: 1 }}></CardContent>
+        <CardContent sx={{ flexGrow: 1 }}>
+          <Typography variant="body2" color="white">
+            View and manage your expenses.
+          </Typography>
+        </CardContent>
       </Card>
     </Link>
   );
