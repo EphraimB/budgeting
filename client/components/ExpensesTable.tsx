e0aa2218ea76f0fc32f9c289973c646ec3fe0d83
Ephraim Becker <emb16@outlook.com>
2023-10-17 18:10:52 -0400

    Frixed hydration error in expenses.

--- client/components/ExpensesTable.tsx
+++ client/components/ExpensesTable.tsx
@@ -332,7 +332,15 @@ function ExpensesTable({ accountId }: { accountId: number }) {
           />
           <TableBody>
             {loading || !expenses ? (
-              <Skeleton />
+              <TableRow
+                sx={{
+                  height: 53 * 5,
+                }}
+              >
+                <TableCell colSpan={6}>
+                  <Skeleton />
+                </TableCell>
+              </TableRow>
             ) : (
               visibleRows.map((row, index) => {
                 const isItemSelected = isSelected(expenses.expense_title);
