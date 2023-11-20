222b35191d53f599d3158b3f1f4e0903637b2fe3
Ephraim Becker <emb16@outlook.com>
2023-11-20 10:23:15 -0500

    Trying to get payroll jobs to work.

--- sql/init.sql
+++ sql/init.sql
@@ -302,11 +302,83 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION plpython_function_to_read_file(filename text)
-RETURNS text AS $$
-    with open(filename, 'r') as file:
-        return file.read()
-$$ LANGUAGE plpython3u;
+CREATE OR REPLACE FUNCTION process_payroll_for_employee(employee_id integer) RETURNS void AS $$
+DECLARE
+    pay_period RECORD;
+    cron_expression text;
+BEGIN
+    FOR pay_period IN SELECT make_date(extract(year from current_date)::integer, extract(month from current_date)::integer, s2.payroll_start_day::integer) AS start_date,
+            make_date(extract(year from current_date)::integer, extract(month from current_date)::integer, s1.adjusted_payroll_end_day) AS end_date,
+            SUM(s.work_days::integer) AS work_days,
+                SUM(COALESCE(
+                    e.regular_hours * e.hourly_rate * work_days
+                ))::numeric(20, 2) AS gross_pay,
+                SUM(COALESCE(
+                    e.regular_hours * e.hourly_rate * (1 - COALESCE(pt.rate, 0)) * work_days
+                ))::numeric(20, 2) AS net_pay,
+            SUM(COALESCE(
+                    e.regular_hours * work_days
+                ))::numeric(20, 2) AS hours_worked
+                FROM employee e
+            CROSS JOIN LATERAL (
+            SELECT
+                payroll_start_day,
+                CASE 
+                    WHEN payroll_end_day > EXTRACT(DAY FROM DATE_TRUNC('MONTH', current_date) + INTERVAL '1 MONTH - 1 DAY') 
+                    THEN EXTRACT(DAY FROM DATE_TRUNC('MONTH', current_date) + INTERVAL '1 MONTH - 1 DAY')
+                    ELSE payroll_end_day 
+                    END AS unadjusted_payroll_end_day
+                FROM payroll_dates
+            ) s2
+            CROSS JOIN LATERAL (
+                SELECT
+                s2.payroll_start_day,
+                CASE
+                    WHEN EXTRACT(DOW FROM MAKE_DATE(EXTRACT(YEAR FROM current_date)::integer, EXTRACT(MONTH FROM current_date)::integer, s2.unadjusted_payroll_end_day::integer)) = 0 
+                        THEN s2.unadjusted_payroll_end_day - 2 -- If it's a Sunday, subtract 2 days to get to Friday
+                    WHEN EXTRACT(DOW FROM MAKE_DATE(EXTRACT(YEAR FROM current_date)::integer, EXTRACT(MONTH FROM current_date)::integer, s2.unadjusted_payroll_end_day::integer)) = 6
+                        THEN s2.unadjusted_payroll_end_day - 1 -- If it's a Saturday, subtract 1 day to get to Friday
+                    ELSE s2.unadjusted_payroll_end_day
+                END::integer AS adjusted_payroll_end_day
+            ) s1
+            CROSS JOIN LATERAL(
+                SELECT
+                generate_series(
+                make_date(extract(year from current_date)::integer, extract(month from current_date)::integer, s1.payroll_start_day), 
+                make_date(extract(year from current_date)::integer, extract(month from current_date)::integer, s1.adjusted_payroll_end_day),
+                '1 day'
+                )
+                ) AS dates(date)
+            CROSS JOIN LATERAL (
+                SELECT
+                SUM(CASE 
+                    WHEN (work_schedule::integer & (1 << (6 - extract(dow from dates.date)::integer))) <> 0
+                    THEN 1 
+                    ELSE 0 
+                END) AS work_days
+                FROM employee e
+            ) s
+            LEFT JOIN (
+                SELECT employee_id, SUM(rate) AS rate
+                FROM payroll_taxes
+                GROUP BY employee_id
+            ) pt ON e.employee_id = pt.employee_id
+            WHERE e.employee_id = employee_id AND work_days <> 0
+            GROUP BY s2.payroll_start_day, e.employee_id, s.work_days, s1.adjusted_payroll_end_day
+            ORDER BY start_date, end_date
+
+        LOOP
+          cron_expression := '0 0 ' || EXTRACT(DAY FROM pay_period.end_date) || ' ' || EXTRACT(MONTH FROM pay_period.end_date) || ' *';
+
+          EXECUTE format('SELECT cron.schedule(%L, %L, %L)',
+            'payroll-' || pay_period.employee_id,
+            cron_expression,
+            format($$INSERT INTO transaction_history (account_id, transaction_amount, transaction_tax_rate, transaction_title, transaction_description, date_created, date_modified) 
+                   VALUES ((SELECT account_id FROM employee WHERE employee_id = %s), %s, 0, 'Payroll', 'Payroll for %s to %s', current_timestamp, current_timestamp)$$,
+                   pay_period.employee_id, pay_period.net_pay, pay_period.start_date, pay_period.end_date));
+        END LOOP;
+END;
+$$ LANGUAGE plpgsql;
 
 CREATE TRIGGER update_accounts_dates
 BEFORE INSERT OR UPDATE ON accounts
--- sql/zpayrollsEmployee.sql
+++ sql/zpayrollsEmployee.sql
@@ -1,17 +1,11 @@
+SELECT cron.schedule('monthly-payroll-job', '0 0 1 * *', $$
 DO $$
 DECLARE
     employee_record RECORD;
-    query text;
 BEGIN
-    -- Load the query from an external file
-    query := plpython_function_to_read_file('/zgetPayrollsByEmployee.txt');
-
     FOR employee_record IN SELECT employee_id FROM employee
     LOOP
-        -- Replace placeholder with actual employee ID
-        query := replace(query, '%s', employee_record.employee_id::text);
-
-        -- Execute the query
-        EXECUTE query;
+        PERFORM process_payroll_for_employee(employee_record.employee_id);
     END LOOP;
 END$$;
+$$);
