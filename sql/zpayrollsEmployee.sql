1fb2e619c86d5cafae7da148fb9fb234cc719c27
Ephraim Becker <emb16@outlook.com>
2023-11-20 11:36:38 -0500

    Hopefully fixed the problem.

--- sql/zpayrollsEmployee.sql
+++ sql/zpayrollsEmployee.sql
@@ -1,11 +1,4 @@
 SELECT cron.schedule('monthly-payroll-job', '0 0 1 * *', $$
-DO $$
-DECLARE
-    employee_record RECORD;
-BEGIN
-    FOR employee_record IN SELECT employee_id FROM employee
-    LOOP
-        PERFORM process_payroll_for_employee(employee_record.employee_id);
-    END LOOP;
-END$$;
+    WITH employee_list AS (SELECT employee_id FROM employee)
+    SELECT process_payroll_for_employee(employee_id) FROM employee_list;
 $$);
