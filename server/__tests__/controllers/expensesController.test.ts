e3f2aebebe65cbc09a946b5f6e55494755bfeb0d
Ephraim Becker <emb16@outlook.com>
2023-12-08 12:16:40 -0500

    Updated more tests.

--- server/__tests__/controllers/expensesController.test.ts
+++ server/__tests__/controllers/expensesController.test.ts
@@ -250,50 +250,16 @@ describe('GET /api/expenses', () => {
 
     it('should respond with an array of expenses with account id and id', async () => {
         // Arrange
-        const expenses = [
-            {
-                expense_id: 1,
-                account_id: 1,
-                tax_id: 1,
-                expense_amount: 50,
-                expense_title: 'Test Expense',
-                expense_description: 'Test Expense to test the expense route',
-                frequency_type: 2,
-                frequency_type_variable: null,
-                frequency_day_of_month: null,
-                frequency_day_of_week: null,
-                frequency_week_of_month: null,
-                frequency_month_of_year: null,
-                expense_subsidized: 0,
-                expense_begin_date: '2020-01-01',
-                date_created: '2020-01-01',
-                date_modified: '2020-01-01',
-            },
-        ];
-
-        const expectedResponse = [
-            {
-                id: 1,
-                account_id: 1,
-                tax_id: 1,
-                amount: 50,
-                title: 'Test Expense',
-                description: 'Test Expense to test the expense route',
-                frequency_type: 2,
-                frequency_type_variable: null,
-                frequency_day_of_month: null,
-                frequency_day_of_week: null,
-                frequency_week_of_month: null,
-                frequency_month_of_year: null,
-                subsidized: 0,
-                begin_date: '2020-01-01',
-                next_date: '2020-01-01',
-                date_created: '2020-01-01',
-                date_modified: '2020-01-01',
-            },
-        ];
-
-        mockModule([expenses], [], [], []);
+        mockModule(
+            [
+                expenses
+                    .filter((expense) => expense.account_id === 1)
+                    .filter((exnse) => exnse.expense_id === 1),
+            ],
+            [],
+            [],
+            [],
+        );
 
         const { getExpenses } = await import(
             '../../controllers/expensesController.js'
@@ -306,7 +272,11 @@ describe('GET /api/expenses', () => {
 
         // Assert
         expect(mockResponse.status).toHaveBeenCalledWith(200);
-        expect(mockResponse.json).toHaveBeenCalledWith(expectedResponse);
+        expect(mockResponse.json).toHaveBeenCalledWith(
+            expectedResponse
+                .filter((expense) => expense.account_id === 1)
+                .filter((exnse) => exnse.id === 1),
+        );
     });
 
     it('should handle errors correctly with account id and id', async () => {
