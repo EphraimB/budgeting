13783fc50e31beb2b71ab1647c0381ed7fbf320c
Ephraim Becker <emb16@outlook.com>
2023-12-08 11:02:09 -0500

    Put mockModule in a seperate file.

--- server/__tests__/controllers/expensesController.test.ts
+++ server/__tests__/controllers/expensesController.test.ts
@@ -1,6 +1,6 @@
 import { jest } from '@jest/globals';
 import { type Request, type Response } from 'express';
-import { parseIntOrFallback } from '../../utils/helperFunctions.js';
+import { mockModule } from '../__mocks__/mockModule';
 
 jest.mock('../../config/winston', () => ({
     logger: {
@@ -27,81 +27,6 @@ beforeEach(() => {
 afterEach(() => {
     jest.resetModules();
 });
-
-/**
- *
- * @param executeQueryResponses - Array of responses for executeQuery
- * @param handleErrorResponses - Array of responses for handleError
- * @param scheduleQueryResponses - Array of responses for scheduleQuery
- * @param unscheduleQueryResponses - Array of responses for unscheduleQuery
- * Mock module with mock implementations for executeQuery, handleError, scheduleQuery, and unscheduleQuery
- */
-const mockModule = (
-    executeQueryResponses: any = [], // Array of responses for executeQuery
-    handleErrorResponses: any = [], // Array of responses for handleError
-    scheduleQueryResponses: any = [], // Array of responses for scheduleQuery
-    unscheduleQueryResponses: any = [], // Array of responses for unscheduleQuery
-) => {
-    const executeQuery = jest.fn();
-    const handleError = jest.fn();
-    const scheduleQuery = jest.fn();
-    const unscheduleQuery = jest.fn();
-
-    // Set up mock implementations for executeQuery
-    executeQueryResponses.forEach((response: Response) => {
-        if (response instanceof Error) {
-            executeQuery.mockImplementationOnce(() => Promise.reject(response));
-        } else {
-            executeQuery.mockImplementationOnce(() =>
-                Promise.resolve(response),
-            );
-        }
-    });
-
-    // Set up mock implementations for handleError
-    handleErrorResponses.forEach((response: any) => {
-        handleError.mockImplementationOnce((res: any, message: any) => {
-            res.status(response.status || 400).json({
-                message: response.message || message,
-            });
-        });
-    });
-
-    // Set up mock implementations for scheduleQuery
-    scheduleQueryResponses.forEach((response: Response) => {
-        if (response instanceof Error) {
-            scheduleQuery.mockImplementationOnce(() =>
-                Promise.reject(response),
-            );
-        } else {
-            scheduleQuery.mockImplementationOnce(() =>
-                Promise.resolve(response),
-            );
-        }
-    });
-
-    // Set up mock implementations for unscheduleQuery
-    unscheduleQueryResponses.forEach((response: Response) => {
-        if (response instanceof Error) {
-            unscheduleQuery.mockImplementationOnce(() =>
-                Promise.reject(response),
-            );
-        } else {
-            unscheduleQuery.mockImplementationOnce(() =>
-                Promise.resolve(response),
-            );
-        }
-    });
-
-    jest.mock('../../utils/helperFunctions.js', () => ({
-        executeQuery,
-        handleError,
-        scheduleQuery,
-        unscheduleQuery,
-        parseIntOrFallback,
-        nextTransactionFrequencyDate: jest.fn().mockReturnValue('2020-01-01'),
-    }));
-};
 
 describe('GET /api/expenses', () => {
     it('should respond with an array of expenses', async () => {
