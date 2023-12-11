62c88bb7f4f27091ccc2ccef4cd622b007bcc939
Ephraim Becker <emb16@outlook.com>
2023-12-11 09:57:33 -0500

    Updated more tests.

--- server/__tests__/controllers/commuteScheduleController.test.ts
+++ server/__tests__/controllers/commuteScheduleController.test.ts
@@ -192,53 +192,44 @@ describe('GET /api/expenses/commute/schedule', () => {
             message: 'Error getting schedule for given account_id',
         });
     });
+
+    it('should respond with an array of schedules with an id', async () => {
+        // Arrange
+        mockModule([
+            commuteSchedule.filter((s) => s.commute_schedule_id === 1),
+        ]);
+
+        const { getCommuteSchedule } = await import(
+            '../../controllers/commuteScheduleController.js'
+        );
+
+        mockRequest.query = { account_id: null, id: 1 };
+
+        // Call the function with the mock request and response
+        await getCommuteSchedule(mockRequest as Request, mockResponse);
+
+        const responseObj = {
+            schedule: [
+                {
+                    day_of_week: 1,
+                    passes: [
+                        {
+                            commute_schedule_id: 1,
+                            start_time: '08:00:00',
+                            duration: 60,
+                            pass: 'LIRR Peak',
+                            fare_amount: 10.75,
+                        },
+                    ],
+                },
+            ],
+        };
+
+        // Assert
+        expect(mockResponse.status).toHaveBeenCalledWith(200);
+        expect(mockResponse.json).toHaveBeenCalledWith(responseObj);
+    });
 });
-
-//     it('should respond with an array of schedules with an id', async () => {
-//         // Arrange
-//         mockModule([
-//             {
-//                 commute_schedule_id: 1,
-//                 account_id: 1,
-//                 day_of_week: 1,
-//                 commute_ticket_id: 1,
-//                 start_time: '08:00:00',
-//                 duration: 60,
-//                 pass: 'LIRR Peak',
-//                 fare_amount: 10.75,
-//             },
-//         ]);
-
-//         const { getCommuteSchedule } = await import(
-//             '../../controllers/commuteScheduleController.js'
-//         );
-
-//         mockRequest.query = { account_id: null, id: 1 };
-
-//         // Call the function with the mock request and response
-//         await getCommuteSchedule(mockRequest as Request, mockResponse);
-
-//         const responseObj = {
-//             schedule: [
-//                 {
-//                     day_of_week: 1,
-//                     passes: [
-//                         {
-//                             commute_schedule_id: 1,
-//                             start_time: '08:00:00',
-//                             duration: 60,
-//                             pass: 'LIRR Peak',
-//                             fare_amount: 10.75,
-//                         },
-//                     ],
-//                 },
-//             ],
-//         };
-
-//         // Assert
-//         expect(mockResponse.status).toHaveBeenCalledWith(200);
-//         expect(mockResponse.json).toHaveBeenCalledWith(responseObj);
-//     });
 
 //     it('should handle errors correctly with an id', async () => {
 //         // Arrange
