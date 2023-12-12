f2a31802450cf27d987ae5c1ee186b877325b6b0
Ephraim Becker <emb16@outlook.com>
2023-12-11 21:43:19 -0500

    Updated more tests.

--- server/__tests__/controllers/commuteScheduleController.test.ts
+++ server/__tests__/controllers/commuteScheduleController.test.ts
@@ -566,63 +566,63 @@ describe('PUT /api/expenses/commute/schedule/:id', () => {
         expect(mockResponse.status).toHaveBeenCalledWith(404);
         expect(mockResponse.send).toHaveBeenCalledWith('Schedule not found');
     });
+
+    it('should respond with the updated schedule', async () => {
+        const newSchedule = [
+            {
+                commute_schedule_id: 1,
+                commute_system_id: 1,
+                account_id: 1,
+                day_of_week: 1,
+                fare_detail_id: 1,
+                start_time: '08:00:00',
+                duration: 60,
+                fare_amount: 10.75,
+                pass: 'LIRR Peak',
+                date_created: '2021-01-01',
+                date_modified: '2021-01-01',
+            },
+        ];
+
+        mockModule([newSchedule]);
+
+        const { updateCommuteScheduleReturnObject } = await import(
+            '../../controllers/commuteScheduleController.js'
+        );
+
+        mockRequest = { commute_schedule_id: 1 };
+        mockRequest.alerts = [];
+
+        await updateCommuteScheduleReturnObject(
+            mockRequest as Request,
+            mockResponse,
+        );
+
+        const responseObj = {
+            schedule: [
+                {
+                    id: 1,
+                    commute_system_id: 1,
+                    account_id: 1,
+                    day_of_week: 1,
+                    fare_detail_id: 1,
+                    start_time: '08:00:00',
+                    duration: 60,
+                    fare_amount: 10.75,
+                    pass: 'LIRR Peak',
+                    timed_pass_duration: null,
+                    date_created: '2021-01-01',
+                    date_modified: '2021-01-01',
+                },
+            ],
+            alerts: [],
+        };
+
+        // Assert
+        expect(mockResponse.status).toHaveBeenCalledWith(200);
+        expect(mockResponse.json).toHaveBeenCalledWith(responseObj);
+    });
 });
-
-//     it('should respond with the updated schedule', async () => {
-//         const updatedSchedule = [
-//             {
-//                 commute_schedule_id: 1,
-//                 commute_system_id: 1,
-//                 account_id: 1,
-//                 day_of_week: 1,
-//                 fare_detail_id: 1,
-//                 start_time: '08:00:00',
-//                 duration: 60,
-//                 fare_amount: 10.75,
-//                 pass: 'LIRR Peak',
-//                 date_created: '2021-01-01',
-//                 date_modified: '2021-01-01',
-//             },
-//         ];
-
-//         mockModule(updatedSchedule);
-
-//         const { updateCommuteScheduleReturnObject } = await import(
-//             '../../controllers/commuteScheduleController.js'
-//         );
-
-//         mockRequest = { commute_schedule_id: 1 };
-//         mockRequest.alerts = [];
-
-//         await updateCommuteScheduleReturnObject(
-//             mockRequest as Request,
-//             mockResponse,
-//         );
-
-//         const responseObj = {
-//             schedule: [
-//                 {
-//                     commute_schedule_id: 1,
-//                     commute_system_id: 1,
-//                     account_id: 1,
-//                     day_of_week: 1,
-//                     fare_detail_id: 1,
-//                     start_time: '08:00:00',
-//                     duration: 60,
-//                     fare_amount: 10.75,
-//                     pass: 'LIRR Peak',
-//                     timed_pass_duration: null,
-//                     date_created: '2021-01-01',
-//                     date_modified: '2021-01-01',
-//                 },
-//             ],
-//             alerts: [],
-//         };
-
-//         // Assert
-//         expect(mockResponse.status).toHaveBeenCalledWith(200);
-//         expect(mockResponse.json).toHaveBeenCalledWith(responseObj);
-//     });
 
 //     it('should handle errors correctly', async () => {
 //         // Arrange
