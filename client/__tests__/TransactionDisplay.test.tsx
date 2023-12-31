a7a20fe9b5c052ab25a84d72316a029ffe7e1d50
Ephraim Becker <emb16@outlook.com>
2023-12-31 12:05:46 -0500

    Trying to pass TransactionDisplay.

--- client/__tests__/TransactionDisplay.test.tsx
+++ client/__tests__/TransactionDisplay.test.tsx
@@ -1,8 +1,33 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import dayjs from "dayjs";
 import { cleanup } from "@testing-library/react";
+import dayjs from "dayjs";
+
+const mockTransactions = [
+  {
+    id: "eqawd3qfd3",
+    transaction_id: 1,
+    title: "Test Transaction",
+    description: "A test transaction",
+    date: dayjs("2021-09-03"),
+    date_modified: dayjs("2021-09-03"),
+    amount: 100,
+    tax_rate: 0.05,
+    total_amount: 200,
+  },
+  {
+    id: "eqawd3qfd3",
+    transaction_id: 2,
+    title: "Test Transaction 2",
+    description: "A test transaction 2",
+    date: dayjs("2021-09-03"),
+    date_modified: dayjs("2021-09-03"),
+    amount: 100,
+    tax_rate: 0.05,
+    total_amount: 200,
+  },
+];
 
 describe("TransactionDisplay", () => {
   afterEach(() => {
@@ -12,78 +37,36 @@ describe("TransactionDisplay", () => {
   });
 
   it("displays loader while fetching data", async () => {
-    jest.mock("swr", () => ({
-      __esModule: true,
-      default: jest
-        .fn()
-        .mockReturnValue({ data: null, error: null, isLoading: true }),
-    }));
-
     const TransactionDisplay = (
       await import("../components/TransactionDisplay")
     ).default;
 
     const { getByRole } = render(
-      <TransactionDisplay accountId={1} fromDate={dayjs()} toDate={dayjs()} />
+      <TransactionDisplay transactions={mockTransactions} />
     );
 
     expect(getByRole("progressbar")).toBeInTheDocument();
   });
 
   it("displays error message on fetch error", async () => {
-    jest.mock("swr", () => ({
-      __esModule: true,
-      default: jest.fn().mockReturnValue({
-        data: null,
-        error: new Error("Failed to fetch"),
-        isLoading: false,
-      }),
-    }));
-
     const TransactionDisplay = (
       await import("../components/TransactionDisplay")
     ).default;
 
     const { getByText } = render(
-      <TransactionDisplay accountId={1} fromDate={dayjs()} toDate={dayjs()} />
+      <TransactionDisplay transactions={mockTransactions} />
     );
 
     expect(getByText("failed to load")).toBeInTheDocument();
   });
 
   it("displays transactions when data is fetched", async () => {
-    const mockData = [
-      {
-        transactions: [
-          {
-            id: "eqawd3qfd3",
-            date: new Date().toISOString(),
-            title: "Test Transaction",
-            description: "A test transaction",
-            amount: 100,
-            tax_rate: 0.05,
-            total_amount: 105,
-            balance: 1000,
-          },
-        ],
-      },
-    ];
-
-    jest.mock("swr", () => ({
-      __esModule: true,
-      default: jest.fn().mockReturnValue({
-        data: mockData,
-        error: null,
-        isLoading: false,
-      }),
-    }));
-
     const TransactionDisplay = (
       await import("../components/TransactionDisplay")
     ).default;
 
     const { getByText } = render(
-      <TransactionDisplay accountId={1} fromDate={dayjs()} toDate={dayjs()} />
+      <TransactionDisplay transactions={mockTransactions} />
     );
 
     expect(getByText("Test Transaction")).toBeInTheDocument();
