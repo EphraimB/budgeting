a02ffd36a2360fe34d851ed6f2325b77b1c2c070
Ephraim Becker <emb16@outlook.com>
2024-01-01 08:19:07 -0500

    Trying to figure out AccountActionMenu test.

---
+++ client/__tests__/AccountActionsMenu.test.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import AccountActionsMenu from "../components/AccountActionsMenu";
+
+const setAccountModes = jest.fn();
+
+const anchorEl = null;
+
+const open = Boolean(anchorEl);
+
+const handleClose = () => jest.fn();
+
+describe("AccountActionsMenu", () => {
+  it("displays the menu when the button is clicked", async () => {
+    const { getByText } = render(
+      <AccountActionsMenu
+        anchorEl={anchorEl}
+        open={open}
+        handleClose={handleClose}
+        setAccountModes={setAccountModes}
+        accountId={1}
+      />
+    );
+
+    expect(getByText("Edit")).toBeInTheDocument();
+    expect(getByText("Delete")).toBeInTheDocument();
+    expect(getByText("Deposit")).toBeInTheDocument();
+    expect(getByText("Withdraw")).toBeInTheDocument();
+  });
+});
