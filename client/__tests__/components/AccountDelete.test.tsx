60605a7a214c1cf515fb02b71d5c5aed73a25d1a
Ephraim Becker <emb16@outlook.com>
2024-01-02 11:03:20 -0500

    Added AccountDelete test.

---
+++ client/__tests__/components/AccountDelete.test.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import AccountList from "../../components/AccountList";
+import "@testing-library/jest-dom";
+import AccountDelete from "../../components/AccountDelete";
+
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(),
+}));
+
+describe("AccountList", () => {
+  it("renders", () => {
+    const { getByText } = render(
+      <AccountDelete
+        account={{
+          account_id: 1,
+          account_name: "Test Account",
+          account_type: "Checking",
+          account_balance: 0,
+        }}
+        setAccountModes={() => {}}
+      />
+    );
+    expect(getByText("Cancel")).toBeInTheDocument();
+  });
+});
