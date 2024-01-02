f19dce83db268e2f1759ce2f1dba46473d2406db
Ephraim Becker <emb16@outlook.com>
2024-01-02 12:55:26 -0500

    Changed fetch url.

--- client/services/actions/transactionHistory.ts
+++ client/services/actions/transactionHistory.ts
@@ -3,16 +3,13 @@
 import { revalidatePath } from "next/cache";
 
 export async function addTransactionHistory(expense: any) {
-  const response = await fetch(
-    "http://localhost:5001/api/transactions/history",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(expense),
-    }
-  );
+  const response = await fetch("http://server:5001/api/transactions/history", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(expense),
+  });
   const result = await response.json();
 
   revalidatePath("/");
