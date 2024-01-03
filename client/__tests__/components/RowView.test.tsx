9a6daaa1258f8b0f732e198dca369703a4c41e27
Ephraim Becker <emb16@outlook.com>
2024-01-03 10:46:23 -0500

    Added row view test.

---
+++ client/__tests__/components/RowView.test.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import RowView from "../../components/RowView";
+import "@testing-library/jest-dom";
+import { Table, TableBody } from "@mui/material";
+
+describe("RowView", () => {
+  test("renders RowView component", () => {
+    const row = {
+      id: 1,
+      title: "Test",
+      description: "Just a test",
+      amount: 1,
+      frequency: 2,
+      next_date: "2021-10-10",
+      tax_id: null,
+      subsidized: 0,
+      account_id: 1,
+    };
+
+    const index = 1;
+    const handleClick = jest.fn();
+    const isSelected = jest.fn();
+    const taxes = [
+      {
+        tax_id: 1,
+        tax_rate: 1,
+      },
+    ];
+    const getExpenseFrequency = jest.fn();
+
+    const { getByText } = render(
+      <Table>
+        <TableBody>
+          <RowView
+            row={row}
+            index={index}
+            handleClick={handleClick}
+            isSelected={isSelected}
+            taxes={taxes}
+            getExpenseFrequency={getExpenseFrequency}
+          />
+        </TableBody>
+      </Table>
+    );
+
+    expect(getByText("Test")).toBeInTheDocument();
+    expect(getByText("Just a test")).toBeInTheDocument();
+    expect(getByText("$1.00")).toBeInTheDocument();
+  });
+});
