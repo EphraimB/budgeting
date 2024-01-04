68654db2dccc269da513e62a695f20b07158f2e8
Ephraim Becker <emb16@outlook.com>
2024-01-04 13:28:01 -0500

    Added test with end date enabled.

--- client/__tests__/components/RowAdd.test.tsx
+++ client/__tests__/components/RowAdd.test.tsx
@@ -3,6 +3,7 @@ import { render } from "@testing-library/react";
 import RowAdd from "../../components/RowAdd";
 import "@testing-library/jest-dom";
 import { Table, TableBody } from "@mui/material";
+import { act } from "react-dom/test-utils";
 
 describe("RowAdd", () => {
   it("renders RowAdd component", () => {
@@ -64,6 +65,45 @@ describe("RowAdd", () => {
     expect(getByLabelText("Expense begin date")).toBeInTheDocument();
     expect(getByLabelText("Tax")).toBeInTheDocument();
     expect(getByDisplayValue("1")).toBeInTheDocument();
+    expect(getByLabelText("Subsidized")).toBeInTheDocument();
+    expect(getByText("Add expense")).toBeInTheDocument();
+    expect(getByText("Cancel")).toBeInTheDocument();
+  });
+
+  it("renders RowAdd component with end date checked", () => {
+    const setShowAddForm = jest.fn();
+    const handleAdd = jest.fn();
+
+    const { getByLabelText, getByText } = render(
+      <Table>
+        <TableBody>
+          <RowAdd
+            account_id={1}
+            taxes={[]}
+            setShowAddForm={setShowAddForm}
+            handleAdd={handleAdd}
+          />
+        </TableBody>
+      </Table>
+    );
+
+    // Check end date
+    const endDate = getByLabelText("Expense end date");
+    expect(endDate).toBeInTheDocument();
+    expect(endDate).not.toBeChecked();
+
+    act(() => {
+      endDate.click();
+    });
+
+    expect(endDate).toBeChecked();
+
+    expect(getByLabelText("Title")).toBeInTheDocument();
+    expect(getByLabelText("Description")).toBeInTheDocument();
+    expect(getByLabelText("Amount")).toBeInTheDocument();
+    expect(getByLabelText("Frequency")).toBeInTheDocument();
+    expect(getByLabelText("Expense begin date")).toBeInTheDocument();
+    expect(getByLabelText("Tax")).toBeInTheDocument();
     expect(getByLabelText("Subsidized")).toBeInTheDocument();
     expect(getByText("Add expense")).toBeInTheDocument();
     expect(getByText("Cancel")).toBeInTheDocument();
