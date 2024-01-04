12efd385f77fed156aaf396d6fb1134f4dd1f508
Ephraim Becker <emb16@outlook.com>
2024-01-04 13:19:58 -0500

    Added another test.

--- client/__tests__/components/RowAdd.test.tsx
+++ client/__tests__/components/RowAdd.test.tsx
@@ -6,18 +6,6 @@ import { Table, TableBody } from "@mui/material";
 
 describe("RowAdd", () => {
   test("renders RowAdd component", () => {
-    const row = {
-      id: 1,
-      title: "Test",
-      description: "Just a test",
-      amount: 1,
-      frequency: 2,
-      next_date: "2021-10-10",
-      tax_id: null,
-      subsidized: 0,
-      account_id: 1,
-    };
-
     const setShowAddForm = jest.fn();
     const handleAdd = jest.fn();
 
@@ -40,6 +28,42 @@ describe("RowAdd", () => {
     expect(getByLabelText("Frequency")).toBeInTheDocument();
     expect(getByLabelText("Expense begin date")).toBeInTheDocument();
     expect(getByLabelText("Tax")).toBeInTheDocument();
+    expect(getByLabelText("Subsidized")).toBeInTheDocument();
+    expect(getByText("Add expense")).toBeInTheDocument();
+    expect(getByText("Cancel")).toBeInTheDocument();
+  });
+
+  test("renders RowAdd component with taxes", () => {
+    const setShowAddForm = jest.fn();
+    const handleAdd = jest.fn();
+
+    const { getByLabelText, getByText, getByDisplayValue } = render(
+      <Table>
+        <TableBody>
+          <RowAdd
+            account_id={1}
+            taxes={[
+              {
+                id: 1,
+                title: "IVA",
+                rate: 0.05,
+                account_id: 1,
+              },
+            ]}
+            setShowAddForm={setShowAddForm}
+            handleAdd={handleAdd}
+          />
+        </TableBody>
+      </Table>
+    );
+
+    expect(getByLabelText("Title")).toBeInTheDocument();
+    expect(getByLabelText("Description")).toBeInTheDocument();
+    expect(getByLabelText("Amount")).toBeInTheDocument();
+    expect(getByLabelText("Frequency")).toBeInTheDocument();
+    expect(getByLabelText("Expense begin date")).toBeInTheDocument();
+    expect(getByLabelText("Tax")).toBeInTheDocument();
+    expect(getByDisplayValue("1")).toBeInTheDocument();
     expect(getByLabelText("Subsidized")).toBeInTheDocument();
     expect(getByText("Add expense")).toBeInTheDocument();
     expect(getByText("Cancel")).toBeInTheDocument();
