62d92b094b0c718df8988b75e83669113b638d18
Ephraim Becker <emb16@outlook.com>
2024-01-23 11:20:20 -0500

    Added another assertion to RowEdit.

--- client/__tests__/components/RowEdit.test.tsx
+++ client/__tests__/components/RowEdit.test.tsx
@@ -131,4 +131,48 @@ describe("RowEdit", () => {
     expect(screen.getByText("Update expense")).toBeInTheDocument();
     expect(screen.getByText("Cancel")).toBeInTheDocument();
   });
+
+  it("renders RowEdit component with yearly frequency", () => {
+    const setRowModes = jest.fn();
+    const handleEdit = jest.fn();
+
+    render(
+      <Table>
+        <TableBody>
+          <RowEdit
+            account_id={1}
+            taxes={[]}
+            type={0}
+            row={{
+              id: 1,
+              title: "Rent",
+              description: "Monthly rent",
+              amount: 1000,
+              frequency_type: 3,
+              frequency: 0,
+              expense_begin_date: "2021-10-01",
+              subsidized: false,
+              day_of_week: null,
+              week_of_month: null,
+              account_id: 1,
+            }}
+            setRowModes={setRowModes}
+            handleEdit={handleEdit}
+          />
+        </TableBody>
+      </Table>
+    );
+
+    expect(screen.getByLabelText("Title")).toBeInTheDocument();
+    expect(screen.getByLabelText("Description")).toBeInTheDocument();
+    expect(screen.getByLabelText("Amount")).toBeInTheDocument();
+    expect(screen.getByLabelText("Frequency")).toBeInTheDocument();
+    expect(screen.getByLabelText("Expense begin date")).toBeInTheDocument();
+    expect(screen.getByLabelText("Subsidized")).toBeInTheDocument();
+    expect(screen.getByLabelText("Day of week")).toBeInTheDocument();
+    expect(screen.getByLabelText("Week of month")).toBeInTheDocument();
+    expect(screen.getByLabelText("Month of year")).toBeInTheDocument();
+    expect(screen.getByText("Update expense")).toBeInTheDocument();
+    expect(screen.getByText("Cancel")).toBeInTheDocument();
+  });
 });
