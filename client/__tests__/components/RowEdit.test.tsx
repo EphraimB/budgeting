3036c0e7c93e4a8513f017f4e4aa89a676a02158
Ephraim Becker <emb16@outlook.com>
2024-01-23 11:23:35 -0500

    Added another assertion to RowEdit.

--- client/__tests__/components/RowEdit.test.tsx
+++ client/__tests__/components/RowEdit.test.tsx
@@ -175,4 +175,60 @@ describe("RowEdit", () => {
     expect(screen.getByText("Update expense")).toBeInTheDocument();
     expect(screen.getByText("Cancel")).toBeInTheDocument();
   });
+
+  it("renders RowEdit component with type 2", () => {
+    const setRowModes = jest.fn();
+    const handleEdit = jest.fn();
+
+    render(
+      <Table>
+        <TableBody>
+          <RowEdit
+            account_id={1}
+            taxes={[
+              {
+                id: 1,
+                name: "GST",
+                rate: 5,
+                account_id: 1,
+              },
+              {
+                id: 2,
+                name: "PST",
+                rate: 7,
+                account_id: 1,
+              },
+            ]}
+            type={2}
+            row={{
+              id: 1,
+              title: "Rent",
+              description: "Monthly rent",
+              amount: 1000,
+              frequency_type: 0,
+              frequency: 0,
+              expense_begin_date: "2021-10-01",
+              subsidized: false,
+              day_of_week: null,
+              week_of_month: null,
+              account_id: 1,
+            }}
+            setRowModes={setRowModes}
+            handleEdit={handleEdit}
+          />
+        </TableBody>
+      </Table>
+    );
+
+    expect(screen.getByLabelText("Title")).toBeInTheDocument();
+    expect(screen.getByLabelText("Description")).toBeInTheDocument();
+    expect(screen.getByLabelText("Amount")).toBeInTheDocument();
+    expect(screen.getByLabelText("Frequency")).toBeInTheDocument();
+    expect(screen.getByLabelText("Expense begin date")).toBeInTheDocument();
+    expect(screen.getByLabelText("Subsidized")).toBeInTheDocument();
+    expect(screen.getByLabelText("Tax")).toBeInTheDocument();
+    expect(screen.getByText("None - 0%")).toBeInTheDocument();
+    expect(screen.getByText("Update expense")).toBeInTheDocument();
+    expect(screen.getByText("Cancel")).toBeInTheDocument();
+  });
 });
