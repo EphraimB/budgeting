2a756ed6b8e261a086070669a85ebffe3de2aaf6
Ephraim Becker <emb16@outlook.com>
2024-01-22 20:43:20 -0500

    Added more assertions to RowAdd.

--- client/__tests__/components/RowAdd.test.tsx
+++ client/__tests__/components/RowAdd.test.tsx
@@ -147,3 +147,73 @@ describe("RowAdd", () => {
     expect(screen.getByText("Cancel")).toBeInTheDocument();
   });
 });
+
+it("renders RowAdd component with frequency weekly", async () => {
+  const setShowAddForm = jest.fn();
+  const handleAdd = jest.fn();
+
+  render(
+    <Table>
+      <TableBody>
+        <RowAdd
+          account_id={1}
+          taxes={[]}
+          setShowAddForm={setShowAddForm}
+          handleAdd={handleAdd}
+          type={0}
+        />
+      </TableBody>
+    </Table>
+  );
+
+  const selectElement = screen.getByLabelText("Frequency");
+  fireEvent.mouseDown(selectElement);
+
+  const dailyOption = screen.getByTestId("weekly-menu-item");
+  fireEvent.click(dailyOption);
+
+  expect(screen.getByLabelText("Title")).toBeInTheDocument();
+  expect(screen.getByLabelText("Description")).toBeInTheDocument();
+  expect(screen.getByLabelText("Amount")).toBeInTheDocument();
+  expect(screen.getByLabelText("Expense begin date")).toBeInTheDocument();
+  expect(screen.getByLabelText("Subsidized")).toBeInTheDocument();
+  expect(screen.getByLabelText("Day of week")).toBeInTheDocument();
+  expect(screen.getByText("Add expense")).toBeInTheDocument();
+  expect(screen.getByText("Cancel")).toBeInTheDocument();
+});
+
+it("renders RowAdd component with frequency yearly", async () => {
+  const setShowAddForm = jest.fn();
+  const handleAdd = jest.fn();
+
+  render(
+    <Table>
+      <TableBody>
+        <RowAdd
+          account_id={1}
+          taxes={[]}
+          setShowAddForm={setShowAddForm}
+          handleAdd={handleAdd}
+          type={0}
+        />
+      </TableBody>
+    </Table>
+  );
+
+  const selectElement = screen.getByLabelText("Frequency");
+  fireEvent.mouseDown(selectElement);
+
+  const dailyOption = screen.getByTestId("yearly-menu-item");
+  fireEvent.click(dailyOption);
+
+  expect(screen.getByLabelText("Title")).toBeInTheDocument();
+  expect(screen.getByLabelText("Description")).toBeInTheDocument();
+  expect(screen.getByLabelText("Amount")).toBeInTheDocument();
+  expect(screen.getByLabelText("Expense begin date")).toBeInTheDocument();
+  expect(screen.getByLabelText("Subsidized")).toBeInTheDocument();
+  expect(screen.getByLabelText("Day of week")).toBeInTheDocument();
+  expect(screen.getByLabelText("Week of month")).toBeInTheDocument();
+  expect(screen.getByLabelText("Month of year")).toBeInTheDocument();
+  expect(screen.getByText("Add expense")).toBeInTheDocument();
+  expect(screen.getByText("Cancel")).toBeInTheDocument();
+});
