32fcb38c2095da3d84e392730af4d7f793ca5d2e
Ephraim Becker <emb16@outlook.com>
2024-01-30 15:39:20 -0500

    You can now delete an expense.

--- client/components/ExpenseDelete.tsx
+++ client/components/ExpenseDelete.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import { Expense } from "@/app/types/types";
+import Box from "@mui/material/Box";
+import { deleteExpense } from "../services/actions/expense";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 function ExpenseDelete({
   expense,
@@ -9,10 +15,45 @@ function ExpenseDelete({
   expense: Expense;
   setExpenseModes: React.Dispatch<React.SetStateAction<Record<number, string>>>;
 }) {
+  const handleDelete = async () => {
+    try {
+      await deleteExpense(expense.id);
+    } catch (error) {
+      console.log(error);
+    }
+    setExpenseModes((prev) => ({ ...prev, [expense.id]: "view" }));
+  };
+
+  const handleCancel = () => {
+    setExpenseModes((prev) => ({ ...prev, [expense.id]: "view" }));
+  };
+
   return (
-    <div>
-      <h1>Delete form under construction</h1>
-    </div>
+    <Box sx={{ p: 2 }}>
+      <IconButton
+        aria-label="more"
+        sx={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+        }}
+        size="small"
+        onClick={handleCancel}
+      >
+        <CloseIcon />
+      </IconButton>
+      <br />
+      <br />
+      <Typography variant="subtitle1" component="h3">
+        Delete "{expense.title}"?
+      </Typography>
+      <Button color="error" variant="contained" onClick={handleDelete}>
+        Delete
+      </Button>
+      <Button color="primary" variant="contained" onClick={handleCancel}>
+        Cancel
+      </Button>
+    </Box>
   );
 }
 
