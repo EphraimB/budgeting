e54388328bd13e85d42425863e537981420ba940
Ephraim Becker <emb16@outlook.com>
2024-02-01 19:04:58 -0500

    Added a stepper.

--- client/components/NewExpenseForm.tsx
+++ client/components/NewExpenseForm.tsx
@@ -6,7 +6,14 @@ import IconButton from "@mui/material/IconButton";
 import Close from "@mui/icons-material/Close";
 import Typography from "@mui/material/Typography";
 import { Tax } from "@/app/types/types";
-import { CardContent, CardHeader, TextField } from "@mui/material";
+import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
+import TextField from "@mui/material/TextField";
+import MobileStepper from "@mui/material/MobileStepper";
+import Button from "@mui/material/Button";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import { useTheme } from "@mui/material/styles";
 
 function NewExpenseForm({
   setShowExpenseForm,
@@ -17,10 +24,20 @@ function NewExpenseForm({
 }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [activeStep, setActiveStep] = useState(0);
+
+  const theme = useTheme();
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
 
   return (
     <Card
-      component="form"
       sx={{
         position: "relative",
         maxWidth: "18rem",
@@ -39,22 +56,69 @@ function NewExpenseForm({
         <Close />
       </IconButton>
       <br />
+      <CardHeader
+        title={`New Expense - Step ${activeStep + 1} of 6`}
+        sx={{
+          textAlign: "center",
+        }}
+      />
       <CardContent>
-        <TextField
-          label="Title"
-          variant="standard"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          fullWidth
-        />
+        {activeStep === 0 && (
+          <>
+            <TextField
+              label="Title"
+              variant="standard"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              fullWidth
+            />
+            <br />
+            <br />
+            <TextField
+              label="Description"
+              variant="standard"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              fullWidth
+            />
+          </>
+        )}
         <br />
         <br />
-        <TextField
-          label="Description"
-          variant="standard"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          fullWidth
+        <MobileStepper
+          variant="dots"
+          steps={6}
+          position="static"
+          activeStep={activeStep}
+          sx={{ maxWidth: 400, flexGrow: 1 }}
+          nextButton={
+            <Button
+              size="small"
+              onClick={handleNext}
+              disabled={activeStep === 5}
+            >
+              Next
+              {theme.direction === "rtl" ? (
+                <KeyboardArrowLeft />
+              ) : (
+                <KeyboardArrowRight />
+              )}
+            </Button>
+          }
+          backButton={
+            <Button
+              size="small"
+              onClick={handleBack}
+              disabled={activeStep === 0}
+            >
+              {theme.direction === "rtl" ? (
+                <KeyboardArrowRight />
+              ) : (
+                <KeyboardArrowLeft />
+              )}
+              Back
+            </Button>
+          }
         />
       </CardContent>
     </Card>
