cc9bf3acd11f5baa4a03ead166bd84a42f90e3b5
Ephraim Becker <emb16@outlook.com>
2024-02-06 12:22:56 -0500

    Got rid of all the errors in LoanEdit.

--- client/services/actions/loan.ts
+++ client/services/actions/loan.ts
@@ -1,9 +1,25 @@
 "use server";
 
-import { Loan } from "@/app/types/types";
 import { revalidatePath } from "next/cache";
 
-export async function addLoan(loan: Loan) {
+interface LoanRequest {
+  account_id: number;
+  amount: number;
+  plan_amount: number;
+  title: string;
+  description: string;
+  frequency_type: number;
+  frequency_type_variable: number | null;
+  frequency_day_of_week: number | null;
+  frequency_week_of_month: number | null;
+  frequency_month_of_year: number | null;
+  subsidized: number;
+  interest_rate: number;
+  interest_frequency_type: number;
+  begin_date: string;
+}
+
+export async function addLoan(loan: LoanRequest) {
   console.log("loan: ", loan);
   const response = await fetch("http://server:5001/api/loans", {
     method: "POST",
@@ -18,7 +34,7 @@ export async function addLoan(loan: Loan) {
   return result;
 }
 
-export async function editLoan(loan: Loan, id: number) {
+export async function editLoan(loan: LoanRequest, id: number) {
   const response = await fetch(`http://server:5001/api/loans/${id}`, {
     method: "PUT",
     headers: {
