d31ed7fd09a0f0e826a7fac50d2bca6ee1b331f9
Ephraim Becker <emb16@outlook.com>
2024-03-11 19:19:31 -0400

    Added another test.

--- server/__tests__/utils/helperFunctions.test.ts
+++ server/__tests__/utils/helperFunctions.test.ts
@@ -177,6 +177,20 @@ describe('nextTransactionFrequencyDate function', () => {
         expect(result).toEqual('2020-12-16T00:00:00-05:00');
     });
 
+    it('should return the next transaction frequency date on a regular yearly', async () => {
+        const { nextTransactionFrequencyDate } = await import(
+            '../../src/utils/helperFunctions'
+        );
+
+        const transaction = {
+            frequency_type: 3,
+            begin_date: '2020-12-15',
+        };
+
+        const result = nextTransactionFrequencyDate(transaction);
+        expect(result).toEqual('2021-12-15T00:00:00-05:00');
+    });
+
     it('should return the next transaction frequency date on a weekly with a specific day', async () => {
         const { nextTransactionFrequencyDate } = await import(
             '../../src/utils/helperFunctions'
