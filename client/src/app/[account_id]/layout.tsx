d55d96492977db84922be11bf994526c272a74ea
Ephraim Becker <emb16@outlook.com>
2024-03-12 08:58:57 -0400

    Returns page not found if account doesn't exist.

--- client/src/app/[account_id]/layout.tsx
+++ client/src/app/[account_id]/layout.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 import DataManagementWidgets from "../../../components/DataManagementWidgets";
+import { notFound } from "next/navigation";
+
+async function getAccount(account_id: number) {
+  const res = await fetch(`http://server:5001/api/accounts?id=${account_id}`);
+
+  if (!res.ok) {
+    return notFound();
+    // open alert
+  }
+
+  return res.json();
+}
 
 async function getExpenses(account_id: number) {
   const res = await fetch(
@@ -45,6 +57,8 @@ export default async function Layout({
   params: { account_id: string };
 }) {
   const account_id = parseInt(params.account_id);
+
+  await getAccount(parseInt(params.account_id));
 
   const expenses = await getExpenses(account_id);
   const taxes = await getTaxes(account_id);
