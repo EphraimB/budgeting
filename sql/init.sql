bcac1c1fe3eda129f57b01b618881baa9101aa8d
Ephraim Becker <emb16@outlook.com>
2024-03-12 17:56:55 -0400

    Trying to find a way to attach the job to the account.

--- sql/init.sql
+++ sql/init.sql
@@ -17,6 +17,7 @@ GRANT USAGE ON SCHEMA cron TO marco;
 
 CREATE TABLE jobs (
   job_id SERIAL PRIMARY KEY,
+  account_id INTEGER REFERENCES accounts(account_id),
   job_name TEXT NOT NULL,
   hourly_rate NUMERIC(6,2) NOT NULL,
   regular_hours NUMERIC(4,2) NOT NULL,
