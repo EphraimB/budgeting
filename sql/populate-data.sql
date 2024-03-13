bcf55a3a506ccd8a3aa63d5528090c15548c355f
Ephraim Becker <emb16@outlook.com>
2024-03-13 09:50:31 -0400

    Fixed database.

--- sql/populate-data.sql
+++ sql/populate-data.sql
@@ -1,7 +1,7 @@
-INSERT INTO jobs (account_id, job_name, hourly_rate, regular_hours, vacation_days, sick_days, work_schedule) VALUES (1, 'Testing Inc.', 16.00, 8, 15, 15, B'0111100');;
-
 INSERT INTO accounts (account_name) VALUES ('Personal');
 INSERT INTO accounts (account_name) VALUES ('Savings');
+
+INSERT INTO jobs (account_id, job_name, hourly_rate, regular_hours, vacation_days, sick_days, work_schedule) VALUES (1, 'Testing Inc.', 16.00, 8, 15, 15, B'0111100');;
 
 INSERT INTO transaction_history (account_id, transaction_amount, transaction_tax_rate, transaction_title, transaction_description) VALUES (1, 1000.00, 0, 'Test Deposit', 'Just a test deposit');
 INSERT INTO transaction_history (account_id, transaction_amount, transaction_tax_rate, transaction_title, transaction_description) VALUES (2, 5000.00, 0, 'Test Deposit', 'Just a test deposit');
