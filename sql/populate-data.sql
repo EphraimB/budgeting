28f4140f29f39ec7ae1d9aeddba0eb735013aa04
Ephraim Becker <emb16@outlook.com>
2024-03-14 19:37:33 -0400

    Fixed the database.

--- sql/populate-data.sql
+++ sql/populate-data.sql
@@ -1,9 +1,11 @@
 INSERT INTO accounts (account_name) VALUES ('Personal');
 INSERT INTO accounts (account_name) VALUES ('Savings');
 
-INSERT INTO job_schedule (day_of_week, start_time, end_time) VALUES (1, '08:00:00', '17:00:00');
-
-INSERT INTO jobs (account_id, job_name, hourly_rate, vacation_days, sick_days, job_schedule_id) VALUES (1, 'Testing Inc.', 16.00, 15, 15, 1);;
+INSERT INTO jobs (account_id, job_name, hourly_rate, vacation_days, sick_days) VALUES (1, 'Testing Inc.', 16.00, 15, 15);
+INSERT INTO job_schedule (job_id, day_of_week, start_time, end_time) VALUES (1, 1, '09:00:00', '17:00:00');
+INSERT INTO job_schedule (job_id, day_of_week, start_time, end_time) VALUES (1, 2, '09:00:00', '17:00:00');
+INSERT INTO job_schedule (job_id, day_of_week, start_time, end_time) VALUES (1, 3, '09:00:00', '17:00:00');
+INSERT INTO job_schedule (job_id, day_of_week, start_time, end_time) VALUES (1, 4, '09:00:00', '17:00:00');
 
 INSERT INTO transaction_history (account_id, transaction_amount, transaction_tax_rate, transaction_title, transaction_description) VALUES (1, 1000.00, 0, 'Test Deposit', 'Just a test deposit');
 INSERT INTO transaction_history (account_id, transaction_amount, transaction_tax_rate, transaction_title, transaction_description) VALUES (2, 5000.00, 0, 'Test Deposit', 'Just a test deposit');
