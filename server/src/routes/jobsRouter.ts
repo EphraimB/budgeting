43b79b9301f192a00fcd7bd77216633eaf238035
Ephraim Becker <emb16@outlook.com>
2024-03-14 14:34:40 -0400

    Updated the router.

--- server/src/routes/jobsRouter.ts
+++ server/src/routes/jobsRouter.ts
@@ -25,6 +25,11 @@ import {
 
 const router: Router = express.Router();
 
+const convertTimeToMinutes = (time: string) => {
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
+};
+
 router.get(
     '/',
     [
@@ -61,10 +66,52 @@ router.post(
             .withMessage('Day of week must be a number'),
         body('work_schedule.*.start_time')
             .isString()
-            .withMessage('Start time must be a string'),
+            .withMessage('Start time must be a string')
+            .matches(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/)
+            .withMessage('Start time must be in HH:MM format')
+            .custom((value, { req, path }) => {
+                const startTime = convertTimeToMinutes(value);
+                if (
+                    startTime < convertTimeToMinutes('00:00') ||
+                    startTime > convertTimeToMinutes('23:59')
+                ) {
+                    return Promise.reject(
+                        'Start time is out of bounds (00:00 to 23:59)',
+                    );
+                }
+
+                // Extract index to compare with end time
+                const index = path.split('.')[2];
+                const endTime = convertTimeToMinutes(
+                    req.body.work_schedule[index].end_time,
+                );
+
+                if (endTime <= startTime) {
+                    return Promise.reject(
+                        'End time must be greater than start time',
+                    );
+                }
+
+                return true;
+            }),
+
         body('work_schedule.*.end_time')
             .isString()
-            .withMessage('End time must be a string'),
+            .withMessage('End time must be a string')
+            .matches(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/)
+            .withMessage('End time must be in HH:MM format')
+            .custom((value) => {
+                const endTime = convertTimeToMinutes(value);
+                if (
+                    endTime < convertTimeToMinutes('00:00') ||
+                    endTime > convertTimeToMinutes('23:59')
+                ) {
+                    return Promise.reject(
+                        'End time is out of bounds (00:00 to 23:59)',
+                    );
+                }
+                return true;
+            }),
         validateRequest,
     ],
     createJob,
@@ -97,10 +144,52 @@ router.put(
             .withMessage('Day of week must be a number'),
         body('work_schedule.*.start_time')
             .isString()
-            .withMessage('Start time must be a string'),
+            .withMessage('Start time must be a string')
+            .matches(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/)
+            .withMessage('Start time must be in HH:MM format')
+            .custom((value, { req, path }) => {
+                const startTime = convertTimeToMinutes(value);
+                if (
+                    startTime < convertTimeToMinutes('00:00') ||
+                    startTime > convertTimeToMinutes('23:59')
+                ) {
+                    return Promise.reject(
+                        'Start time is out of bounds (00:00 to 23:59)',
+                    );
+                }
+
+                // Extract index to compare with end time
+                const index = path.split('.')[2];
+                const endTime = convertTimeToMinutes(
+                    req.body.work_schedule[index].end_time,
+                );
+
+                if (endTime <= startTime) {
+                    return Promise.reject(
+                        'End time must be greater than start time',
+                    );
+                }
+
+                return true;
+            }),
+
         body('work_schedule.*.end_time')
             .isString()
-            .withMessage('End time must be a string'),
+            .withMessage('End time must be a string')
+            .matches(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/)
+            .withMessage('End time must be in HH:MM format')
+            .custom((value) => {
+                const endTime = convertTimeToMinutes(value);
+                if (
+                    endTime < convertTimeToMinutes('00:00') ||
+                    endTime > convertTimeToMinutes('23:59')
+                ) {
+                    return Promise.reject(
+                        'End time is out of bounds (00:00 to 23:59)',
+                    );
+                }
+                return true;
+            }),
         validateRequest,
     ],
     updateJob,
