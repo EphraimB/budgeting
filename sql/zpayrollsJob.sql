7df3caf76e9b3437d04a8ae63dffae5873d52f81
Ephraim Becker <emb16@outlook.com>
2024-03-14 20:01:40 -0400

    Commented out outdated query.

--- sql/zpayrollsJob.sql
+++ sql/zpayrollsJob.sql
@@ -1,7 +1,7 @@
-WITH job_list AS (SELECT job_id FROM jobs)
-    SELECT process_payroll_for_job(job_id) FROM job_list;
+-- WITH job_list AS (SELECT job_id FROM jobs)
+--     SELECT process_payroll_for_job(job_id) FROM job_list;
 
-SELECT cron.schedule('monthly-payroll-job', '0 0 1 * *', $$
-    WITH employee_list AS (SELECT job_id FROM jobs)
-    SELECT process_payroll_for_job(job_id) FROM job_list;
-$$);
+-- SELECT cron.schedule('monthly-payroll-job', '0 0 1 * *', $$
+--     WITH employee_list AS (SELECT job_id FROM jobs)
+--     SELECT process_payroll_for_job(job_id) FROM job_list;
+-- $$);
