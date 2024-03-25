d6078901f20dacfc4f671de96ed1a05ff27353df
Ephraim Becker <emb16@outlook.com>
2024-03-25 11:27:18 -0400

    Added more job information.

--- client/src/app/[account_id]/jobs/[job_id]/page.tsx
+++ client/src/app/[account_id]/jobs/[job_id]/page.tsx
@@ -2,6 +2,9 @@ import { Job } from "@/app/types/types";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import JobScheduleDayView from "../../../../../components/JobScheduleDayView";
+import HourlyWage from "../../../../../components/HourlyWage";
+import SickDays from "../../../../../components/SickDays";
+import VacationDays from "../../../../../components/VacationDays";
 
 async function getJob(job_id: number) {
   const res = await fetch(`http://server:5001/api/jobs?id=${job_id}`);
@@ -23,6 +26,12 @@ async function JobDetails({ params }: { params: { job_id: string } }) {
       <Typography variant="h4" component="h2">
         Job Details for {job[0].name}
       </Typography>
+      <br />
+      <Stack direction="row" spacing={2}>
+        <HourlyWage hourly_wage={job[0].hourly_rate} />
+        <SickDays sick_days={job[0].sick_days} />
+        <VacationDays vacation_days={job[0].vacation_days} />
+      </Stack>
       <br />
       <JobScheduleDayView job_schedule={job[0].job_schedule} />
     </Stack>
