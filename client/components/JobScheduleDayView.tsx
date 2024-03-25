e67d0c538903c28171d7ccad204c8bcac53fe768
Ephraim Becker <emb16@outlook.com>
2024-03-25 11:07:47 -0400

    Fixed job schedules on mobile.

--- client/components/JobScheduleDayView.tsx
+++ client/components/JobScheduleDayView.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { useAlert, useSnackbar } from "../context/FeedbackContext";
 import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
 import { Typography } from "@mui/material";
 import { JobSchedule } from "@/app/types/types";
 import JobScheduleView from "./JobScheduleView";
@@ -20,34 +20,33 @@ function JobScheduleDayView({ job_schedule }: { job_schedule: JobSchedule[] }) {
   ];
 
   return (
-    <Grid container spacing={2}>
+    <Stack direction="row" spacing={2} useFlexGap flexWrap="wrap">
       {days.map((day, index) => (
-        <Grid key={index} item xs={6} md={1}>
-          <Paper
-            sx={{
-              backgroundColor: "black",
-              color: "white",
-              p: 2,
-              position: "relative",
-            }}
+        <Paper
+          key={index}
+          sx={{
+            backgroundColor: "black",
+            color: "white",
+            p: 2,
+            position: "relative",
+          }}
+        >
+          <Typography
+            variant="body2"
+            component="p"
+            sx={{ textAlign: "center" }}
+            gutterBottom
           >
-            <Typography
-              variant="body2"
-              component="p"
-              sx={{ textAlign: "center" }}
-              gutterBottom
-            >
-              {day}
-            </Typography>
-            <JobScheduleView
-              job_day_of_week={job_schedule.filter(
-                (js) => js.day_of_week === index
-              )}
-            />
-          </Paper>
-        </Grid>
+            {day}
+          </Typography>
+          <JobScheduleView
+            job_day_of_week={job_schedule.filter(
+              (js) => js.day_of_week === index
+            )}
+          />
+        </Paper>
       ))}
-    </Grid>
+    </Stack>
   );
 }
 
