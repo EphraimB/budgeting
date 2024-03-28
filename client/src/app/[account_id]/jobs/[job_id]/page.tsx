f0239d04ea8faff0275521bf4433f8b80acf5e7d
Ephraim Becker <emb16@outlook.com>
2024-03-28 09:30:25 -0400

    Getting started on payroll dates.

--- client/src/app/[account_id]/jobs/[job_id]/page.tsx
+++ client/src/app/[account_id]/jobs/[job_id]/page.tsx
@@ -17,10 +17,23 @@ async function getJob(job_id: number) {
   return res.json();
 }
 
+async function getPayrollDates(job_id: number) {
+  const res = await fetch(
+    `http://server:5001/api/payroll_dates?job_id=${job_id}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch payroll dates");
+  }
+
+  return res.json();
+}
+
 async function JobDetails({ params }: { params: { job_id: string } }) {
   const job_id = parseInt(params.job_id);
 
   const job: Job[] = await getJob(job_id);
+  const payroll_dates = await getPayrollDates(job_id);
 
   return <JobDetailsView job={job[0]} />;
 }
