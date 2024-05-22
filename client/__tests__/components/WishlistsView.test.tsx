c134272dd115e402f9dae6d6530fdca082b14d69
Ephraim Becker <emb16@outlook.com>
2024-05-22 15:17:55 -0400

    Fixed a test.

--- client/__tests__/components/WishlistsView.test.tsx
+++ client/__tests__/components/WishlistsView.test.tsx
@@ -5,25 +5,25 @@ import "@testing-library/jest-dom";
 import { Wishlist } from "@/app/types/types";
 
 describe("WishlistView", () => {
-  const setWishlistModes = jest.fn();
+  it("Renders a wishlist view without a url link provided", () => {
+    const setWishlistModes = jest.fn();
 
-  const wishlist: Wishlist = {
-    id: 1,
-    tax_id: 1,
-    tax_rate: 0,
-    account_id: 1,
-    wishlist_title: "Wishlist test",
-    wishlist_description: "This is a test wishlist",
-    wishlist_amount: 1000,
-    wishlist_priority: 0,
-    wishlist_date_available: "2024-10-10",
-    wishlist_date_can_purchase: "2024-10-10",
-    wishlist_url_link: "",
-    date_created: "2021-10-01T00:00:00.000Z",
-    date_modified: "2021-10-01T00:00:00.000Z",
-  };
+    const wishlist: Wishlist = {
+      id: 1,
+      tax_id: 1,
+      tax_rate: 0,
+      account_id: 1,
+      wishlist_title: "Wishlist test",
+      wishlist_description: "This is a test wishlist",
+      wishlist_amount: 1000,
+      wishlist_priority: 0,
+      wishlist_date_available: "2024-10-10",
+      wishlist_date_can_purchase: "2024-10-10",
+      wishlist_url_link: "",
+      date_created: "2021-10-01T00:00:00.000Z",
+      date_modified: "2021-10-01T00:00:00.000Z",
+    };
 
-  it("renders", () => {
     render(
       <WishlistsView wishlist={wishlist} setWishlistModes={setWishlistModes} />
     );
@@ -37,4 +37,37 @@ describe("WishlistView", () => {
       )
     ).toBeInTheDocument();
   });
+});
+it("Renders a wishlist view with a url link provided", () => {
+  const setWishlistModes = jest.fn();
+
+  const wishlist: Wishlist = {
+    id: 1,
+    tax_id: 1,
+    tax_rate: 0,
+    account_id: 1,
+    wishlist_title: "iPhone 16 Pro Max",
+    wishlist_description: "iPhone 16 Pro Max 1TB",
+    wishlist_amount: 1699,
+    wishlist_priority: 0,
+    wishlist_date_available: "2024-09-22",
+    wishlist_date_can_purchase: "2024-09-22",
+    wishlist_url_link: "https://www.apple.com/iphone",
+    date_created: "2021-10-01T00:00:00.000Z",
+    date_modified: "2021-10-01T00:00:00.000Z",
+  };
+
+  render(
+    <WishlistsView wishlist={wishlist} setWishlistModes={setWishlistModes} />
+  );
+
+  expect(screen.getByTestId("MoreVertIcon")).toBeInTheDocument();
+  expect(screen.getByText("iPhone 16 Pro Max")).toBeInTheDocument();
+  expect(screen.getByText("iPhone 16 Pro Max 1TB")).toBeInTheDocument();
+  expect(
+    screen.getByText(
+      "You will be charged $1699 for this item on Sunday September 22, 2024 12:00 AM."
+    )
+  ).toBeInTheDocument();
+  expect(screen.getByText("View wishlist item here")).toBeInTheDocument();
 });
