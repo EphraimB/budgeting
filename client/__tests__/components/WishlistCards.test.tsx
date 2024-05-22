66e65864fb819f0e5c9c03f94106841f7f7873c4
Ephraim Becker <emb16@outlook.com>
2024-05-22 15:22:25 -0400

    Added wishlist cards test.

---
+++ client/__tests__/components/WishlistCards.test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import WishlistsCards from "../../components/WishlistsCards";
+import { Tax } from "@/app/types/types";
+
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(),
+  usePathname: () => "/",
+}));
+
+describe("WishlistCards", () => {
+  it("renders wishlist cards with provided wishlists", () => {
+    const account_id: number = 1;
+    const taxes: Tax[] = [];
+
+    render(
+      <WishlistsCards account_id={account_id} wishlists={[]} taxes={taxes} />
+    );
+
+    // Assert that the FAB with the AddIcon is rendered
+    expect(screen.getByTestId("AddIcon")).toBeInTheDocument();
+  });
+});
