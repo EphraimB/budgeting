6e5e086eda2fdf3f9382f882a7ba2639b4494b80
Ephraim Becker <emb16@outlook.com>
2024-05-22 15:26:13 -0400

    Added a wishlist action menu test.

---
+++ client/__tests__/components/WishlistsActionMenu.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import WishlistsActionsMenu from "../../components/WishlistsActionsMenu";
+
+const setWishlistModes = jest.fn();
+
+const anchorEl = document.createElement("div");
+
+const open = true;
+
+const handleClose = () => jest.fn();
+
+describe("WishlistsActionsMenu", () => {
+  it("displays the menu when the button is clicked", async () => {
+    render(
+      <WishlistsActionsMenu
+        anchorEl={anchorEl}
+        open={open}
+        handleClose={handleClose}
+        setWishlistModes={setWishlistModes}
+        wishlist_id={1}
+      />
+    );
+
+    expect(screen.getByText("Edit")).toBeInTheDocument();
+    expect(screen.getByText("Delete")).toBeInTheDocument();
+  });
+});
