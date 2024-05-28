d76162c2cba11e76c1bc0405d1cd1e8f96d97e82
Ephraim Becker <emb16@outlook.com>
2024-05-28 10:59:11 -0400

    Added payroll date card test.

---
+++ client/__tests__/components/PayrollDateCard.test.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import PayrollDateCard from "../../components/PayrollDateCard";
+import "@testing-library/jest-dom";
+import { PayrollDate } from "@/app/types/types";
+
+jest.mock("../../context/FeedbackContext", () => ({
+  useAlert: () => ({
+    alert: () => {},
+    closeAlert: () => {},
+  }),
+  useSnackbar: () => ({
+    showSnackbar: () => {},
+  }),
+}));
+
+describe("PayrollDates", () => {
+  const payroll_date: PayrollDate = {
+    id: 1,
+    job_id: 1,
+    payroll_day: 15,
+  };
+  it("renders a box which is not a payroll day", () => {
+    render(<PayrollDateCard job_id={1} payroll_date={payroll_date} date={1} />);
+
+    expect(screen.getByText("1")).toBeInTheDocument();
+  });
+});
