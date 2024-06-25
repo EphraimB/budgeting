7b613c2316423e287d32af5b55f9e56fd890f457
Ephraim Becker <emb16@outlook.com>
2024-06-25 14:38:20 -0400

    Added the add fab.

--- client/components/PayrollTaxesCards.tsx
+++ client/components/PayrollTaxesCards.tsx
@@ -5,6 +5,9 @@ import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import { Typography } from "@mui/material";
 import NewPayrollTaxForm from "./NewPayrollTaxForm";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import Box from "@mui/material/Box";
 
 function PayrollTaxesCards({
   job,
@@ -30,6 +33,12 @@ function PayrollTaxesCards({
           </Grid>
         )}
       </Grid>
+      <br />
+      <Box sx={{ position: "fixed", bottom: 16, right: 16 }}>
+        <Fab color="primary" onClick={() => setShowPayrollTaxesForm(true)}>
+          <AddIcon />
+        </Fab>
+      </Box>
     </>
   );
 }
