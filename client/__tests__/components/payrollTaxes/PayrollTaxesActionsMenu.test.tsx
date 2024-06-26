e6274739a35d0b7aede867b9ee273f379ba8b719
Ephraim Becker <emb16@outlook.com>
2024-06-26 11:05:08 -0400

    Added a test.

---
+++ client/__tests__/components/payrollTaxes/PayrollTaxesActionsMenu.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import PayrollTaxesActionsMenu from "../../../components/payrollTaxes/PayrollTaxesActionsMenu";
+
+const setPayrollTaxModes = jest.fn();
+
+const anchorEl = document.createElement("div");
+
+const open = true;
+
+const handleClose = () => jest.fn();
+
+describe("PayrollTaxActionsMenu", () => {
+  it("displays the menu when the button is clicked", async () => {
+    render(
+      <PayrollTaxesActionsMenu
+        anchorEl={anchorEl}
+        open={open}
+        handleClose={handleClose}
+        setPayrollTaxModes={setPayrollTaxModes}
+        payroll_tax_id={1}
+      />
+    );
+
+    expect(screen.getByText("Edit")).toBeInTheDocument();
+    expect(screen.getByText("Delete")).toBeInTheDocument();
+  });
+});
