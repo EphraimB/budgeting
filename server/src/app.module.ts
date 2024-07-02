5d5ec07b6e1ae43f70a7dd8293ef2bb04a5a16a6
Ephraim Becker <emb16@outlook.com>
2024-07-02 11:26:02 -0400

    Removed unused import.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AccountsController } from './accounts/accounts.controller';
 import { AccountsModule } from './accounts/accounts.module';
 
 @Module({
