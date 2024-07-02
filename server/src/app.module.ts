c69928a072acf720304f7fa8055ccac2521be189
Ephraim Becker <emb16@outlook.com>
2024-07-02 11:25:38 -0400

    Added accounts.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccountsController } from './accounts/accounts.controller';
+import { AccountsModule } from './accounts/accounts.module';
 
 @Module({
-  imports: [],
+  imports: [AccountsModule],
   controllers: [AppController],
   providers: [AppService],
 })
