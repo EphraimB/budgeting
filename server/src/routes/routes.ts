3dbcca9edef27b3988777a3117d117efad14a6d1
Ephraim Becker <emb16@outlook.com>
2024-08-22 10:32:18 -0400

    Changed the response of the root api.

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -3,7 +3,7 @@ import express, { type Request, type Response, type Router } from 'express';
 const router: Router = express.Router();
 
 router.get('/', (_: Request, res: Response) => {
-    res.status(200).send('Hello World!');
+    res.status(200).send('Hello Budgeting!');
 });
 
 export default router;
