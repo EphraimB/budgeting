0e9d48a0c177d00866f31c8776d8bd2ec5cbfdf4
Ephraim Becker <emb16@outlook.com>
2024-10-09 10:41:20 -0400

    Added a test for end date until.

--- client/__tests__/components/expenses/ExpensesView.test.tsx
+++ client/__tests__/components/expenses/ExpensesView.test.tsx
@@ -7,32 +7,32 @@ import { Expense } from "@/app/types/types";
 describe("ExpensesView", () => {
   const setExpenseModes = jest.fn();
 
-  const expense: Expense = {
-    id: 1,
-    taxId: null,
-    accountId: 1,
-    title: "Test",
-    description: "This is a test expense",
-    amount: 1000,
-    nextDate: "2022-01-01T00:00:00.000Z",
-    frequency: {
-      type: 2,
-      typeVariable: 1,
-      dayOfMonth: null,
-      dayOfWeek: null,
-      weekOfMonth: null,
-      monthOfYear: null,
-    },
-    subsidized: 0,
-    dates: {
-      beginDate: "2021-01-01T00:00:00.000Z",
-      endDate: null,
-    },
-    dateCreated: "2021-10-01T00:00:00.000Z",
-    dateModified: "2021-10-01T00:00:00.000Z",
-  };
+  it("renders without an end date", () => {
+    const expense: Expense = {
+      id: 1,
+      taxId: null,
+      accountId: 1,
+      title: "Test",
+      description: "This is a test expense",
+      amount: 1000,
+      nextDate: "2022-01-01T00:00:00.000Z",
+      frequency: {
+        type: 2,
+        typeVariable: 1,
+        dayOfMonth: null,
+        dayOfWeek: null,
+        weekOfMonth: null,
+        monthOfYear: null,
+      },
+      subsidized: 0,
+      dates: {
+        beginDate: "2021-01-01T00:00:00.000Z",
+        endDate: null,
+      },
+      dateCreated: "2021-10-01T00:00:00.000Z",
+      dateModified: "2021-10-01T00:00:00.000Z",
+    };
 
-  it("renders", () => {
     render(
       <ExpensesView
         expense={expense}
@@ -46,6 +46,49 @@ describe("ExpensesView", () => {
     expect(
       screen.getByText(
         "You will be charged $1000.00 next on Friday December 31, 2021 7:00 PM. You get charged monthly on the 31st."
+      )
+    ).toBeInTheDocument();
+  });
+
+  it("renders with an end date", () => {
+    const expense: Expense = {
+      id: 1,
+      taxId: null,
+      accountId: 1,
+      title: "Test",
+      description: "This is a test expense",
+      amount: 1000,
+      nextDate: "2022-01-01T00:00:00.000Z",
+      frequency: {
+        type: 2,
+        typeVariable: 1,
+        dayOfMonth: null,
+        dayOfWeek: null,
+        weekOfMonth: null,
+        monthOfYear: null,
+      },
+      subsidized: 0,
+      dates: {
+        beginDate: "2021-01-01T00:00:00.000Z",
+        endDate: "2022-03-01T00:00:00.000Z",
+      },
+      dateCreated: "2021-10-01T00:00:00.000Z",
+      dateModified: "2021-10-01T00:00:00.000Z",
+    };
+
+    render(
+      <ExpensesView
+        expense={expense}
+        setExpenseModes={setExpenseModes}
+        taxes={[]}
+      />
+    );
+
+    expect(screen.getByTestId("MoreVertIcon")).toBeInTheDocument();
+    expect(screen.getByText("Test")).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        "You will be charged $1000.00 next on Friday December 31, 2021 7:00 PM. You get charged monthly on the 31st until Monday February 28, 2022 7:00 PM."
       )
     ).toBeInTheDocument();
   });
