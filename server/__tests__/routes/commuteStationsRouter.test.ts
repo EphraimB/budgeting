b47d23e4372db6d9c33b1f728a07df9c55e745ab
Ephraim Becker <emb16@outlook.com>
2024-10-30 15:14:35 -0400

    Added commute station router test.

---
+++ server/__tests__/routes/commuteStationsRouter.test.ts
@@ -0,0 +1,144 @@
+import { jest } from '@jest/globals';
+import request from 'supertest';
+import express, {
+    type Express,
+    type Request,
+    type Response,
+    type Router,
+} from 'express';
+import {
+    beforeAll,
+    afterAll,
+    beforeEach,
+    describe,
+    it,
+    expect,
+} from '@jest/globals';
+
+/**
+ *
+ * @returns {Promise<Express>} A promise that resolves to an Express app
+ */
+const createApp = async (): Promise<Express> => {
+    const app: Express = express();
+    app.use(express.json());
+
+    // Import the module that uses the mock
+    const routerModule = await import('../../src/routes/commuteStationsRouter');
+    const commuteStationsRouter: Router = routerModule.default;
+    app.use('/', commuteStationsRouter);
+
+    return app;
+};
+
+beforeAll(() => {
+    jest.mock('../../src/controllers/commuteStationsController', () => ({
+        getStations: (_: Request, res: Response) =>
+            res.json({ message: 'success' }),
+        getStationById: (_: Request, res: Response) =>
+            res.json({ message: 'success' }),
+        createStation: (_: Request, res: Response) =>
+            res.json({ message: 'success' }),
+        updateStation: (_: Request, res: Response) =>
+            res.json({ message: 'success' }),
+        deleteStation: (_: Request, res: Response) =>
+            res.json({ message: 'success' }),
+    }));
+});
+
+afterAll(() => {
+    jest.restoreAllMocks();
+});
+
+let app: Express;
+
+beforeEach(async () => {
+    // Create a new app for each test
+    app = await createApp();
+});
+
+describe('GET /', () => {
+    it('responds with json', async () => {
+        const response: request.Response = await request(app)
+            .get('/')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('GET / with id param', () => {
+    it('responds with json', async () => {
+        const response: request.Response = await request(app)
+            .get('/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('GET / with account id query', () => {
+    it('responds with json', async () => {
+        const response: request.Response = await request(app)
+            .get('/?accountId=1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('POST /', () => {
+    it('responds with json', async () => {
+        const newStation = {
+            commuteSystemId: 1,
+            fromStation: '7th Av',
+            toStation: '34 St-Penn Station',
+        };
+
+        const response: request.Response = await request(app)
+            .post('/')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .send(newStation);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('PUT /:id', () => {
+    it('responds with json', async () => {
+        const updatedStation = {
+            commuteSystemId: 1,
+            fromStation: '7th Av',
+            toStation: '34 St-Penn Station',
+        };
+
+        const response: request.Response = await request(app)
+            .put('/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .send(updatedStation);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
+
+describe('DELETE /:id', () => {
+    it('responds with json', async () => {
+        const response: request.Response = await request(app)
+            .delete('/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ message: 'success' });
+    });
+});
