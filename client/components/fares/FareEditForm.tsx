95b337db018d930f176e74c60fb827cefbc5334d
Ephraim Becker <emb16@outlook.com>
2024-11-14 11:22:21 -0500

    Added space between the start and end time.

--- client/components/fares/FareEditForm.tsx
+++ client/components/fares/FareEditForm.tsx
@@ -168,6 +168,7 @@ const FareEditForm = ({
                     }}
                   />
                   <br />
+                  <br />
                   <TimeField
                     label="End Time"
                     value={dayjs()
