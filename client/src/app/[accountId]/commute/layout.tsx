9a4333b9eac8463c6bb3306d7f0db7f944be9abc
Ephraim Becker <emb16@outlook.com>
2024-11-21 11:26:37 -0500

    Fixed bugs.

--- client/src/app/[accountId]/commute/layout.tsx
+++ client/src/app/[accountId]/commute/layout.tsx
@@ -1,13 +1,7 @@
 import CommutePanels from "../../../../components/commute/CommutePanels";
-import { CommuteSystem } from "@/app/types/types";
 
 async function CommuteLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <CommutePanels>{children}</CommutePanels>
-      {children}
-    </>
-  );
+  return <CommutePanels>{children}</CommutePanels>;
 }
 
 export default CommuteLayout;
