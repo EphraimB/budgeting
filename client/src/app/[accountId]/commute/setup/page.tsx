b1df7e56ce9bbb16653b7d86461544d7c2bf0e2b
Ephraim Becker <emb16@outlook.com>
2024-12-02 10:55:20 -0500

    Got rid of the extra semicolon.

--- client/src/app/[accountId]/commute/setup/page.tsx
+++ client/src/app/[accountId]/commute/setup/page.tsx
@@ -1,5 +1,6 @@
 import { CommuteSystem } from "@/app/types/types";
 import CommuteSystemCards from "../../../../../components/commuteSystem/CommuteSystemCards";
+import { Typography } from "@mui/material";
 
 async function getCommuteSystems() {
   try {
@@ -18,7 +19,16 @@ async function getCommuteSystems() {
 async function CommuteSetup() {
   const commuteSystems: CommuteSystem[] = await getCommuteSystems();
 
-  return <CommuteSystemCards commuteSystems={commuteSystems} />;
+  return (
+    <>
+      <Typography component="h2" variant="h6">
+        Setup
+      </Typography>
+      <br />
+      <br />
+      <CommuteSystemCards commuteSystems={commuteSystems} />
+    </>
+  );
 }
 
 export default CommuteSetup;
